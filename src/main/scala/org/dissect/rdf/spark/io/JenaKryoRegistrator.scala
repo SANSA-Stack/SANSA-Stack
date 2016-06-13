c1a6fb68a6c3ab241862999e1c4a3dc25f9dd4c0
Nilesh Chakraborty <nilesh@nileshc.com>
2016-06-13 18:28:54 +0200

    Register more classes

--- src/main/scala/org/dissect/rdf/spark/io/JenaKryoRegistrator.scala
+++ src/main/scala/org/dissect/rdf/spark/io/JenaKryoRegistrator.scala
@@ -13,11 +13,14 @@ class JenaKryoRegistrator extends KryoRegistrator {
   override def registerClasses(kryo: Kryo) {
     kryo.register(classOf[Array[org.apache.jena.graph.Node]], new NodeArraySerializer)
     kryo.register(classOf[org.apache.jena.graph.Node_Blank], new BlankNodeSerializer)
-    kryo.register(classOf[org.apache.jena.graph.Node_ANY], new VariableNodeSerializer)
+    kryo.register(classOf[org.apache.jena.graph.Node_ANY], new ANYNodeSerializer)
+    kryo.register(classOf[org.apache.jena.graph.Node_Variable], new VariableNodeSerializer)
     kryo.register(classOf[org.apache.jena.graph.Node_URI], new URINodeSerializer)
     kryo.register(classOf[org.apache.jena.graph.Node_Literal], new LiteralNodeSerializer)
     kryo.register(classOf[org.apache.jena.graph.Triple], new TripleSerializer)
-    kryo.register(classOf[org.dissect.rdf.spark.model.SparkRDDGraphOps[JenaSparkRDD]])
+    kryo.register(classOf[Array[org.apache.jena.graph.Triple]])
+    kryo.register(Class.forName("org.dissect.rdf.spark.model.SparkRDDGraphOps$$anonfun$findGraph$1"))
+    kryo.register(classOf[scala.collection.mutable.WrappedArray.ofRef[_]])
     kryo.register(classOf[org.dissect.rdf.spark.model.TripleRDD])
   }
 }
