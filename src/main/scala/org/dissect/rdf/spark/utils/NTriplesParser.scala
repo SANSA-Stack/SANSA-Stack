975d2629b56b109e31661e365412789ec70bfbc7
Ivan Ermilov <earthquakesan@gmail.com>
2016-08-09 16:37:14 +0200

    removed commented out lines, keep your code tidy added return type annotation to functions

--- src/main/scala/org/dissect/rdf/spark/utils/NTriplesParser.scala
+++ src/main/scala/org/dissect/rdf/spark/utils/NTriplesParser.scala
@@ -3,6 +3,7 @@ package org.dissect.rdf.spark.utils
 import org.apache.jena.riot.RiotReader
 import org.apache.jena.riot.Lang
 import org.apache.jena.riot.RDFDataMgr
+import org.apache.jena.graph.Node
 import java.io.ByteArrayInputStream
 
 object NTriplesParser {
@@ -13,32 +14,15 @@ object NTriplesParser {
   // regex pattern for language tag
   def languageTagPattern() = "@[\\w-]+".r
 
-  /*  private def parseSubject(c: Char): Unit = {
-    c match {
-      case '<' => parseURI()
-      case '_' => parseBNode()
-      case x => throw Error("Subject of Triple must start with a URI or bnode .")
-    }
-  }
-  
-  private def parseObject(c: Int) = {
-    c match {
-      case '<' => URI()
-      case '"' => Literal()
-      case '_' => BlankNode()
-      case other => throw Error("illegal character to start triple entity ( subject, relation, or object)")
-    }
-  }*/
-
   /*
    * Parse Triples
    */
-  def parseTriple(fn: String) = {
+  def parseTriple(fn: String): (String, String, String) = {
     val triples = RDFDataMgr.createIteratorTriples(new ByteArrayInputStream(fn.getBytes), Lang.NTRIPLES, "http://example/base").next
     (triples.getSubject.toString(), triples.getPredicate.toString(), if (triples.getObject.isLiteral()) triples.getObject.getLiteralLexicalForm().toString() else triples.getObject().toString())
   }
-  
-  def parseTripleAsNode(fn: String) = {
+
+  def parseTripleAsNode(fn: String): (Node, Node, Node) = {
     val triples = RDFDataMgr.createIteratorTriples(new ByteArrayInputStream(fn.getBytes), Lang.NTRIPLES, "http://example/base").next
     (triples.getSubject(), triples.getPredicate(), triples.getObject())
   }
