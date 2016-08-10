f4a99bf265a2a22826da329f09b928b606046971
Ivan Ermilov <earthquakesan@gmail.com>
2016-08-10 11:22:26 +0200

    added spec for NTripleReader

---
+++ src/test/scala/spark/utils/NTripleReaderSpec.scala
@@ -0,0 +1,36 @@
+package spark.utils
+
+import org.dissect.rdf.spark.utils.NTripleReader
+import java.io.File
+
+import org.scalatest._
+import spark.UnitSpec
+
+class NTripleReaderSpec extends UnitSpec {
+
+  def fixture =
+    new {
+      //val rdfFile = Source.fromURL(getClass.getResource("/rdf.nt"))
+      val rdfFile = new File(getClass.getResource("/rdf.nt").getPath())
+    }
+
+  it should "read rdf.nt" in {
+    val f = fixture
+    val tripleReader = NTripleReader.fromFile(f.rdfFile)
+    var count = 0
+    for (quad <- tripleReader) {
+      val (subject, predicate, _object, literal) = quad
+      count += 1
+    }
+    assert(count === 100)
+  }
+
+  it should "not read the whole file in the memory" in {
+    assert(false === true, "the whole RDF file should not be read completely into the memory")
+  }
+
+  it should "use NTriplesParser for parsing" in {
+    assert(false === true, "should use NTriplesParser for parsing ntriples")
+  }
+
+}
