d710d95dee22d2a881aa809b67ca2602a53a02f9
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-08-10 14:18:33 +0200

    Cntd. plan generator.

--- inference-common/src/main/scala/org/sansa/inference/rules/plan/PlanGenerator.scala
+++ inference-common/src/main/scala/org/sansa/inference/rules/plan/PlanGenerator.scala
@@ -2,14 +2,20 @@ package org.sansa.inference.rules.plan
 
 import java.io.PrintWriter
 
+import com.google.common.collect.ImmutableList
 import org.apache.calcite.config.Lex
-import org.apache.calcite.plan.{Contexts, ConventionTraitDef}
-import org.apache.calcite.rel.RelCollationTraitDef
+import org.apache.calcite.plan._
+import org.apache.calcite.plan.volcano.VolcanoPlanner
+import org.apache.calcite.rel.{RelCollationTraitDef, RelNode}
 import org.apache.calcite.rel.`type`.RelDataTypeSystem
 import org.apache.calcite.rel.externalize.RelWriterImpl
+import org.apache.calcite.rel.rules.{FilterJoinRule, ProjectJoinTransposeRule}
 import org.apache.calcite.rel.rules.FilterJoinRule.FilterIntoJoinRule
+import org.apache.calcite.schema.SchemaPlus
 import org.apache.calcite.sql.parser.SqlParser
-import org.apache.calcite.tools.{Frameworks, RuleSets}
+import org.apache.calcite.sql2rel.RelDecorrelator
+import org.apache.calcite.tools.Frameworks.PlannerAction
+import org.apache.calcite.tools.{Frameworks, Programs, RuleSets}
 import org.apache.jena.graph.Node
 import org.apache.jena.reasoner.rulesys.Rule
 import org.sansa.inference.utils.RuleUtils
@@ -23,7 +29,13 @@ import scala.collection.mutable
   */
 class PlanGenerator {
 
-  val traitDefs = List(ConventionTraitDef.INSTANCE, RelCollationTraitDef.INSTANCE)
+  val traitDefs: ImmutableList[RelTraitDef[_ <: RelTrait]] = ImmutableList.of(ConventionTraitDef.INSTANCE, RelCollationTraitDef.INSTANCE)
+
+  val optRuleSet = RuleSets.ofList(
+    FilterJoinRule.FILTER_ON_JOIN,// push a filter into a join
+    FilterJoinRule.JOIN,// push filter into the children of a join
+    ProjectJoinTransposeRule.INSTANCE// push a projection to the children of a join
+  )
 
   val calciteFrameworkConfig = Frameworks.newConfigBuilder()
     .parserConfig(SqlParser.configBuilder()
@@ -33,19 +45,39 @@ class PlanGenerator {
       .build())
     // Sets the schema to use by the planner
     .defaultSchema(TriplesSchema.get())
-//    .traitDefs(traitDefs)
+    .traitDefs(traitDefs)
     // Context provides a way to store data within the planner session that can be accessed in planner rules.
     .context(Contexts.EMPTY_CONTEXT)
     // Rule sets to use in transformation phases. Each transformation phase can use a different set of rules.
-    .ruleSets(RuleSets.ofList(new FilterIntoJoinRule()))
+    .ruleSets(optRuleSet)
     // Custom cost factory to use during optimization
     .costFactory(null)
     .typeSystem(RelDataTypeSystem.DEFAULT)
     .build()
 
+  // prepare planner and collect context instances
+  val clusters: Array[RelOptCluster] = Array(null)
+  val relOptSchemas: Array[RelOptSchema] = Array(null)
+  val rootSchemas: Array[SchemaPlus] = Array(null)
+  Frameworks.withPlanner(new PlannerAction[Void] {
+    override def apply(
+                        cluster: RelOptCluster,
+                        relOptSchema: RelOptSchema,
+                        rootSchema: SchemaPlus)
+    : Void = {
+      clusters(0) = cluster
+      relOptSchemas(0) = relOptSchema
+      rootSchemas(0) = rootSchema
+      null
+    }
+  })
+
+  val planner2 = clusters(0).getPlanner
+
+
   val planner = Frameworks.getPlanner(calciteFrameworkConfig)
 
-  def generate(rule: Rule) = {
+  def generate(rule: Rule): RelNode = {
     println(s"Rule:\n$rule")
 
     val body = rule.bodyTriplePatterns().map(tp => tp.toTriple).toSet
@@ -79,9 +111,16 @@ class PlanGenerator {
 
     val validatedSqlNode = planner.validate(sqlNode)
 
-    val logicalPlan = planner.rel(validatedSqlNode)
+    val relNode = planner.rel(validatedSqlNode).project()
 
-    logicalPlan.project()
+    // decorrelate
+    val decorPlan = RelDecorrelator.decorrelateQuery(relNode)
+
+    val optProgram = Programs.ofRules(optRuleSet)
+
+    val program = optProgram.run(planner2, decorPlan, relNode.getTraitSet)
+
+    program
   }
 
 
