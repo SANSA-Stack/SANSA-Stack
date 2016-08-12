a1c87c2fc4a79c83c1228de8737d927028c7f28d
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-08-12 13:42:00 +0200

    Need to work on temp tables when executing SQL on a new Dataframe.

--- inference-spark/src/main/scala/org/sansa/inference/spark/rules/plan/PlanExecutorSQL.scala
+++ inference-spark/src/main/scala/org/sansa/inference/spark/rules/plan/PlanExecutorSQL.scala
@@ -13,15 +13,20 @@ class PlanExecutorSQL(sparkSession: SparkSession) extends PlanExecutor[DataFrame
   override val logger = com.typesafe.scalalogging.slf4j.Logger(LoggerFactory.getLogger(this.getClass.getName))
 
   def execute(plan: Plan, graph: RDFGraphDataFrame): RDFGraphDataFrame = {
-
     // generate SQL query
     val sql = plan.toSQL
     logger.info(s"SQL Query:\n$sql")
 
-    // execute the query
-    val results = sparkSession.sql(sql)
-//    println(results.explain(true))
+    val tmpName = "TEMP_TRIPLES"
 
+    // execute the query
+    // we have to register the Dataframe as a temp table on which the SQL query will be executed
+    graph.toDataFrame(sparkSession).createTempView(tmpName)
+    val results = sparkSession.sql(sql.replace("TRIPLES", tmpName))
+    // unregister the temp table
+    sparkSession.sqlContext.dropTempTable(tmpName)
+
+//    println(results.explain(true))
     new RDFGraphDataFrame(results)
   }
 }
