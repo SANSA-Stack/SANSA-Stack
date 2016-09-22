601476b299f2158c0340caf65549211c89962674
Gezim <g.sejdiu@gmail.com>
2016-09-22 15:10:04 +0200

    Clean up the deprecated classes

--- src/main/scala/net/sansa/rdf/spark/graph/LoadGraph.scala
+++ src/main/scala/net/sansa/rdf/spark/graph/LoadGraph.scala
@@ -6,19 +6,23 @@ import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.PairRDDFunctions
+import net.sansa.rdf.spark.model.JenaSparkRDDOps
 
 object LoadGraph extends Logging {
 
-  //var _graph: Graph[String, String] = null
-  //var _graph: Graph[VertexAtt, EdgeAtt] = null
-
   def apply(filePath: String, sparkContext: SparkContext) = {
-    val text = sparkContext.textFile(filePath) //hdfsfile + "/gsejdiu/DistLODStats/Dbpedia/en/geonames_links_en.nt.bz2")
+    /*  val text = sparkContext.textFile(filePath) //hdfsfile + "/gsejdiu/DistLODStats/Dbpedia/en/geonames_links_en.nt.bz2")
       .filter(line => !line.trim().isEmpty & !line.startsWith("#"))
+*/
+    val ops = JenaSparkRDDOps(sparkContext)
+    import ops._
 
-    val rs = text.map(NTriplesParser.parseTriple)
+    val text = fromNTriples(filePath, "http://dbpedia.org")
+    val rs = ops.sparkContext.parallelize((text.map(x =>
+      (x.getSubject.getLiteralLexicalForm, x.getPredicate.getLiteralLexicalForm, x.getObject.getLiteralLexicalForm)).toList))
 
     val indexedmap = (rs.map(_._1) union rs.map(_._3)).distinct.zipWithIndex //indexing
+
     val vertices: RDD[(VertexId, String)] = indexedmap.map(x => (x._2, x._1))
     val _iriToId: RDD[(String, VertexId)] = indexedmap.map(x => (x._1, x._2))
 
@@ -30,12 +34,10 @@ object LoadGraph extends Logging {
       case (k, ((si, p), oi)) => Edge(si, oi, p)
     })
 
-    // TODO is there a specific reason to not return the graph directly? ~ Claus
-    //_graph =
-    Graph(vertices, edges)
+    Graph(null, null)//TODO
 
     new {
-      val graph = Graph(vertices, edges)
+      val graph = Graph(null, null)
       val iriToId = _iriToId
     }
 
