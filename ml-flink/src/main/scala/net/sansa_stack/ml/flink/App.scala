cacebd1c71de43bb6c7adc5eb65c04ce8f5209ba
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-10-27 10:22:55 +0200

    Added new Maven structure.

---
+++ ml-flink/src/main/scala/net/sansa_stack/ml/flink/App.scala
@@ -0,0 +1,15 @@
+package net.sansa_stack.ml.flink
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
