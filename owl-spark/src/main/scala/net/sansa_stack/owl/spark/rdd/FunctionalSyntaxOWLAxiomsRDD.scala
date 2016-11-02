fdf1f752e47c9fd22200adf37e0eceacfde29559
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-11-02 16:22:48 +0100

    Refactored out FS-specific code from RDD code
    Removed functional sytax parsing code from RDD class and now refer to owl-common trait with same functionality.

--- owl-spark/src/main/scala/net/sansa_stack/owl/spark/rdd/FunctionalSyntaxOWLAxiomsRDD.scala
+++ owl-spark/src/main/scala/net/sansa_stack/owl/spark/rdd/FunctionalSyntaxOWLAxiomsRDD.scala
@@ -1,57 +1,18 @@
 package net.sansa_stack.owl.spark.rdd
 
+import net.sansa_stack.owl.common.parsing.FunctionalSyntaxParsing
 import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
 import org.apache.spark.{Partition, SparkContext, TaskContext}
-import org.semanticweb.owlapi.apibinding.OWLManager
-import org.semanticweb.owlapi.functional.parser.OWLFunctionalSyntaxOWLParserFactory
-import org.semanticweb.owlapi.io.{OWLParserException, StringDocumentSource}
+import org.semanticweb.owlapi.io.OWLParserException
 import org.semanticweb.owlapi.model.OWLAxiom
 
 
 class FunctionalSyntaxOWLAxiomsRDD(
                                     @transient val sc: SparkContext,
-                                    parent: OWLExpressionsRDD) extends OWLAxiomsRDD(sc, parent) {
+                                    parent: OWLExpressionsRDD) extends OWLAxiomsRDD(sc, parent) with FunctionalSyntaxParsing {
 
-  private def parser = new OWLFunctionalSyntaxOWLParserFactory().createParser()
-  private def man = OWLManager.createOWLOntologyManager()
-  private def ontConf = man.getOntologyLoaderConfiguration
   @transient private lazy val spark = {
     SparkSession.builder().appName(sc.appName).getOrCreate()
-  }
-
-  /**
-    * Builds a snipped conforming to the OWL functional syntax which then can
-    * be parsed by the OWLAPI functional syntax parser. A single expression,
-    * e.g.
-    *
-    * Declaration(Class(bar:Cls2))
-    *
-    * has thus to be wrapped into an ontology declaration as follows
-    *
-    * Ontology( <http://the.ontology.uri#>
-    * Declaration(Class(bar:Cls2))
-    * )
-    *
-    * @param expression A String containing an expression in OWL functional
-    *                   syntax, e.g. Declaration(Class(bar:Cls2))
-    * @return The parsed axiom or null in case something went wrong during parsing
-    */
-  def makeAxiom(expression: String): OWLAxiom = {
-    val ontStr = "Ontology(<" + parent.ontURI + ">\n"
-    val axStr = ontStr + expression + "\n)"
-
-    val ont = man.createOntology()
-
-    parser.parse(new StringDocumentSource(axStr), ont, ontConf)
-
-    val it = ont.axioms().iterator()
-
-    if (it.hasNext) {
-      it.next()
-    } else {
-      log.warn("No axiom was created for expression " + expression)
-      null
-    }
   }
 
   override def compute(split: Partition, context: TaskContext): Iterator[OWLAxiom] = {
