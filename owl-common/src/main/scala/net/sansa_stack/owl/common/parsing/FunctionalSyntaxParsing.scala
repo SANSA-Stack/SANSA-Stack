a4cea2a95a4128b67ff0d16d61698ef8b20d9ac6
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-11-09 17:27:35 +0100

    Restructured functionality of owl-common
    - Introduced FunctionalSyntaxInputFormat with FunctionalSyntaxRecordReader
    - Split functionality of former FunctionalSyntaxParsing into
      - FunctionalSyntaxParsing
      - FunctionalSyntaxPrefixParsing
      - FunctionalSyntaxExpressionBuilder

--- owl-common/src/main/scala/net/sansa_stack/owl/common/parsing/FunctionalSyntaxParsing.scala
+++ owl-common/src/main/scala/net/sansa_stack/owl/common/parsing/FunctionalSyntaxParsing.scala
@@ -6,19 +6,33 @@ import org.semanticweb.owlapi.functional.parser.OWLFunctionalSyntaxOWLParserFact
 import org.semanticweb.owlapi.io.{OWLParserException, StringDocumentSource}
 import org.semanticweb.owlapi.model.OWLAxiom
 
-import scala.collection.mutable
-import scala.io.{BufferedSource, Source}
+
+/**
+  * Object containing several constants used by the FunctionalSyntaxParsing
+  * trait and the FunctionalSyntaxExpressionBuilder
+  */
+package object constants {
+  /** marker used to store the prefix for the default namespace */
+  val _empty = "_EMPTY_"
+  // TODO: refine
+  val prefixPattern = "Prefix\\(([a-zA-Z]*)\\:=<(.*)>\\)".r
+  val ontologyPattern = "Ontology\\(<(.*)>".r
+}
 
 
+/**
+  * Trait to support the parsing of input OWL files in functional syntax. This
+  * trait mainly defines how to make axioms from input functional syntax
+  * expressions.
+  */
 trait FunctionalSyntaxParsing {
   private val logger = Logger(classOf[FunctionalSyntaxParsing])
-
   private def parser = new OWLFunctionalSyntaxOWLParserFactory().createParser()
   private def man = OWLManager.createOWLOntologyManager()
   private def ontConf = man.getOntologyLoaderConfiguration
 
   /**
-    * Builds a snipped conforming to the OWL functional syntax which then can
+    * Builds a snippet conforming to the OWL functional syntax which then can
     * be parsed by the OWLAPI functional syntax parser. A single expression,
     * e.g.
     *
@@ -36,10 +50,8 @@ trait FunctionalSyntaxParsing {
     */
   @throws(classOf[OWLParserException])
   def makeAxiom(expression: String): OWLAxiom = {
-//    val ontStr = "Ontology(<" + ontURI + ">\n"
     val ontStr = "Ontology(<http://example.com/dummy>\n"
     val axStr = ontStr + expression + "\n)"
-
     val ont = man.createOntology()
 
     parser.parse(new StringDocumentSource(axStr), ont, ontConf)
@@ -57,212 +69,117 @@ trait FunctionalSyntaxParsing {
 
 
 /**
-  * Iterator which
-  * - reads the next functional syntax axiom definition (taking care of
-  *   line breaks in case of multi-line string literals)
-  * - skips the read axiom if it is not a multiple of the split number
-  *   (i.e. axiomNumber % numPartitions != split.index)
-  * - skips all comments and empty lines
-  * - yields a string containing the next axiom (with
-  *   axiomNumber % numPartitions == split.index)
+  * Trait to support the parsing of prefixes from expressions given in
+  * functional syntax.
   */
-class FunctionalSyntaxInputFileIterator(filePath: String,
-                                        numPartitions: Int,
-                                        partitionIndex: Int) extends Iterator[String] {
-
-  val inputFileIt = Source.fromFile(filePath)
-  var ontURI: String = null
+trait FunctionalSyntaxPrefixParsing {
   /**
-    * the axiom counter to only return axioms with
-    * axiomNumber % numPartitions == split.index */
-  var axiomNumber = 0
-  /**
-    * Map containing all read prefixes; required to expand namespace
-    * shortcuts in the returned (functional syntax) axiom string */
-  var prefixes = new mutable.HashMap[String, String]()
-  /**
-    * The extraction of the next functional syntax axiom expression is
-    * triggered by the hasNext method. If there is a functional syntax
-    * axiom expression that could be extracted, it is held in
-    * lastExpression */
-  var lastExpression: String = null
-  // TODO: refine
-  val prefixPattern = "Prefix\\(([a-zA-Z]*)\\:=<(.*)>\\)".r
-  val ontologyPattern = "Ontology\\(<(.*)>".r
-  /** marker used to store default namespace */
-  val _empty = "_EMPTY_"
-
-  override def hasNext: Boolean = {
-    lastExpression = nextExpression()
-    if (lastExpression == null) inputFileIt.close()
-    lastExpression != null
-  }
-
-  override def next(): String = lastExpression
-
-  /**
-    * Tries to extract the next expression from the buffered file input
-    * iterator.
-    *
-    * Main approach:
-    * 1) read until next newline is reached
-    *    - if the quotes read so far are un-balanced, there was a multi-
-    *      line string literal --> read on until next newline is reached
-    *      and check again
-    *    - else expression string is read
-    * 2) check whether the read expression
-    *    a) is an empty line --> skip and go on with 1)
-    *    b) is a prefix declaration --> add it this.prefixes and go on
-    *       with 1)
-    *    c) is an ontology declaration --> get ontology URI and go on
-    *       with 1)
-    *    d) belongs to this partition, i.e.
-    *       axiomNumber % numPartitions == split.index --> yield
-    *       expression; else go on with 1)
-    *
-    * TODO: check parentheses: nested parentheses are not checked so far
-    */
-  private def nextExpression() = {
-    var expression: String = null
-
-    // 1)
-    // we're done if 2d) check is positive or we reached the end of file
-    var notDone = true
-    while (notDone) {
-      expression = ""
-      var foundExpressionEnd = false
-      var nrOfReadQuotesIsBalanced = true
-
-      while (!foundExpressionEnd) {
-        if (!inputFileIt.hasNext)
-          foundExpressionEnd = true
-
-        else {
-          var readChar = inputFileIt.next()
-
-          // keep track of quotes read so far
-          if (readChar == '"') nrOfReadQuotesIsBalanced = !nrOfReadQuotesIsBalanced
-
-          foundExpressionEnd =
-            (readChar == '\n' && nrOfReadQuotesIsBalanced) || !inputFileIt.hasNext
-
-          expression += readChar
-        }
-      }
-
-      // 2) Now one expression is read (possibly with multi-line string literal)
-      // `--> check if the read expression
-      //        a) is an empty line or a comment
-      //        b) is a prefix declaration
-      //        c) is an ontology declaration
-      //        d) belongs to this partition
-
-      // a) empty line or a comment
-      var skipLine =
-        expression.trim() == "" || // empty line
-          expression.trim.startsWith("#") || // comment
-          expression.trim.startsWith(")") // last, outermost closing parenthesis
-
-      if (!skipLine) {
-        if (expression.trim.startsWith("Prefix")) {
-          // b) prefix declaration
-          val (k, v) = parsePrefix(expression.trim())
-          prefixes.put(k, v)
-          skipLine = true
-
-        } else if (expression.trim.startsWith("Ontology")) {
-          // c) ontology declaration
-          ontURI = parseOntologyURI(expression)
-          skipLine = true
-
-        } else if (expression.trim.startsWith("<http")) {
-          // e.g. <http://purl.obolibrary.org/obo/pato/releases/2016-05-22/pato.owl>
-          // as part of an Ontology(...) declaration
-          skipLine = true
-        }
-      }
-
-      // d) check whether expression belongs to this partition
-      if ((axiomNumber % numPartitions == partitionIndex && !skipLine) || !inputFileIt.hasNext) {
-        notDone = false
-      }
-
-      if (!skipLine) axiomNumber += 1
-      else expression = null
-    }
-
-    if (expression != null) {
-      // Expand prefix abbreviations: foo:Bar --> http://foo.com/somePath#Bar
-      for (prefix <- prefixes.keys) {
-        val p = prefix + ":"
-
-        if (expression.contains(p)) {
-          val v: String = "<" + prefixes.get(prefix).get
-          // TODO: refine regex
-          val pattern = (p + "([a-zA-Z][0-9a-zA-Z_-]*)").r
-
-          pattern.findAllIn(expression).foreach(hit => {
-            if (!expression.contains(hit + ">")) {
-              expression = expression.replace(hit, hit + ">")
-            }
-          })
-          expression = expression.replace(p.toCharArray, v.toCharArray)
-        }
-      }
-
-      // handle default prefix e.g. :Bar --> http://foo.com/defaultPath#Bar
-      // TODO: refine regex
-      val pattern = ":[^/][a-zA-Z][0-9a-zA-Z_-]*".r
-      val v: String = "<" + prefixes.get(_empty).get
-
-      if (prefixes.contains(_empty)) {
-        pattern.findAllIn(expression).foreach(hit => {
-          val full = hit.replace(":".toCharArray, v.toCharArray)
-          expression = expression.replace(hit, full + ">")
-        })
-      }
-
-      expression.trim()
-
-    } else {
-      expression  // == null
-    }
-  }
-
-  /**
-    * Parses the prefix declaration of a namespce URI and returns the
+    * Parses the prefix declaration of a namespace URI and returns the
     * pair (prefix, namespace URI)
     *
     * @param prefixLine Sth like
     *                   Prefix(:=<http://purl.obolibrary.org/obo/pato.owl#>) or
     *                   Prefix(dc:=<http://purl.org/dc/elements/1.1/>)
     */
-  private def parsePrefix(prefixLine: String) = {
+  def parsePrefix(prefixLine: String) = {
     var prefix, uri: String = null
 
     prefixLine.trim match {
-      case prefixPattern(p, u) => {
+      case constants.prefixPattern(p, u) => {
         prefix = p
         uri = u
       }
     }
 
-    if (prefix.isEmpty) prefix = _empty
+    if (prefix.isEmpty) prefix = constants._empty
 
     (prefix, uri)
   }
 
-  /**
-    * Parses and returns the URI of an ontology definition line.
-    *
-    * @param ontologyLine Sth like
-    *                     'Ontology(<http://purl.obolibrary.org/obo/pato.owl>'
-    * @return The parsed string containing ontology URI or null of nothing
-    *         was found
-    */
-  private def parseOntologyURI(ontologyLine: String): String = {
-    ontologyLine.trim match {
-      case ontologyPattern(uri) => uri
+  def isPrefixDeclaration(expression: String): Boolean = {
+    constants.prefixPattern.pattern.matcher(expression).matches()
+  }
+}
+
+
+/**
+  * The main purpose of this class is to provide an object which is initialized
+  * with
+  *
+  * - all prefix declarations
+  *
+  * and, fed with an expression in functional syntax, returns expressions in
+  * functional syntax, with
+  *
+  * - all prefix URIs being extended
+  * - all comments removed
+  * - all non-axiom expressions (e.g. Ontology(...), Prefix(...)) removed
+  *
+  * 'Removed' here means that a null value is returned discarding the input
+  * string.
+  *
+  * @param prefixes Map containing all read prefixes; required to expand
+  *                 namespace shortcuts in the cleaned (functional syntax)
+  *                 axiom string
+  */
+class FunctionalSyntaxExpressionBuilder(val prefixes: Map[String, String]) extends Serializable {
+  def clean(expression: String): String = {
+    var trimmedExpr = expression.trim
+
+    /* Throw away expressions that are of no use for further processing:
+     * 1) empty lines
+     * 2) comments
+     * 3) the last, outermost closing parenthesis
+     * 4) prefix declaration
+     * 5) ontology declaration
+     * 6) URL being part of the ontology declaration, e.g.
+     *    <http://purl.obolibrary.org/obo/pato/releases/2016-05-22/pato.owl>
+     *    being part of Ontology(...)
+     */
+    val discardExpression: Boolean =
+      trimmedExpr.isEmpty  || // 1)
+      trimmedExpr.startsWith("#") ||  // 2)
+      trimmedExpr.startsWith(")") ||  // 3)
+      trimmedExpr.startsWith("Prefix(") ||  // 4)
+      trimmedExpr.startsWith("Ontology(") ||  // 5)
+      trimmedExpr.startsWith("<http") // 6
+
+    if (discardExpression)
+      null
+
+    else {
+      // Expand prefix abbreviations: foo:Bar --> http://foo.com/somePath#Bar
+      for (prefix <- prefixes.keys) {
+        val p = prefix + ":"
+
+        if (trimmedExpr.contains(p)) {
+          val v: String = "<" + prefixes.get(prefix).get
+          // TODO: refine regex
+          val pattern = (p + "([a-zA-Z][0-9a-zA-Z_-]*)").r
+
+          pattern.findAllIn(trimmedExpr).foreach(hit => {
+            if (!trimmedExpr.contains(hit + ">")) {
+              trimmedExpr = trimmedExpr.replace(hit, hit + ">")
+            }
+          })
+          trimmedExpr = trimmedExpr.replace(p.toCharArray, v.toCharArray)
+        }
+      }
+
+      // handle default prefix e.g. :Bar --> http://foo.com/defaultPath#Bar
+      // TODO: refine regex
+      val pattern = ":[^/][a-zA-Z][0-9a-zA-Z_-]*".r
+      val v: String = "<" + prefixes.get(constants._empty).get
+
+      if (prefixes.contains(constants._empty)) {
+        pattern.findAllIn(trimmedExpr).foreach(hit => {
+          val full = hit.replace(":".toCharArray, v.toCharArray)
+          trimmedExpr = trimmedExpr.replace(hit, full + ">")
+        })
+      }
+
+      trimmedExpr
     }
   }
 }
+
