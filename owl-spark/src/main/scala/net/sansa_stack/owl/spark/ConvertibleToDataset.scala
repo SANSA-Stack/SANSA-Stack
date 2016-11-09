3b8ba5268736891f97ae77a4811cdb1729e98a6a
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-11-09 17:34:32 +0100

    Cleanup
    Removed obsolete files and custom RDD implementations that will be replaced

--- owl-spark/src/main/scala/net/sansa_stack/owl/spark/ConvertibleToDataset.scala
+++
@@ -1,8 +0,0 @@
-package net.sansa_stack.owl.spark
-
-import org.apache.spark.sql.Dataset
-
-
-trait ConvertibleToDataset[T] {
-  def asDataset: Dataset[T]
-}
---
+++ owl-spark/src/main/scala/net/sansa_stack/owl/spark/PackageDeclarations.scala
@@ -0,0 +1,16 @@
+package net.sansa_stack.owl.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Dataset
+import org.semanticweb.owlapi.model.OWLAxiom
+
+
+package object rdd {
+  type OWLExpressionsRDD = RDD[String]
+  type OWLAxiomsRDD = RDD[OWLAxiom]
+}
+
+package object dataset {
+  type OWLExpressionsDataset = Dataset[String]
+  type OWLAxiomsDataset = Dataset[OWLAxiom]
+}
