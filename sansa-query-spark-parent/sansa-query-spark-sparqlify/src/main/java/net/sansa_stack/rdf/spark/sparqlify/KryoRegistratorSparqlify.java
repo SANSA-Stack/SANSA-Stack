d27c3058c9b24d0972afd1d2dcfbdb35bad0bd14
Claus Stadler <RavenArkadon@googlemail.com>
2016-11-21 20:45:36 +0100

    Module compiling again - yet some class conflict...

---
+++ sansa-query-spark-parent/sansa-query-spark-sparqlify/src/main/java/net/sansa_stack/rdf/spark/sparqlify/KryoRegistratorSparqlify.java
@@ -0,0 +1,18 @@
+package net.sansa_stack.rdf.spark.sparqlify;
+
+import org.apache.spark.serializer.KryoRegistrator;
+
+import com.esotericsoftware.kryo.Kryo;
+
+import net.sansa_stack.rdf.spark.io.RestrictedExprSerializer;
+
+public class KryoRegistratorSparqlify
+	implements KryoRegistrator
+{
+
+	@Override
+	public void registerClasses(Kryo kryo) {
+		kryo.register(org.aksw.jena_sparql_api.views.RestrictedExpr.class, new RestrictedExprSerializer());
+	}
+
+}
