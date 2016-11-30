d3c8f929021be6922cea5fa8f1535a0abc8ed2f2
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-11-30 21:36:38 +0100

    Refactoring in Flink Man. syntax exprs. builder

--- sansa-owl-flink/src/main/scala/net/sansa_stack/owl/flink/dataset/ManchesterSyntaxOWLExpressionsDataSetBuilder.scala
+++ sansa-owl-flink/src/main/scala/net/sansa_stack/owl/flink/dataset/ManchesterSyntaxOWLExpressionsDataSetBuilder.scala
@@ -8,6 +8,12 @@ import org.apache.hadoop.io.{LongWritable, Text}
 
 object ManchesterSyntaxOWLExpressionsDataSetBuilder extends  ManchesterSyntaxPrefixParsing {
   def build(env: ExecutionEnvironment, filePath: String): OWLExpressionsDataSet = {
+    buildAndGetPrefixes(env, filePath)._1
+  }
+
+  private[dataset] def buildAndGetPrefixes(env: ExecutionEnvironment,
+      filePath: String): (OWLExpressionsDataSet, Map[String, String]) = {
+
     import org.apache.flink.api.scala._
     val hadoopDataSet: DataSet[(LongWritable, Text)] =
       env.readHadoopFile[LongWritable, Text](
@@ -23,6 +29,6 @@ object ManchesterSyntaxOWLExpressionsDataSetBuilder extends  ManchesterSyntaxPre
 
     val builder = new ManchesterSyntaxExpressionBuilder(prefixes)
 
-    rawDataSet.map(builder.clean(_)).filter(_ != null)
+    (rawDataSet.map(builder.clean(_)).filter(_ != null), prefixes)
   }
 }
