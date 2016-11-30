859b747ba73b7c47b75e29cd91e4434565a99708
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-11-30 21:12:08 +0100

    Extended testing for Manch. syntax expr. parsing

--- sansa-owl-spark/src/test/scala/net/sansa_stack/owl/spark/dataset/ManchesterSyntaxOWLExpressionsDatasetBuilderTest.scala
+++ sansa-owl-spark/src/test/scala/net/sansa_stack/owl/spark/dataset/ManchesterSyntaxOWLExpressionsDatasetBuilderTest.scala
@@ -19,21 +19,102 @@ class ManchesterSyntaxOWLExpressionsDatasetBuilderTest extends FunSuite with Sha
     _dataset
   }
 
-  test("There should be an annotations frame comprising three annotations with full URIs") {
-    val res = dataset.filter(frame => frame.startsWith("Annotations:")).collect()
-    val expected = "Annotations: \n" +
-      "    <http://ex.com/bar#hasTitle> \"Title\",\n" +
-      "    description \"A longer\n" +
-      "description running over\n" +
-      "several lines\",\n" +
-      "    <http://ex.com/foo#hasName> \"Name\""
-
-    assert(res.length == 1)
-    assert(res(0).trim == expected)
+  test("The total number of frames should be correct") {
+    val total = 49
+    assert(dataset.count() == total)
   }
 
-  test("The total number of frames should be correct") {
-    val total = 50
-    assert(dataset.count() == total)
+  test("The number of Class frames should be correct") {
+    val expectedNumClassFrames = 21
+    val actualNumClassFrames = dataset.filter(_.trim.startsWith("Class:")).count()
+    assert(actualNumClassFrames == expectedNumClassFrames)
+  }
+
+  test("The number of AnnotationProperty frames should be correct") {
+    val expectedNumAnnoPropertyFrames = 7
+    val actualNumAnnoPropertyFrames =
+      dataset.filter(_.trim.startsWith("AnnotationProperty:")).count()
+
+    assert(actualNumAnnoPropertyFrames == expectedNumAnnoPropertyFrames)
+  }
+
+  test("The number of ObjectProperty frames should be correct") {
+    val expectedNumObjPropertyFrames = 7
+    val actualNumObjPropertyFrames =
+      dataset.filter(_.trim.startsWith("ObjectProperty:")).count()
+
+    assert(actualNumObjPropertyFrames == expectedNumObjPropertyFrames)
+  }
+
+  test("The number of DataProperty frames should be correct") {
+    val expectedNumDataPropertyFrames = 4
+    val actualNumDataPropertyFrames =
+      dataset.filter(_.trim.startsWith("DataProperty:")).count()
+
+    assert(actualNumDataPropertyFrames == expectedNumDataPropertyFrames)
+  }
+
+  test("The number of Individual frames should be correct") {
+    val expectedNumIndividualFrames = 3
+    val actualNumIndividualFrames =
+      dataset.filter(_.trim.startsWith("Individual:")).count()
+
+    assert(actualNumIndividualFrames == expectedNumIndividualFrames)
+  }
+
+  test("The number of DisjointClasses frames should be correct") {
+    val expectedNumDisjointClassesFrames = 0
+    val actualNumDisjointClassesFrames =
+      dataset.filter(_.trim.startsWith("DisjointClasses:")).count()
+
+    assert(actualNumDisjointClassesFrames == expectedNumDisjointClassesFrames)
+  }
+
+  test("The number of EquivalentClasses frames should be correct") {
+    val expectedNumEquivalentClassesFrames = 0
+    val actualNumEquivalentClassesFrames =
+      dataset.filter(_.trim.startsWith("EquivalentClasses:")).count()
+
+    assert(actualNumEquivalentClassesFrames == expectedNumEquivalentClassesFrames)
+  }
+
+  test("The number of EquivalentProperties frames should be correct") {
+    val expectedNumEquivalentProperties = 0
+    val actualNumEquivalentProperties =
+      dataset.filter(_.trim.startsWith("EquivalentProperties:")).count()
+
+    assert(actualNumEquivalentProperties == expectedNumEquivalentProperties)
+  }
+
+  test("The number of DisjointProperties frames should be correct") {
+    val expectedNumDisjointProperties = 0
+    val actualNumDisjointProperties =
+      dataset.filter(_.trim.startsWith("DisjointProperties:")).count()
+
+    assert(actualNumDisjointProperties == expectedNumDisjointProperties)
+  }
+
+  test("The number of SameIndividual frames should be correct") {
+    val expectedNumSameIndividualFrames = 0
+    val actualNumSameIndividualFrames =
+      dataset.filter(_.trim.startsWith("SameIndividual:")).count()
+
+    assert(actualNumSameIndividualFrames == expectedNumSameIndividualFrames)
+  }
+
+  test("The number of DifferentIndividuals frames should be correct") {
+    val expectedNumDifferentIndividualsFrames = 0
+    val actualNumDifferentIndividualsFrame =
+      dataset.filter(_.trim.startsWith("DifferentIndividuals:")).count()
+
+    assert(actualNumDifferentIndividualsFrame == expectedNumDifferentIndividualsFrames)
+  }
+
+  test("The number of Datatype frames should be correct") {
+    val expectedNumDatatypeFrames = 7
+    val actualNumDatatypeFrames =
+      dataset.filter(_.trim.startsWith("Datatype:")).count()
+
+    assert(actualNumDatatypeFrames == expectedNumDatatypeFrames)
   }
 }
