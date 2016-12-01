1384a75406d43ac6166233177b87cd1fa3cb7022
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-12-01 14:35:20 +0100

    Added Flink OWLReaderDataSet example

---
+++ sansa-examples-flink/src/main/scala/net/sansa_stack/examples/flink/owl/OWLReaderDataSet.scala
@@ -0,0 +1,66 @@
+package net.sansa_stack.examples.flink.owl
+
+
+import net.sansa_stack.owl.flink.dataset.{FunctionalSyntaxOWLAxiomsDataSetBuilder, ManchesterSyntaxOWLAxiomsDataSetBuilder}
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+import scala.collection.mutable
+
+object OWLReaderDataSet {
+  def main(args: Array[String]) = {
+    if (args.length < 1) {
+      System.err.println(
+        "Usage: Dataset OWL reader <input> <syntax>")
+      System.err.println("Supported 'Syntax' as follows:")
+      System.err.println("  fun               Functional syntax")
+      System.err.println("  manch             Manchester syntax")
+      System.err.println("  owl_xml           OWL/XML")
+      System.exit(1)
+    }
+    val input = args(0)
+    val syntax = args(1)
+    val optionsList = args.drop(2).map { arg =>
+      arg.dropWhile(_ == '-').split('=') match {
+        case Array(opt, v) => (opt -> v)
+        case _             => throw new IllegalArgumentException("Invalid argument: " + arg)
+      }
+    }
+    val options = mutable.Map(optionsList: _*)
+
+    syntax match {
+      case "fun" =>
+        options.foreach {
+          case (opt, _) => throw new IllegalArgumentException("Invalid option: " + opt)
+        }
+
+        println(".================================================.")
+        println("| Dataset OWL reader example (Functional syntax) |")
+        println("`================================================´")
+
+        val env = ExecutionEnvironment.getExecutionEnvironment
+
+        val dataset = FunctionalSyntaxOWLAxiomsDataSetBuilder.build(env, input)
+        dataset.first(10).print()
+
+      case "manch" =>
+        options.foreach {
+          case (opt, _) => throw new IllegalArgumentException("Invalid option: " + opt)
+        }
+
+        println(".================================================.")
+        println("| Dataset OWL reader example (Manchester syntax) |")
+        println("`================================================´")
+
+        val env = ExecutionEnvironment.getExecutionEnvironment
+
+        val rdd = ManchesterSyntaxOWLAxiomsDataSetBuilder.build(env, input)
+        rdd.first(10).print()
+
+      case "owl_xml" =>
+        println("Not supported, yet.")
+
+      case _ =>
+        println("Invalid syntax type.")
+    }
+  }
+}
