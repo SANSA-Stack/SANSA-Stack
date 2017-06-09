b459982b6c42f0ff0cc68bde8a8fce07babed608
Simon Bin <sbin@informatik.uni-leipzig.de>
2017-06-09 15:38:55 +0200

    fix partitioner version

--- sansa-rdf-flink/src/main/scala/net/sansa_stack/rdf/flink/partition/core/RdfPartitionUtilsFlink.scala
+++ sansa-rdf-flink/src/main/scala/net/sansa_stack/rdf/flink/partition/core/RdfPartitionUtilsFlink.scala
@@ -13,21 +13,24 @@ import org.apache.flink.types.Row
 
 object RdfPartitionUtilsFlink extends Serializable {
 
-  implicit def partitionGraph[T <: RdfPartition: TypeInformation: ClassTag](graphRdd: DataSet[Triple], partitioner: RdfPartitioner[T] = RdfPartitionerDefault): Map[T, DataSet[Row]] = {
+  implicit def partitionGraph[T <: RdfPartition: TypeInformation: ClassTag](graphRdd: DataSet[Triple], partitioner: RdfPartitioner[T] = RdfPartitionerDefault): Map[T, DataSet[Product]] = {
     val map = Map(partitionGraphArray(graphRdd, partitioner): _*)
     map
   }
 
-  implicit def partitionGraphArray[T <: RdfPartition: TypeInformation: ClassTag](graphRdd: DataSet[Triple], partitioner: RdfPartitioner[T] = RdfPartitionerDefault): Seq[(T, DataSet[Row])] = {
+  implicit def partitionGraphArray[T <: RdfPartition: TypeInformation: ClassTag](graphRdd: DataSet[Triple], partitioner: RdfPartitioner[T] = RdfPartitionerDefault): Seq[(T, DataSet[Product])] = {
     val partitions = graphRdd.map(x => partitioner.fromTriple(x)).distinct.collect
     val array = partitions map { p =>
       (
         p,
         graphRdd
         .filter(t => p.matches(t))
-        .map(t => new Row(p.layout.fromTriple(t).productArity)))
-      //.map(t => new Row(p.layout.fromTriple(t).productIterator.toList: _*))
+        //.map(t => new Row(p.layout.fromTriple(t).productArity)))
+      .map(t => p.layout.fromTriple(t)
       //.persist())
+        )
+      )
+
     }
     array
   }
