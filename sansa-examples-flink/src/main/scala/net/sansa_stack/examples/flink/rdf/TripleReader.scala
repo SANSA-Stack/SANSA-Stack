5e4a9c06404f89de2666cdc93506425fbef0704b
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-06-09 18:35:51 +0200

    Added TripleReader example for Flink

---
+++ sansa-examples-flink/src/main/scala/net/sansa_stack/examples/flink/rdf/TripleReader.scala
@@ -0,0 +1,36 @@
+package net.sansa_stack.examples.flink.rdf
+
+import scala.collection.mutable
+import org.apache.flink.api.scala.ExecutionEnvironment
+import net.sansa_stack.inference.flink.data.RDFGraphLoader
+
+object TripleReader {
+ def main(args: Array[String]) {
+    if (args.length < 1) {
+      System.err.println(
+        "Usage: Triple reader <input>")
+      System.exit(1)
+    }
+    val input = args(0)
+    val optionsList = args.drop(1).map { arg =>
+      arg.dropWhile(_ == '-').split('=') match {
+        case Array(opt, v) => (opt -> v)
+        case _             => throw new IllegalArgumentException("Invalid argument: " + arg)
+      }
+    }
+    val options = mutable.Map(optionsList: _*)
+
+    options.foreach {
+      case (opt, _) => throw new IllegalArgumentException("Invalid option: " + opt)
+    }
+    println("======================================")
+    println("|        Triple reader example       |")
+    println("======================================")
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val rdfgraph = RDFGraphLoader.loadFromFile(input, env)
+
+    rdfgraph.triples.first(10).print()
+  }
+}
