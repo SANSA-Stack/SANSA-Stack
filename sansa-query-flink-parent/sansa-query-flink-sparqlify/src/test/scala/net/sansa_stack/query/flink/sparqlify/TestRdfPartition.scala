c29a8dae28e07cb8ba00ea9ac48387920ca76452
Simon Bin <sbin@informatik.uni-leipzig.de>
2017-06-09 16:44:50 +0200

    test sql layer

--- sansa-query-flink-parent/sansa-query-flink-sparqlify/src/test/scala/net/sansa_stack/query/flink/sparqlify/TestRdfPartition.scala
+++ sansa-query-flink-parent/sansa-query-flink-sparqlify/src/test/scala/net/sansa_stack/query/flink/sparqlify/TestRdfPartition.scala
@@ -10,20 +10,40 @@ import net.sansa_stack.rdf.flink.model.RDFTriple
 
 import scala.collection.JavaConverters._
 import net.sansa_stack.rdf.flink.partition.core.RdfPartitionUtilsFlink
+import net.sansa_stack.rdf.partition.sparqlify.SparqlifyUtils2
 import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment, _}
+import org.apache.flink.table.api.TableEnvironment
+import org.apache.flink.table.api.scala._
+import org.apache.flink.types.Row
 import org.apache.jena.query.ResultSetFormatter
 
 class TestRdfPartition extends FlatSpec {
 
   "A partitioner" should "support custom datatypes" in {
     val env = ExecutionEnvironment.getExecutionEnvironment
+    val tblEnv = TableEnvironment.getTableEnvironment(env)
     val triples = RDFDataMgr.createIteratorTriples(getClass.getResourceAsStream("/dbpedia-01.nt"), Lang.NTRIPLES, null).asScala
         //.map(t => RDFTriple(t.getSubject, t.getPredicate, t.getObject))
       .toList
     val ds: DataSet[Triple] = env.fromCollection(triples)
     val partition = RdfPartitionUtilsFlink.partitionGraph(ds)
-    partition.foreach(u => print(u._1))
-    ds.print()
+    val tables = partition.foreach { case (p, ds) => {
+      println(p)
+      val vd = SparqlifyUtils2.createViewDefinition(p)
+      println(vd.getName)
+      tblEnv.registerDataSet(vd.getName, ds)
+      (p, vd.getName)
+    }
+    }
+    val res = tblEnv.sql(
+      "SELECT * FROM deathPlace"
+    )
+    res.toDataSet[Row].print()
+    //println(tblEnv.explain(res))
+
+
+
+    //ds.print()
     //env.execute()
   }
 }
