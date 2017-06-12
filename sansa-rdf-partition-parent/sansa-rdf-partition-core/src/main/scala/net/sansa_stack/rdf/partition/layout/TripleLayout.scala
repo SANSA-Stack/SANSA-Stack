c68c6557d052acbb5d980c4a084e4d319bed2ff7
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-06-12 16:12:04 +0200

    removed unused dep

--- sansa-rdf-partition-parent/sansa-rdf-partition-core/src/main/scala/net/sansa_stack/rdf/partition/layout/TripleLayout.scala
+++ sansa-rdf-partition-parent/sansa-rdf-partition-core/src/main/scala/net/sansa_stack/rdf/partition/layout/TripleLayout.scala
@@ -3,8 +3,6 @@ package net.sansa_stack.rdf.partition.layout
 import scala.reflect.runtime.universe.Type
 
 import org.apache.jena.graph.Triple
-import org.aksw.jena_sparql_api.views.E_Triple
-
 
 trait TripleLayout {
   def schema: Type
