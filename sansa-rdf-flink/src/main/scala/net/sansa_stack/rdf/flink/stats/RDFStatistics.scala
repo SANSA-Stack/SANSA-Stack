92a4853a75ff5c3b602135cd8bf05d167b302508
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-06-12 13:48:44 +0200

    Added PropertiesDefined and PropertyUsage stats

--- sansa-rdf-flink/src/main/scala/net/sansa_stack/rdf/flink/stats/RDFStatistics.scala
+++ sansa-rdf-flink/src/main/scala/net/sansa_stack/rdf/flink/stats/RDFStatistics.scala
@@ -72,3 +72,60 @@ object Classes_Defined {
 
   def apply(rdfgraph: RDFGraph, env: ExecutionEnvironment)= new Classes_Defined(rdfgraph, env).Voidify()
 }
+
+class PropertiesDefined(rdfgraph: RDFGraph, env: ExecutionEnvironment) extends Serializable with Logging {
+
+  def Filter() = rdfgraph.triples.filter(f =>
+    (f.getPredicate.toString().equals(StatsPrefixes.RDF_TYPE) && f.getObject.toString().equals(StatsPrefixes.OWL_OBJECT_PROPERTY))
+      || (f.getPredicate.toString().equals(StatsPrefixes.RDF_TYPE) && f.getObject.toString().equals(StatsPrefixes.RDF_PROPERTY))
+      && !f.getSubject.isURI())
+  def Action() = Filter().map(_.getPredicate).distinct()
+
+  def PostProc() = Action().count()
+
+  def Voidify() = {
+    var cd = new Array[String](1)
+    cd(0) = "\nvoid:properties  " + PostProc() + ";"
+    env.fromCollection(cd)
+  }
+}
+object PropertiesDefined {
+
+  def apply(rdfgraph: RDFGraph, env: ExecutionEnvironment) = new PropertiesDefined(rdfgraph, env).Voidify()
+}
+
+class PropertyUsage(rdfgraph: RDFGraph, env: ExecutionEnvironment) extends Serializable with Logging {
+
+  def Filter() = rdfgraph.triples
+
+  //M[?p]++
+  def Action() = Filter().map(_.getPredicate)
+    .map(f => (f, 1))
+    .groupBy(0)
+    .sum(1)
+
+  //top(M,100)
+  def PostProc() = Action().collect().sortBy(_._2).take(100)
+
+  def Voidify() = {
+
+    var triplesString = new Array[String](1)
+    triplesString(0) = "\nvoid:propertyPartition "
+
+    val properties =  env.fromCollection(PostProc())
+    val vp = properties.map(t => "[ \nvoid:property " + "<" + t._1 + ">; \nvoid:triples " + t._2 + ";\n], ")
+
+    var pl_a = new Array[String](1)
+    pl_a(0) = "\nvoid:properties " + Action().map(f => f._1).distinct().count
+    val c_p =  env.fromCollection(triplesString)
+    val p =  env.fromCollection(pl_a)
+    p.union(c_p).union(vp)
+  }
+}
+object PropertyUsage {
+
+  def apply(rdfgraph: RDFGraph, env: ExecutionEnvironment)  = new PropertyUsage(rdfgraph, env).Voidify()
+
+}
+
+
