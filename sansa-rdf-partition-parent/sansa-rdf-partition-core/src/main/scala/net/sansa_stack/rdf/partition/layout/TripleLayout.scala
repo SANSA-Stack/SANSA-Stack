1c06ca46129041efb1771303fb295f42bcd9a084
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-06-12 16:02:44 +0200

    towards making the views working with both spark and flink

--- sansa-rdf-partition-parent/sansa-rdf-partition-core/src/main/scala/net/sansa_stack/rdf/partition/layout/TripleLayout.scala
+++ sansa-rdf-partition-parent/sansa-rdf-partition-core/src/main/scala/net/sansa_stack/rdf/partition/layout/TripleLayout.scala
@@ -3,6 +3,7 @@ package net.sansa_stack.rdf.partition.layout
 import scala.reflect.runtime.universe.Type
 
 import org.apache.jena.graph.Triple
+import org.aksw.jena_sparql_api.views.E_Triple
 
 
 trait TripleLayout {
