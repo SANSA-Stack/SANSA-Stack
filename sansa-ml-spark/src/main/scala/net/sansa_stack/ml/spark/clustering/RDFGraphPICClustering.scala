64a0eef37e8c16538cf9dd949f0adbb3fc3f7ce0
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-06-26 11:04:08 +0200

    Remove build error ( MathUtils.log => math.log)

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/clustering/RDFGraphPICClustering.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/clustering/RDFGraphPICClustering.scala
@@ -71,73 +71,66 @@ class RDFGraphPICClustering(@transient val sparkSession: SparkSession,
     // Logarithm base 2 
     val LOG2 = math.log(2)
     val log2 = { x: Double => math.log(x) / LOG2 }
-	  
-	  
-	  val vertexCount = vertices.count()
-	  
-	  
-	  // Similarity based on Information Theory
-	  
-	  
-     var icc = 0.0
-    val logC = MathUtils.log(10.0, vertexCount.toDouble)
- 
-     def informationContent(a: Long): Double = {
+
+    val vertexCount = vertices.count()
+
+    // Similarity based on Information Theory
+
+    var icc = 0.0
+    //val logC = MathUtils.log(10.0, vertexCount.toDouble)
+    val logC = math.log(vertexCount.toDouble)
+
+    def informationContent(a: Long): Double = {
       if (a == 0) { return 0.0 }
-       1 - (MathUtils.log(10.0, a.toDouble) / logC)
- 
-     }
-     
-	  def sigmaIc(a : List[Long]) : Double ={
-   var sigma = 0.0
-    for(k <- 0 until a.length) yield{
-      val d = neighbors.lookup(a(k)).distinct.head.toSet
-      val s = informationContent(d.size.toLong)
-      sigma = sigma.+(s)
-      
-      
+      //      1 - (MathUtils.log(10.0, a.toDouble) / logC)
+      1 - (math.log(a.toDouble) / logC)
+
     }
-   
-   sigma
-   
-   
- } 
- 
-     def ic(a: Long): Double = {
-       val d = neighbors.lookup(a).distinct.head.toSet
-       if (d.isEmpty) { return 0.0 }
+
+    def sigmaIc(a: List[Long]): Double = {
+      var sigma = 0.0
+      for (k <- 0 until a.length) yield {
+        val d = neighbors.lookup(a(k)).distinct.head.toSet
+        val s = informationContent(d.size.toLong)
+        sigma = sigma.+(s)
+
+      }
+
+      sigma
+
+    }
+
+    def ic(a: Long): Double = {
+      val d = neighbors.lookup(a).distinct.head.toSet
+      if (d.isEmpty) { return 0.0 }
       else {
- 
-         return sigmaIc(d.toList)
-       }
-     }
-     
-	  
-	  
-	
+
+        return sigmaIc(d.toList)
+      }
+    }
+
     def mostICA(a: Long, b: Long): Double = {
- 
-       val an = neighbors.lookup(a).distinct.head.toSet
-       val an1 = neighbors.lookup(b).distinct.head.toSet
-       if (an.isEmpty || an1.isEmpty) { return 0.0 }
-       val commonNeighbor = an.intersect(an1).toArray
-      
+
+      val an = neighbors.lookup(a).distinct.head.toSet
+      val an1 = neighbors.lookup(b).distinct.head.toSet
+      if (an.isEmpty || an1.isEmpty) { return 0.0 }
+      val commonNeighbor = an.intersect(an1).toArray
+
       if (commonNeighbor.isEmpty) { return 0.0 }
       else {
         return sigmaIc(commonNeighbor.toList)
       }
- 
-     }
-  
-    
+
+    }
+
     /*
 			 * Lin similarity measure 
-			 */ 
-     def simLin(e: Long, d: Long): Double = {
+			 */
+    def simLin(e: Long, d: Long): Double = {
       if (ic(e) > 0.0 || ic(d) > 0.0) {
-         (2.0.abs * (mostICA(e, d)).abs) / (ic(e).abs + ic(d).abs)
-       } else { return 0.0 }
-     }
+        (2.0.abs * (mostICA(e, d)).abs) / (ic(e).abs + ic(d).abs)
+      } else { return 0.0 }
+    }
 
     /*
 			 * Jaccard similarity measure 
