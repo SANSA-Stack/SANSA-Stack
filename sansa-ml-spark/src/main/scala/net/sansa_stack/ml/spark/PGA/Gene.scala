dfe94072d12fff141d8081686292ceac413ed520
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-06-30 13:48:18 +0200

    Typo on ../PGA.Gene.scala class

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/PGA/Gene.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/PGA/Gene.scala
@@ -1,4 +1,4 @@
-Class Gene[T](allele: T){
-val Allele:T=allele
-def Mutate[T](newAllele: T) = new Gene(newAllele)
+class Gene[T](allele: T) {
+  val Allele: T = allele
+  def Mutate[T](newAllele: T) = new Gene(newAllele)
 }
