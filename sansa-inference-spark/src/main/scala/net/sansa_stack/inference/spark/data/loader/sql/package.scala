51762b4fcb41a40271ac821ef95c1c9da73f6e19
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2017-07-06 09:22:44 +0200

    Turtle loader to DataFrame

--- sansa-inference-spark/src/main/scala/net/sansa_stack/inference/spark/data/loader/sql/package.scala
+++ sansa-inference-spark/src/main/scala/net/sansa_stack/inference/spark/data/loader/sql/package.scala
@@ -1,21 +1,42 @@
 package net.sansa_stack.inference.spark.data.loader.sql
 
+import org.apache.jena.riot.Lang
 import org.apache.spark.sql.{DataFrame, DataFrameReader, DataFrameWriter}
 
-package object ntriples {
+package object rdf {
+
   /**
-    * Adds a method, `ntriples`, to DataFrameWriter that allows you to write N-Triples files using
+    * Adds methods, `ntriples` and `turtle`, to DataFrameWriter that allows to write N-Triples and Turtle files using
     * the `DataFrameWriter`
     */
-  implicit class NTriplesDataFrameWriter[T](writer: DataFrameWriter[T]) {
+  implicit class RDFDataFrameWriter[T](writer: DataFrameWriter[T]) {
     def ntriples: String => Unit = writer.format("ntriples").save
   }
 
   /**
-    * Adds a method, `ntriples`, to DataFrameReader that allows you to read n-Triples files using
+    * Adds methods, `rdf`, `ntriples` and `turtle`, to DataFrameReader that allows to read N-Triples and Turtle files using
     * the `DataFrameReader`
     */
-  implicit class NTriplesDataFrameReader(reader: DataFrameReader) {
+  implicit class RDFDataFrameReader(reader: DataFrameReader) {
+    /**
+      * Load RDF data into a `DataFrame`. Currently, only N-Triples and Turtle syntax are supported
+      * @param lang the RDF language (Turtle or N-Triples)
+      * @return a `DataFrame[(String, String, String)]`
+      */
+    def rdf(lang: Lang): String => DataFrame = lang match {
+      case i if lang == Lang.NTRIPLES => ntriples
+      case j if lang == Lang.TURTLE => turtle
+      case _ => throw new IllegalArgumentException(s"${lang.getLabel} syntax not supported yet!")
+    }
+    /**
+      * Load RDF data in N-Triples syntax into a `DataFrame` with columns `s`, `p`, and `o`.
+      * @return a `DataFrame[(String, String, String)]`
+      */
     def ntriples: String => DataFrame = reader.format("ntriples").load
+    /**
+      * Load RDF data in Turtle syntax into a `DataFrame` with columns `s`, `p`, and `o`.
+      * @return a `DataFrame[(String, String, String)]`
+      */
+    def turtle: String => DataFrame = reader.format("turtle").load
   }
 }
