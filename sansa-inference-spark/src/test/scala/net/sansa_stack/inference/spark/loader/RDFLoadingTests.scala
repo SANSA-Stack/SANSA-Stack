a13f93c32615026894cd07e3ab781c8f7fd0b8da
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2017-07-11 13:07:50 +0200

    Turtle loading test

--- sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/loader/RDFLoadingTests.scala
+++ sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/loader/RDFLoadingTests.scala
@@ -1,19 +1,19 @@
 package net.sansa_stack.inference.spark.loader
 
-import scala.io.Source
-
-import com.holdenkarau.spark.testing.{DataFrameSuiteBase, SharedSparkContext}
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.apache.jena.riot.Lang
 import org.scalatest.FunSuite
 
 /**
+  * Tests for loading triples from either N-Triples are Turtle files into a DataFrame.
+  *
   * @author Lorenz Buehmann
   */
 class RDFLoadingTests extends FunSuite with DataFrameSuiteBase {
 
   import net.sansa_stack.inference.spark.data.loader.sql.rdf._
 
-  test("loading N-Triples from disk into DataFrame") {
+  test("loading N-Triples file into DataFrame with REGEX parsing mode should result in 9 triples") {
     val sqlCtx = sqlContext
 
     val path = getClass.getResource("/loader/data.nt").getPath
@@ -22,7 +22,19 @@ class RDFLoadingTests extends FunSuite with DataFrameSuiteBase {
     val triples = sqlCtx.read.rdf(lang)(path)
 
     val cnt = triples.count()
-    println(cnt)
+    assert(cnt == 9)
+  }
+
+  test("loading Turtle file into DataFrame should result in 12 triples") {
+    val sqlCtx = sqlContext
+
+    val path = getClass.getResource("/loader/data.ttl").getPath
+    val lang: Lang = Lang.TURTLE
+
+    val triples = sqlCtx.read.rdf(lang)(path)
+
+    val cnt = triples.count()
+    assert(cnt == 12)
   }
 
 }
