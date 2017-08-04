1dc09c7392437e70a05e96c5362b9fa68f31890b
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2017-08-04 20:34:08 +0200

    Started naive backward chaining reasoner.

--- sansa-inference-common/src/main/scala/net/sansa_stack/inference/rules/plan/SimplePlanGenerator.scala
+++ sansa-inference-common/src/main/scala/net/sansa_stack/inference/rules/plan/SimplePlanGenerator.scala
@@ -1,35 +1,89 @@
 package net.sansa_stack.inference.rules.plan
 
 import java.io.PrintWriter
+import java.util.Collections
 
+import com.google.common.collect.ImmutableList
 import org.apache.calcite.config.Lex
-import org.apache.calcite.plan.{Contexts, ConventionTraitDef, RelTrait, RelTraitDef}
+import org.apache.calcite.plan._
 import org.apache.calcite.rel.{RelCollationTraitDef, RelNode}
 import org.apache.calcite.schema.SchemaPlus
 import org.apache.calcite.sql.parser.SqlParser
-import org.apache.calcite.tools.{FrameworkConfig, Frameworks, Planner, RuleSets}
+import org.apache.calcite.tools._
+
 import collection.JavaConverters._
 import scala.util.Try
-
 import org.apache.calcite.rel.`type`.RelDataTypeSystem
 import org.apache.calcite.rel.externalize.RelWriterImpl
-import org.apache.calcite.rel.rules.{FilterJoinRule, ProjectJoinTransposeRule}
+import org.apache.calcite.rel.rules._
 import org.apache.jena.reasoner.rulesys.Rule
-
 import net.sansa_stack.inference.utils.RuleUtils
+import org.apache.calcite.adapter.enumerable.{EnumerableConvention, EnumerableRules}
+import org.apache.calcite.interpreter.{BindableConvention, Bindables}
+import org.apache.calcite.plan.RelOptPlanner.CannotPlanException
+import org.apache.calcite.plan.hep.{HepMatchOrder, HepPlanner, HepProgramBuilder}
+import org.apache.calcite.plan.volcano.VolcanoPlanner
+import org.apache.calcite.sql2rel.{RelDecorrelator, SqlToRelConverter}
 
 /**
   * @author Lorenz Buehmann
   */
 class SimplePlanGenerator(schema: SchemaPlus) {
 
-  val traitDefs: List[RelTraitDef[_ <: RelTrait]] = List(ConventionTraitDef.INSTANCE, RelCollationTraitDef.INSTANCE)
+  val traitDefs: List[RelTraitDef[_ <: RelTrait]] = List(
+//    BindableConvention.INSTANCE.getTraitDef,
+    ConventionTraitDef.INSTANCE
+//    ,
+    ,RelCollationTraitDef.INSTANCE
+  )
 
   val optRuleSet = RuleSets.ofList(
     FilterJoinRule.FILTER_ON_JOIN,// push a filter into a join
     FilterJoinRule.JOIN,// push filter into the children of a join
     ProjectJoinTransposeRule.INSTANCE// push a projection to the children of a join
+//    ,
+//    // push and merge filter rules
+//    FilterAggregateTransposeRule.INSTANCE,
+//    FilterProjectTransposeRule.INSTANCE,
+//    FilterMergeRule.INSTANCE,
+//    FilterJoinRule.FILTER_ON_JOIN,
+//    FilterJoinRule.JOIN, /*push filter into the children of a join*/
+//    FilterTableScanRule.INSTANCE,
+//    // push and merge projection rules
+//    /*check the effectiveness of pushing down projections*/
+//    ProjectRemoveRule.INSTANCE,
+//    ProjectJoinTransposeRule.INSTANCE
   )
+
+  var program = Programs.ofRules(
+    // push and merge filter rules
+    FilterProjectTransposeRule.INSTANCE,
+    FilterMergeRule.INSTANCE,
+    FilterJoinRule.FILTER_ON_JOIN,
+    FilterJoinRule.JOIN, /*push filter into the children of a join*/
+    FilterTableScanRule.INSTANCE,
+    // push and merge projection rules
+    /*check the effectiveness of pushing down projections*/
+    ProjectRemoveRule.INSTANCE,
+    ProjectJoinTransposeRule.INSTANCE,
+    //JoinProjectTransposeRule.BOTH_PROJECT,
+    //ProjectFilterTransposeRule.INSTANCE, /*it is better to use filter first an then project*/
+    ProjectTableScanRule.INSTANCE,
+    ProjectWindowTransposeRule.INSTANCE,
+    ProjectMergeRule.INSTANCE,
+    //join rules
+    /*A simple trick is to consider a window size equal to stream cardinality.
+     * For tuple-based windows, the window size is equal to the number of tuples.
+     * For time-based windows, the window size is equal to the (input_rate*time_of_window).*/
+    //JoinToMultiJoinRule.INSTANCE ,
+    //LoptOptimizeJoinRule.INSTANCE ,
+    //MultiJoinOptimizeBushyRule.INSTANCE,
+    JoinPushThroughJoinRule.RIGHT,
+    JoinPushThroughJoinRule.LEFT, /*choose between right and left*/
+    JoinPushExpressionsRule.INSTANCE,
+    JoinAssociateRule.INSTANCE
+  )
+  program = Programs.ofRules(Bindables.RULES)
 
   val calciteFrameworkConfig: FrameworkConfig =
     Frameworks.newConfigBuilder
@@ -47,6 +101,7 @@ class SimplePlanGenerator(schema: SchemaPlus) {
       .ruleSets(optRuleSet)
       // Custom cost factory to use during optimization
       .costFactory(null)
+//      .programs(program)
       .typeSystem(RelDataTypeSystem.DEFAULT)
       .build()
 
@@ -61,7 +116,6 @@ class SimplePlanGenerator(schema: SchemaPlus) {
     * @return the root node of the logical plan
     */
   def generateLogicalPlan(rule: Rule): RelNode = {
-    planner.close()
     // generate SQL query
     val sqlQuery = sqlGenerator.generateSQLQuery(rule)
     println(sqlQuery)
@@ -73,7 +127,186 @@ class SimplePlanGenerator(schema: SchemaPlus) {
     val validatedSqlNode = planner.validate(sqlNode.get)
 
     // return the root node
-    planner.rel(validatedSqlNode).project
+    var root = planner.rel(validatedSqlNode).project() // .project
+//    root.register(basePlanner)
+//
+//
+//    basePlanner.setRoot(root)
+//    optimizePlan(root)
+
+    import org.apache.calcite.adapter.enumerable.EnumerableConvention
+    import org.apache.calcite.plan.RelTraitSet
+    val desiredTraits = root.getTraitSet.replace(EnumerableConvention.INSTANCE).simplify()
+
+//    root = root.getCluster.getPlanner.chooseDelegate.changeTraits(root, desiredTraits)
+
+    val node = planner.transform(0,
+      root.getTraitSet.simplify().plus(BindableConvention.INSTANCE),
+//      planner.getEmptyTraitSet
+//        .replace(ConventionTraitDef.INSTANCE.getDefault)
+//        .replace(RelCollationTraitDef.INSTANCE.getDefault),
+      root)
+    planner.reset()
+    node
+  }
+
+  val DATASET_NORM_RULES: RuleSet = RuleSets.ofList(
+    // simplify expressions rules
+    ReduceExpressionsRule.FILTER_INSTANCE,
+    ReduceExpressionsRule.PROJECT_INSTANCE,
+    ReduceExpressionsRule.CALC_INSTANCE,
+    ReduceExpressionsRule.JOIN_INSTANCE,
+    ProjectToWindowRule.PROJECT
+  )
+
+  val DATASET_OPT_RULES: RuleSet = RuleSets.ofList(
+
+    // convert a logical table scan to a relational expression
+    TableScanRule.INSTANCE,
+//    EnumerableToLogicalTableScan.INSTANCE,
+
+    // push a filter into a join
+    FilterJoinRule.FILTER_ON_JOIN,
+    // push filter into the children of a join
+    FilterJoinRule.JOIN,
+    // push filter through an aggregation
+    FilterAggregateTransposeRule.INSTANCE,
+
+    // aggregation and projection rules
+    AggregateProjectMergeRule.INSTANCE,
+    AggregateProjectPullUpConstantsRule.INSTANCE,
+    // push a projection past a filter or vice versa
+    ProjectFilterTransposeRule.INSTANCE,
+    FilterProjectTransposeRule.INSTANCE,
+    // push a projection to the children of a join
+    ProjectJoinTransposeRule.INSTANCE,
+    // remove identity project
+    ProjectRemoveRule.INSTANCE,
+    // reorder sort and projection
+    SortProjectTransposeRule.INSTANCE,
+    ProjectSortTransposeRule.INSTANCE,
+
+    // join rules
+    JoinPushExpressionsRule.INSTANCE,
+
+    // remove union with only a single child
+    UnionEliminatorRule.INSTANCE,
+    // convert non-all union into all-union + distinct
+    UnionToDistinctRule.INSTANCE,
+
+    // remove aggregation if it does not aggregate and input is already distinct
+    AggregateRemoveRule.INSTANCE,
+    // push aggregate through join
+    AggregateJoinTransposeRule.EXTENDED,
+    // aggregate union rule
+    AggregateUnionAggregateRule.INSTANCE,
+    // expand distinct aggregate to normal aggregate with groupby
+    AggregateExpandDistinctAggregatesRule.JOIN,
+
+    // remove unnecessary sort rule
+    SortRemoveRule.INSTANCE,
+
+    // prune empty results rules
+    PruneEmptyRules.AGGREGATE_INSTANCE,
+    PruneEmptyRules.FILTER_INSTANCE,
+    PruneEmptyRules.JOIN_LEFT_INSTANCE,
+    PruneEmptyRules.JOIN_RIGHT_INSTANCE,
+    PruneEmptyRules.PROJECT_INSTANCE,
+    PruneEmptyRules.SORT_INSTANCE,
+    PruneEmptyRules.UNION_INSTANCE,
+
+    // calc rules
+    FilterCalcMergeRule.INSTANCE,
+    ProjectCalcMergeRule.INSTANCE,
+    FilterToCalcRule.INSTANCE,
+    ProjectToCalcRule.INSTANCE,
+    CalcMergeRule.INSTANCE
+
+  )
+
+  def optimizePlan(relNode: RelNode): RelNode = {
+
+    def runHepPlanner(
+                       hepMatchOrder: HepMatchOrder,
+                       ruleSet: RuleSet,
+                       input: RelNode,
+                       targetTraits: RelTraitSet): RelNode = {
+      val builder = new HepProgramBuilder
+      builder.addMatchOrder(hepMatchOrder)
+
+      val it = ruleSet.iterator()
+      while (it.hasNext) {
+        builder.addRuleInstance(it.next())
+      }
+
+      val planner = new HepPlanner(builder.build, calciteFrameworkConfig.getContext)
+      planner.setRoot(input)
+      if (input.getTraitSet != targetTraits) {
+        planner.changeTraits(input, targetTraits.simplify)
+      }
+      planner.findBestExp
+    }
+
+    def runVolcanoPlanner(
+                                     ruleSet: RuleSet,
+                                     input: RelNode,
+                                     targetTraits: RelTraitSet): RelNode = {
+      val optProgram = Programs.ofRules(ruleSet)
+
+      val output = try {
+        optProgram.run(basePlanner, input, targetTraits,
+          ImmutableList.of(), ImmutableList.of())
+      } catch {
+        case e: Exception =>
+          throw e
+      }
+      output
+    }
+
+    // 1. decorrelate
+    val decorPlan = RelDecorrelator.decorrelateQuery(relNode)
+
+    // 2. normalize the logical plan
+    val normRuleSet = DATASET_NORM_RULES
+    val normalizedPlan = if (normRuleSet.iterator().hasNext) {
+      runHepPlanner(HepMatchOrder.BOTTOM_UP, normRuleSet, decorPlan, decorPlan.getTraitSet)
+    } else {
+      decorPlan
+    }
+
+    // 3. optimize the logical Flink plan
+    val optRuleSet = DATASET_OPT_RULES
+    val flinkOutputProps = relNode.getTraitSet.replace(ConventionTraitDef.INSTANCE.getDefault).simplify()
+    val optimizedPlan = if (optRuleSet.iterator().hasNext) {
+      runVolcanoPlanner(optRuleSet, normalizedPlan, flinkOutputProps)
+    } else {
+      normalizedPlan
+    }
+
+    optimizedPlan
+  }
+
+  val basePlanner: RelOptPlanner = {
+    val planner = new VolcanoPlanner(calciteFrameworkConfig.getCostFactory, Contexts.empty())
+    planner.setExecutor(calciteFrameworkConfig.getExecutor)
+    planner.addRelTraitDef(ConventionTraitDef.INSTANCE)
+    planner
+  }
+
+  def plan(rule: Rule) = {
+//    // generate SQL query
+//    val sqlQuery = sqlGenerator.generateSQLQuery(rule)
+//
+//    val sqlParser = SqlParser.create(sqlQuery, SqlParser.configBuilder().build())
+//    val sqlNode = sqlParser.parseStmt()
+//    val catalogReader = null
+//
+//    val pl = new VolcanoPlanner()
+//
+//    val sqlToRelConverter = new SqlToRelConverter(new ViewExpanderImpl(), )
+//
+//    program.run(pl, , planner.getEmptyTraitSet, Collections.emptyList(), Collections.emptyList())
+
   }
 
   /**
---
+++ sansa-inference-spark/src/main/scala/net/sansa_stack/inference/spark/backwardchaining/BackwardChainingReasoner.scala
@@ -0,0 +1,270 @@
+package net.sansa_stack.inference.spark.backwardchaining
+
+import java.io.PrintWriter
+
+import net.sansa_stack.inference.rules.RuleSets
+import net.sansa_stack.inference.rules.plan.{SimplePlanGenerator, SimpleSQLGenerator, TriplesSchema}
+import net.sansa_stack.inference.spark.backwardchaining.tree.{AndNode, OrNode}
+import net.sansa_stack.inference.spark.data.loader.RDFGraphLoader
+import net.sansa_stack.inference.spark.data.model.RDFGraph
+import net.sansa_stack.inference.utils.Logging
+import org.apache.jena.graph.{Node, NodeFactory, Triple}
+import org.apache.jena.reasoner.TriplePattern
+import org.apache.jena.reasoner.rulesys.Rule
+import org.apache.jena.reasoner.rulesys.impl.BindingVector
+import org.apache.jena.vocabulary.RDF
+import net.sansa_stack.inference.utils.RuleUtils._
+import net.sansa_stack.inference.utils.TripleUtils._
+import org.apache.calcite.rel.externalize.RelWriterImpl
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author Lorenz Buehmann
+  */
+class BackwardChainingReasoner(val rules: Set[Rule], val graph: RDFGraph) extends Logging{
+
+  def isEntailed(triple: Triple): Boolean = {
+    isEntailed(new TriplePattern(triple))
+  }
+
+  def isEntailed(tp: TriplePattern): Boolean = {
+
+    val tree = buildTree(new AndNode(tp), Seq())
+    println(tree.toString)
+
+    val rdd = processTree(tree)
+//    println(rdd.count())
+
+
+
+//    rules.foreach(r => {
+//      // check if the head is more general than the triple in question
+//      var head = r.headTriplePatterns()
+//
+//      head.foreach(headTP => {
+//        val subsumes = headTP.subsumes(tp)
+//
+//        if(subsumes) {
+//          val boundRule = createBinding(r, headTP, tp)
+//          println(boundRule.bodyTriplePatterns().mkString(";"))
+//        }
+//      })
+//
+//    })
+
+    false
+  }
+
+  private def processTree(tree: AndNode): RDD[Triple] = {
+    // 1. look for asserted triples in the graph
+    var rdd = graph.triples // lookup(tree.element)
+
+    // 2. process the inference rules that can infer the triple pattern
+    tree.children.foreach(child => {
+      println(s"processing rule ${child.element}")
+
+      val targetTp = child.element.headTriplePatterns().head
+
+      // recursively process each instantiated body atom of the rule
+      var node2RDD = child.children.map(
+        c => (c, processTree(c))).toMap
+
+      // and join them
+      node2RDD.map(_._1).toList.combinations(2).foreach(pair => {
+        val vars = joinVars(pair(0).element, pair(1).element)
+        println(vars.mkString("\n"))
+      })
+
+      applyRule(child.element)
+    })
+
+    rdd
+  }
+
+  private def selectedVars(body: TriplePattern, head: TriplePattern): Seq[Int] = {
+    var selectedIndexes: Seq[Int] = Seq()
+
+    val headVars = head.vars()
+
+    if(headVars.contains(body.getSubject)) {
+        selectedIndexes +:= 1
+    }
+    if(headVars.contains(body.getPredicate)) {
+      selectedIndexes +:= 2
+    }
+    if(headVars.contains(body.getObject)) {
+      selectedIndexes +:= 3
+    }
+
+    selectedIndexes
+  }
+
+  private def joinVars(tp1: TriplePattern, tp2: TriplePattern): Seq[(Node, Int, Int)] = {
+    var joinVars: Seq[(Node, Int, Int)] = Seq()
+
+    var tmp: Seq[(Node, Int)] = Seq()
+
+    if(tp1.getSubject.isVariable) {
+      tmp +:= (tp1.getSubject, 1)
+    }
+    if(tp1.getPredicate.isVariable) {
+      tmp +:= (tp1.getPredicate, 2)
+    }
+    if(tp1.getObject.isVariable) {
+      tmp +:= (tp1.getObject, 3)
+    }
+
+    tmp.foreach(entry => {
+      val node = entry._1
+      val index = entry._2
+      if (tp2.getSubject.equals(node)) {
+        joinVars +:= (node, index, 1)
+      }
+      if (tp2.getPredicate.equals(node)) {
+        joinVars +:= (node, index, 2)
+      }
+      if (tp2.getObject.equals(node)) {
+        joinVars +:= (node, index, 3)
+      }
+    })
+
+
+    joinVars
+  }
+
+  private def lookup(tp: TriplePattern): RDD[Triple] = {
+    graph.find(tp.asTriple())
+  }
+
+  private def buildTree(tree: AndNode, visited: Seq[Rule]): AndNode = {
+    val tp = tree.element
+
+    rules.filterNot(visited.contains(_)).foreach(r => {
+      // check if the head is more general than the triple in question
+      var head = r.headTriplePatterns()
+
+      head.foreach(headTP => {
+        val subsumes = headTP.subsumes(tp)
+
+        if(subsumes) {
+          // instantiate the rule
+          val boundRule = instantiateRule(r, tp)
+
+          // add new Or-node to tree
+          val node = new OrNode(boundRule)
+          //          println(node)
+          tree.children :+= node
+
+          boundRule.bodyTriplePatterns().foreach(newTp => {
+            node.children :+= buildTree(new AndNode(newTp), visited ++ Seq(r))
+          })
+        }
+      })
+
+    })
+
+    tree
+  }
+
+  /*
+  // create a binding for the rule variables
+   */
+  private def instantiateRule(rule: Rule, tp: TriplePattern): Rule = {
+    val headTP = rule.headTriplePatterns().head // TODO handle rules with multiple head TPs
+
+    val binding = new BindingVector(5)
+
+    // the subject
+    if(tp.getSubject.isConcrete && headTP.getSubject.isVariable) {
+      binding.bind(headTP.getSubject, tp.getSubject)
+    }
+    // the predicate
+    if(tp.getPredicate.isConcrete && headTP.getPredicate.isVariable) {
+      binding.bind(headTP.getPredicate, tp.getPredicate)
+    }
+    // the object
+    if(tp.getObject.isConcrete && headTP.getObject.isVariable) {
+      binding.bind(headTP.getObject, tp.getObject)
+    }
+
+    rule.instantiate(binding)
+  }
+
+  private def applyRule(rule: Rule) = {
+    // convert to SQL
+    val sqlGenerator = new SimpleSQLGenerator()
+    val sql = sqlGenerator.generateSQLQuery(rule)
+
+    // generate logical execution plan
+    val planGenerator = new SimplePlanGenerator(TriplesSchema.get())
+    val plan = planGenerator.generateLogicalPlan(rule)
+
+    // apply plan
+    plan.explain(new RelWriterImpl(new PrintWriter(System.out)))
+
+  }
+
+  def join(rdd1: RDD[Triple], rdd2: RDD[Triple],
+           idx1: Int, idx2: Int,
+           selectIndexes: Seq[Int],
+           targetTP: TriplePattern): RDD[Triple] = {
+    val tmp1 = idx1 match {
+      case 1 => rdd1.map(t => (t.getSubject, (t.getPredicate, t.getObject)))
+      case 2 => rdd1.map(t => (t.getPredicate, (t.getSubject, t.getObject)))
+      case 3 => rdd1.map(t => (t.getObject, (t.getSubject, t.getPredicate)))
+      case _ => throw new IllegalArgumentException(s"$idx1 is not supported for triples")
+    }
+
+    val tmp2 = idx2 match {
+      case 1 => rdd2.map(t => (t.getSubject, (t.getPredicate, t.getObject)))
+      case 2 => rdd2.map(t => (t.getPredicate, (t.getSubject, t.getObject)))
+      case 3 => rdd2.map(t => (t.getObject, (t.getSubject, t.getPredicate)))
+      case _ => throw new IllegalArgumentException(s"$idx2 is not supported for triples")
+    }
+
+    val joined = tmp1.join(tmp2)
+
+
+
+
+    rdd1
+  }
+
+}
+
+object BackwardChainingReasoner {
+
+
+  def main(args: Array[String]): Unit = {
+
+    val parallelism = 20
+
+    // the SPARK config
+    val session = SparkSession.builder
+      .appName(s"Spark Backward Chaining")
+      .master("local[4]")
+//      .config("spark.eventLog.enabled", "true")
+      .config("spark.hadoop.validateOutputSpecs", "false") // override output files
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.default.parallelism", parallelism)
+      .config("spark.ui.showConsoleProgress", "false")
+      .config("spark.sql.shuffle.partitions", parallelism)
+      .getOrCreate()
+
+    val graph = RDFGraphLoader.loadFromDisk(session, args(0))
+
+    val rules = RuleSets.RDFS_SIMPLE.filter(_.getName == "rdfs2")
+
+    val tp = Triple.create(
+      NodeFactory.createVariable("s"),
+      RDF.`type`.asNode(),
+      NodeFactory.createURI("http://swat.cse.lehigh.edu/onto/univ-bench.owl#Person"))
+
+    val reasoner = new BackwardChainingReasoner(rules, graph)
+
+    println(reasoner.isEntailed(tp))
+
+    session.stop()
+  }
+}
