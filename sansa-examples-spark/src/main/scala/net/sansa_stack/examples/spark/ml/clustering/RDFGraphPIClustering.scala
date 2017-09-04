57c3e52fc4238d90d304de25d65c5de3e006318d
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-09-04 10:33:31 +0200

    Update RDFGraphPIClustering example, fixes #18

--- sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/clustering/RDFGraphPIClustering.scala
+++ sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/clustering/RDFGraphPIClustering.scala
@@ -4,6 +4,10 @@ import scala.collection.mutable
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{ Level, Logger }
 import org.apache.spark.graphx.GraphLoader
+import org.apache.jena.riot.{ Lang, RDFDataMgr }
+import java.io.ByteArrayInputStream
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
 import net.sansa_stack.ml.spark.clustering.{ RDFGraphPICClustering => RDFGraphPICClusteringAlg }
 
 object RDFGraphPIClustering {
@@ -13,7 +17,7 @@ object RDFGraphPIClustering {
         "Usage: RDFGraphPIClustering <input> <k> <numIterations>")
       System.exit(1)
     }
-    val input = args(0) //"src/main/resources/BorderFlow_Sample1.txt"
+    val input = args(0) //"src/main/resources/Clustering_sampledata.nt"
     val k = args(1).toInt
     val numIterations = args(2).toInt
     val optionsList = args.drop(3).map { arg =>
@@ -38,7 +42,36 @@ object RDFGraphPIClustering {
     Logger.getRootLogger.setLevel(Level.ERROR)
 
     // Load the graph 
-    val graph = GraphLoader.edgeListFile(sparkSession.sparkContext, input)
+    //val graph = GraphLoader.edgeListFile(sparkSession.sparkContext, input)
+
+    // Load the RDF dataset 
+    val RDFfile = sparkSession.sparkContext.textFile(input).map(line =>
+      RDFDataMgr.createIteratorTriples(new ByteArrayInputStream(line.getBytes), Lang.NTRIPLES, null).next())
+
+    val r = RDFfile.map(f => {
+      val s = f.getSubject.getURI
+      val p = f.getPredicate.getURI
+      val o = f.getObject.getURI
+
+      (s, p, o)
+    })
+
+    val v11 = r.map(f => f._1)
+    val v22 = r.map(f => f._3)
+    val indexedmap = (v11.union(v22)).distinct().zipWithIndex()
+
+    val vertices: RDD[(VertexId, String)] = indexedmap.map(x => (x._2, x._1))
+    val _iriToId: RDD[(String, VertexId)] = indexedmap.map(x => (x._1, x._2))
+
+    val tuples = r.keyBy(f => f._1).join(indexedmap).map({
+      case (k, ((s, p, o), si)) => (o, (si, p))
+    })
+
+    val edgess: RDD[Edge[String]] = tuples.join(indexedmap).map({
+      case (k, ((si, p), oi)) => Edge(si, oi, p)
+    })
+
+    val graph = org.apache.spark.graphx.Graph(vertices, edgess)
 
     val model = RDFGraphPICClusteringAlg(sparkSession, graph, k, numIterations).run()
 
