c3c5d48d4978b383e6202055018cb5312c9bcee1
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-09-07 13:52:32 +0200

    Add loadAsTriple method on NQuadReader

--- sansa-rdf-spark-parent/sansa-rdf-spark-core/src/main/scala/net/sansa_stack/rdf/spark/io/NQuadReader.scala
+++ sansa-rdf-spark-parent/sansa-rdf-spark-core/src/main/scala/net/sansa_stack/rdf/spark/io/NQuadReader.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD
 import org.apache.jena.sparql.core.Quad
 import org.apache.jena.riot.{ Lang, RDFDataMgr }
+import org.apache.jena.graph.Triple
 
 /**
  * N-Quad reader
@@ -33,10 +34,33 @@ object NQuadReader {
    * @return the RDD of quads
    */
   def load(session: SparkSession, path: String): RDD[Quad] = {
+    def asTriple(quads: RDD[Quad]): RDD[Triple] = quads.map(_.asTriple())
     session.sparkContext.textFile(path)
       .filter(line => !line.trim().isEmpty & !line.startsWith("#"))
       .map(line =>
         RDFDataMgr.createIteratorQuads(new ByteArrayInputStream(line.getBytes), Lang.NQUADS, null).next())
+  }
+
+  /**
+   * Loads an N-Quads file into an RDD of Triple.
+   *
+   * @param session the Spark session
+   * @param path    the path to the N-Quads file(s)
+   * @return the RDD of triples
+   */
+  def loadAsTriple(session: SparkSession, path: URI): RDD[Triple] = {
+    load(session, path.toString).map(_.asTriple())
+  }
+
+  /**
+   * Loads an N-Quads file into an RDD of Triple.
+   *
+   * @param session the Spark session
+   * @param path    the path to the N-Quads file(s)
+   * @return the RDD of triples
+   */
+  def loadAsTriple(session: SparkSession, path: String): RDD[Triple] = {
+    load(session, path).map(_.asTriple())
   }
 
 }
