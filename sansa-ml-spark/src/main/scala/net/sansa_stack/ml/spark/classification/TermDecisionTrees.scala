8ffca4e10dc0c05b20e3878ffd9f38c5b888a296
Heba <heba.a.ibrahim@gmail.com>
2017-11-26 20:56:06 +0100

    initialize pos&neg examples

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/classification/TermDecisionTrees.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/classification/TermDecisionTrees.scala
@@ -1,22 +1,19 @@
-package net.sansa_stack.ml.spark.classification
+package net.sansa_stack.ml.spark.classification 
 
 import java.io.File
 import java.net.URI
+import java.util.ArrayList
 import java.util.stream.Collectors
 import scala.reflect.runtime.universe._
 import scala.collection.JavaConverters._
-
-//import net.sansa_stack.rdf.spark.io.NTripleReader
-//import net.sansa_stack.inference.spark.data.loader.RDFGraphLoader
-
-
-import net.sansa_stack.owl.spark.rdd.FunctionalSyntaxOWLAxiomsRDDBuilder
-import net.sansa_stack.owl.spark.rdd.OWLAxiomsRDD
 
 import net.sansa_stack.ml.spark.classification.KB.KB
 import net.sansa_stack.ml.spark.classification.ClassMembership.ClassMembership
 import net.sansa_stack.ml.spark.classification.TDTInducer.TDTInducer
 import net.sansa_stack.ml.spark.classification.TDTClassifiers.TDTClassifiers
+
+import net.sansa_stack.owl.spark.rdd.FunctionalSyntaxOWLAxiomsRDDBuilder
+import net.sansa_stack.owl.spark.rdd.OWLAxiomsRDD
 
 import scopt.OptionParser
 import org.apache.log4j.{Level, Logger}
@@ -45,24 +42,44 @@ object TermDecisionTrees {
       .appName("Termnological Decision Tree")
       .getOrCreate()
    
-    /*
- 		* Call owl axion builder to read the classes and object properties and print
- 		*/
- 		//val parallelism = 4
- 		//RDFGraphLoader.loadFromDisk(URI.create(input), sparkSession, parallelism) 
-    //val triplesRDD = NTripleReader.load(sparkSession, URI.create(input))
-    //triplesRDD.take(5).foreach(println(_))
-  
+    //Call owl axion builder to read the classes and object properties and print
+ 	 		
     val rdd : OWLAxiomsRDD = FunctionalSyntaxOWLAxiomsRDDBuilder.build(sparkSession.sparkContext, input)
      
     val kb: KB = new KB(input, rdd, sparkSession)
-    var ClassM = new ClassMembership(kb, sparkSession)
-    val ClassName = TDTInducer.toString()
-    ClassM.bootstrap(10, ClassName, sparkSession)
-    
-    //val PostiveExamples = sparkSession.sparkContext.parallelize(Array("east1", "east2", "east3", "east4", "east5"))
-    //val NegativeExamples = sparkSession.sparkContext.parallelize(Array("west6", "west7", "west8", "west9", "west10"))
+   // var ClassM = new ClassMembership(kb, sparkSession)
+   // val ClassName = TDTInducer.toString()
+    // ClassM.bootstrap(10, ClassName, sparkSession)
     //val c : TDTInducer = new TDTInducer(kb, kb.Concepts.count().toInt, sparkSession)
+    
+    var PosExamples = sparkSession.sparkContext.parallelize(Array("http://example.com/foo#east1", 
+        "http://example.com/foo#east2",
+        "http://example.com/foo#east3",
+        "http://example.com/foo#east4",
+        "http://example.com/foo#east5"))
+    
+    var NegExamples = sparkSession.sparkContext.parallelize(Array("http://example.com/foo#west6", 
+        "http://example.com/foo#west7", 
+        "http://example.com/foo#west8", 
+        "http://example.com/foo#west9", 
+        "http://example.com/foo#west10"))
+    
+    var UndExamples = sparkSession.sparkContext.parallelize(new ArrayList[String]().asScala)
+
+    val numPos: Double = PosExamples.count
+    val numNeg: Double = NegExamples.count
+    val perPos: Double = numPos / (numPos + numNeg)
+    val perNeg: Double = numNeg / (numPos + numNeg)
+    
+    println("\nLearning problem: \n --------------------\n")
+    println("No. of Positive examples: " + PosExamples.count)
+    println("No. of Negative examples: " + NegExamples.count)
+    println("No. of Undefined examples: " + UndExamples.count)
+    println("\nper Pos: " + perPos)
+    println("per Neg: " + perNeg)
+    
+    val c : TDTClassifiers = new TDTClassifiers (kb, sparkSession)
+    val tree : DLTree = c.induceDLTree(kb.getDataFactory.getOWLThing, PosExamples, NegExamples, UndExamples, 50, perPos, perNeg)
   
     sparkSession.stop
 
