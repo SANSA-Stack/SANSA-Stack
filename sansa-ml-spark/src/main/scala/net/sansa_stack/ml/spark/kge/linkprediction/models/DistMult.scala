0d8fe1f74ff0a25c4c1d78e0cd247a1af3912fa2
lpfgarcia@gmail.com <lpfgarcia@gmail.com>
2017-11-30 14:08:02 +0100

    add DistMult

---
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/kge/linkprediction/models/DistMult.scala
@@ -0,0 +1,49 @@
+package net.sansa_stack.ml.spark.kge.linkprediction.models
+
+/**
+ * Created by lpfgarcia on 20/11/2017.
+ */
+
+import org.apache.spark.sql._
+
+import com.intel.analytics.bigdl.optim.Adam
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric.NumericFloat
+
+class DistMult(train: DataFrame, batch: Int, k: Int, sk: SparkSession)
+    extends Models(train: DataFrame, batch: Int, k: Int, sk: SparkSession) {
+
+  val epochs = 100
+  val rate = 0.01f
+
+  var opt = new Adam(learningRate = rate)
+
+  def dist(data: DataFrame) = {
+    val aux = data.collect().map { i =>
+      e(i.getInt(0)) * r(i.getInt(1)) * e(i.getInt(2))
+    }.reduce((a, b) => a + b)
+
+    L2(aux)
+  }
+
+  def run() = {
+
+    for (i <- 1 to epochs) {
+
+      e = normalize(e)
+      val pos = subset(train)
+      val neg = generate(pos)
+
+      def delta(x: Tensor[Float]) = {
+        (dist(neg) - dist(pos) + 1, x)
+      }
+
+      opt.optimize(delta, e)
+      opt.optimize(delta, r)
+      val err = dist(pos) - dist(neg) + 1
+      printf("Epoch: %d: %f\n", i, err)
+
+    }
+  }
+
+}
