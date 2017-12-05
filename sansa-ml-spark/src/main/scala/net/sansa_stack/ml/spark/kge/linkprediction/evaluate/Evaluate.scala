a0e3af08a02c017a41acfaee2da8ed63cb89c92f
lpfgarcia <lpfgarcia@gmail.com>
2017-12-05 11:24:24 +0100

    add evaluation code - in progress

---
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/kge/linkprediction/evaluate/Evaluate.scala
@@ -0,0 +1,17 @@
+package net.sansa_stack.ml.spark.kge.linkprediction.evaluate
+
+/**
+ * Evaluate Object
+ * ---------------
+ *
+ * Created by lpfgarcia on 05/12/2017.
+ */
+
+object Evaluate {
+
+  def meanRank(left: Array[Float], right: Array[Float]) {
+    (left.sum / left.length,
+      right.sum / right.length)
+  }
+
+}
