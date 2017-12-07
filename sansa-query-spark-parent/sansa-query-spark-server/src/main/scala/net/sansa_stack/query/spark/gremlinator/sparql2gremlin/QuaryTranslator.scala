37d3d4d36cd755893a96683ba049ed8f952b2db6
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-12-07 14:46:32 +0100

    Add README and QueryTranstalor using Gremlinator

---
+++ sansa-query-spark-parent/sansa-query-spark-server/src/main/scala/net/sansa_stack/query/spark/gremlinator/sparql2gremlin/QuaryTranslator.scala
@@ -0,0 +1,8 @@
+package net.sansa_stack.query.spark.gremlinator.sparql2gremlin
+
+object QuaryTranslator {
+  
+  //TODO not implemented yet.
+  def apply(sparql:String) = "retun gremlin query syntax"
+  
+}
