0273475d56f88b56b9f37fff0f2a7daed29ea3c1
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-12-07 14:46:13 +0100

    Towards SANSA Gremlinator :)

---
+++ sansa-query-spark-parent/sansa-query-spark-server/src/main/scala/net/sansa_stack/query/spark/gremlinator/GremlinatorSpark.scala
@@ -0,0 +1,53 @@
+package net.sansa_stack.query.spark.gremlinator
+
+import org.apache.tinkerpop.gremlin.spark.structure.Spark
+import org.apache.tinkerpop.gremlin.structure.util.GraphFactory
+import org.apache.spark.sql.SparkSession
+import org.apache.commons.configuration.BaseConfiguration
+import org.apache.tinkerpop.gremlin.spark.process.computer.SparkGraphComputer
+import org.apache.tinkerpop.gremlin.process.computer.{ GraphComputer, VertexProgram }
+import org.apache.tinkerpop.gremlin.structure.Graph
+import org.apache.tinkerpop.gremlin.process.computer.traversal.TraversalVertexProgram
+import org.apache.spark.rdd.RDD
+import org.apache.tinkerpop.gremlin.hadoop.structure.io.VertexWritable
+import net.sansa_stack.query.spark.gremlinator.sparql2gremlin.QuaryTranslator
+import net.sansa_stack.query.spark.gremlinator.utils.Config._
+
+/*
+   * GremlinatorSpark - a SPARQL to gremlin query rewriter on Spark.
+ */
+
+object GremlinatorSpark {
+
+  @transient var spark: SparkSession = _
+
+  def apply(sparql: String) = {
+
+    val gremlinquery = QuaryTranslator(sparql)
+
+    val traversalRes = run(gremlinquery)
+
+    traversalRes
+  }
+
+  def run(gremlinQuery: String, lang: String = "gremlin-groovy") = {
+
+    val gremlinSpark = Spark.create(spark.sparkContext)
+    val sparkComputerConnection = GraphFactory.open(getSparkConfig) //spark.conf.getAll.toString())
+
+    val sparkGraphComputer = getSparkGraphComputer(sparkComputerConnection)
+
+    val traversalSource = sparkGraphComputer.program(TraversalVertexProgram.build()
+      .traversal(
+        sparkComputerConnection.traversal().withComputer(classOf[SparkGraphComputer]),
+        lang,
+        gremlinQuery)
+      .create(sparkComputerConnection))
+      .submit()
+      .get()
+
+    traversalSource
+    //traversalSource.graph().compute().submit().get
+  }
+
+}
