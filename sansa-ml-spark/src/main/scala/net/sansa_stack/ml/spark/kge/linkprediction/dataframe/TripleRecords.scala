e576aa882062d728135875bab7e14809dda66dd8
lpfgarcia <lpfgarcia@gmail.com>
2017-12-13 14:23:53 +0100

    rename and reuse

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/kge/linkprediction/dataframe/TripleRecords.scala
+++
@@ -1,7 +0,0 @@
-package net.sansa_stack.ml.spark.kge.linkprediction.dataframe
-
-case class StringRecord(Subject: String, Predicate: String, Object: String)
-
-case class IntegerRecord(Subject: Int, Predicate: Int, Object: Int)
-
-case class LongRecord(Subject: Long, Predicate: Long, Object: Long)
