88fb8c3a37bdf16863425012d17a93bd6b015dbd
lpfgarcia <lpfgarcia@gmail.com>
2017-12-13 14:25:39 +0100

    test script

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/kge/linkprediction/run/TransERun.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/kge/linkprediction/run/TransERun.scala
@@ -9,49 +9,38 @@ import org.apache.spark.sql._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-import net.sansa_stack.ml.spark.kge.linkprediction.dataframe.Triples
 import net.sansa_stack.ml.spark.kge.linkprediction.convertor.ByIndex
 import net.sansa_stack.ml.spark.kge.linkprediction.crossvalidation.Holdout
-import net.sansa_stack.ml.spark.kge.linkprediction.prediction.PredictTransE
 import net.sansa_stack.ml.spark.kge.linkprediction.models.TransE
-import net.sansa_stack.ml.spark.kge.linkprediction.dataframe.IntegerRecord
-import net.sansa_stack.ml.spark.kge.linkprediction.dataframe.StringRecord
+import net.sansa_stack.ml.spark.kge.linkprediction.prediction.PredictTransE
+import net.sansa_stack.ml.spark.kge.linkprediction.triples._
 
 object TransERun {
 
   Logger.getLogger("org").setLevel(Level.OFF)
   Logger.getLogger("akka").setLevel(Level.OFF)
 
-  val sk = SparkSession.builder.master("local")
+  val spark = SparkSession.builder.master("local")
     .appName("Tensor").getOrCreate
 
   def main(args: Array[String]) = {
 
-    val table = new Triples("kge", "/home/lpfgarcia/Desktop/tensor/data/train.txt", sk)
+    val table = new Triples("/home/lpfgarcia/Desktop/SANSA-ML/data/train.txt", "\t", false, false, spark)
 
-    import sk.implicits._
+    val data = new ByIndex(table.triples, spark)
 
-    // should be removed after Hamed adaptation
-    val temp = sk.createDataset(table.triples.collect().map { i =>
-      StringRecord(i.getString(0), i.getString(1), i.getString(2))
-    })
+    print(data.triples.show())
 
-    print(temp.show())
-
-    val data = new ByIndex(temp, sk)
-
-    print(data.df.show())
-
-    val (train, test) = new Holdout(data.df, 0.6f).crossValidation()
+    val (train, test) = new Holdout(data.triples, 0.6f).crossValidation()
 
     println(train.show())
     println(test.show())
 
-    //var model = new TransE(train, data.e.length, data.r.length, 100, 20, 1, "L1", sk)
-    //model.run()
+    var model = new TransE(train, data.e.length, data.r.length, 100, 20, 1, "L1", spark)
+    model.run()
 
-    //val predict = new PredictTransE(model, test).ranking()
-    //println(predict)
+    val predict = new PredictTransE(model, test).ranking()
+    println(predict)
 
   }
 
---
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/kge/linkprediction/run/TriplesRun.scala
@@ -0,0 +1,92 @@
+package net.sansa_stack.ml.spark.kge.linkprediction.NewDesign
+
+import scala.util.Random
+
+import org.apache.spark.sql._
+
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+import org.springframework.util.StopWatch
+
+import net.sansa_stack.ml.spark.kge.linkprediction.triples._
+import net.sansa_stack.ml.spark.kge.linkprediction.convertor.ByIndex
+
+object runTesting extends App {
+
+  def printType[T](x: T): Unit = { println(x.getClass.toString()) }
+
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
+  val spark = SparkSession.builder
+    .master("local")
+    .appName("TransE")
+    .getOrCreate()
+  import spark.implicits._
+
+  //  spark.sparkContext.setLogLevel("ERROR")
+  spark.sparkContext.setLogLevel("OFF")
+
+  println("<<< STARTING >>>")
+
+  var watch: StopWatch = new StopWatch()
+
+  watch.start()
+  val trp = new Triples("/home/hamed/workspace/TransE/DataSets/FB15k/freebase_mtr100_mte100-train.txt", "\t", false, false, spark)
+  watch.stop()
+  println("Readin triples done in " + watch.getTotalTimeSeconds + " seconds")
+
+  watch.start()
+  var num: Long = trp.triples.count()
+  watch.stop()
+  println("\n\n No triples = " + num.toString() + " - Done in " + watch.getTotalTimeSeconds + " seconds.")
+
+  watch.start()
+  num = trp.getEntities().length
+  watch.stop()
+  println("\n\n No Entities = " + num.toString() + " - Done in " + watch.getTotalTimeSeconds + " seconds.")
+
+  watch.start()
+  num = trp.getRelations().length
+  watch.stop()
+  println("\n\n No Predicates = " + num.toString() + " - Done in " + watch.getTotalTimeSeconds + " seconds.")
+  //  trp.getAllDistinctEntities().take(10).foreach(println)
+  //  println("\n \n No entities = ",trp.getAllDistinctEntities().count() )
+  //  println("\n \n No predicates = ",trp.getAllDistinctPredicates().count() )
+
+  //  val e1 = trp.getAllDistinctEntities().take(10).toSeq.toDS()
+  //  println("\n \n ----------")
+  //  e1.foreach(x=>println(x))
+  val n = 10
+  val conv = new ByIndex(trp.triples, spark)
+
+  //  val id1 = conv.entities.select("ID").sample(false,0.2).take(n)
+  //  val ind1 = id1.map( row => row(0).asInstanceOf[Long]).toSeq.toDS()
+  //  
+  //  val r1 = conv.getEntitiesByIndex(ind1).persist()
+  //  println(" count = ", r1.count)
+  //  r1.show()
+  //
+  //  val id2 = conv.predicates.select("ID").sample(false, 0.2).take(n)
+  //  val ind2 = id2.map( row => row(0).asInstanceOf[Long]).toSeq.toDS()
+  //  
+  //  val r2 = conv.getPredicatesByIndex(ind2).persist()
+  //  println(" count = ", r2.count)
+  //  r2.show()
+  //  
+  //  
+  println("\n\n------ TESTING -----")
+
+  lazy val smp1 = trp.triples.take(n)
+  lazy val sample1 = smp1.toSeq.toDF().asInstanceOf[Dataset[StringTriples]]
+
+  sample1.show()
+
+  //val r3 = conv.getTriplesByIndex(sample1)
+  //r3.printSchema()
+  //r3.show
+
+  //val r4 = conv.getTriplesByString(r3)
+  //println("<<< DONE >>>")
+}
