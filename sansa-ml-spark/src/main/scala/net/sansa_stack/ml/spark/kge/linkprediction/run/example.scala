7265b1885d71d86aa6344f8983b4546999e2e36b
shariat <shariat@cs.uni-bonn.de>
2017-12-14 14:26:05 +0100

    providing and example

---
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/kge/linkprediction/run/example.scala
@@ -0,0 +1,35 @@
+package net.sansa_stack.ml.spark.kge.linkprediction.run
+
+import org.apache.spark.sql._
+import  net.sansa_stack.ml.spark.kge.linkprediction._
+import net.sansa_stack.ml.spark.kge.linkprediction.triples.Triples
+import net.sansa_stack.ml.spark.kge.linkprediction.convertor.ByIndex
+
+object example extends App {
+
+  val spark = SparkSession.builder.master("local")
+    .appName("kge").getOrCreate
+    
+  // Assuming the file "test.txt" exists which contains the triples, we load the file 
+  // the function is provided with the path of the file 
+  // delimiter = "\t" , indicates file is in tab separated format
+  // headers = false , indicates that we ignore the header of the file 
+  // numeric = false , indicated that content of the file in not in numeric format i.e. (subject,predicate,object) are not numerically given   
+  
+  val data = new Triples("/home/hamed/Downloads/FB15K-237.2/test.txt","\t", false, false, spark)
+  
+  // getting 10 distinct entities in (s,p,o) i.e. subjects + objects and printing them
+  data.getEntities().take(10).foreach(println)
+  
+  // getting 10 distinct predicates in (s,p,o) and printing them
+  data.getEntities().take(10).foreach(println)
+  
+  // converting the original data to indexData 
+  val indexedData = new ByIndex(data.triples, spark)
+  
+  // getting 10 distinct (s,p,o) in their numeric (indexed) form and print them
+  indexedData.numeric.take(10).foreach(println)
+  
+  
+  println("<< DONE >>")
+}
