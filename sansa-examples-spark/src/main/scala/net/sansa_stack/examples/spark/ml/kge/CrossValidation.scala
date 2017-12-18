33a441ede1da68f69ca685331dc96a754e6ffec1
Gezim Sejdiu <g.sejdiu@gmail.com>
2017-12-18 13:35:45 +0100

    Refactor& add different cross validation technique on CrossValidation example

--- sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/kge/CrossValidation.scala
+++ sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/kge/CrossValidation.scala
@@ -1,22 +1,23 @@
-package net.sansa_stack.examples.spark.rdf
+package net.sansa_stack.examples.spark.ml.kge
 
 import org.apache.spark.sql._
 import net.sansa_stack.rdf.spark.kge.triples.Triples
 import net.sansa_stack.rdf.spark.kge.convertor.ByIndex
-import net.sansa_stack.rdf.spark.kge.crossvalidation.Holdout
+import net.sansa_stack.ml.spark.kge.linkprediction.crossvalidation.{ Bootstrapping, Holdout }
+import net.sansa_stack.ml.spark.kge.linkprediction.crossvalidation.kFold
 
-object KGE_Holdout {
+object CrossValidation {
 
   def main(args: Array[String]) {
     parser.parse(args, Config()) match {
       case Some(config) =>
-        run(config.in)
+        run(config.in, config.technique, config.k)
       case None =>
         println(parser.usage)
     }
   }
 
-  def run(input: String): Unit = {
+  def run(input: String, technique: String, k: Int): Unit = {
 
     val spark = SparkSession.builder
       .appName(s"Holdout Cross validation techniques example  $input")
@@ -25,14 +26,8 @@ object KGE_Holdout {
       .getOrCreate()
 
     println("==============================================")
-    println("|Holdout Cross validation techniques example |")
+    println("|Cross validation techniques example |")
     println("==============================================")
-
-    // Assuming the file "test.txt" exists which contains the triples, we load the file
-    // the function is provided with the path of the file
-    // delimiter = "\t" , indicates file is in tab separated format
-    // headers = false , indicates that we ignore the header of the file
-    // numeric = false , indicated that content of the file in not in numeric format i.e. (subject,predicate,object) are not numerically given
 
     val data = new Triples(input, "\t", false, false, spark)
 
@@ -48,9 +43,14 @@ object KGE_Holdout {
 
     // getting 10 distinct (s,p,o) in their numeric (indexed) form and print them
     indexedData.numeric.take(10).foreach(println)
-    
-    // using the holdout cross validation technique to get 60% of data as training and the rest as testing
-    val (train, test) = new Holdout(numericData, 0.6f).crossValidation()
+
+    val (train, test) = technique match {
+      case "holdout"       => new Holdout(numericData, 0.6f).crossValidation()
+      case "bootstrapping" => new Bootstrapping(numericData).crossValidation()
+      case "kFold"         => new kFold(numericData, k, spark).crossValidation()
+      case _ =>
+        throw new RuntimeException("'" + technique + "' - Not supported, yet.")
+    }
 
     println("<< DONE >>")
 
@@ -58,15 +58,29 @@ object KGE_Holdout {
 
   }
 
-  case class Config(in: String = "")
+  case class Config(in: String = "", technique: String = "", k: Int = 0)
 
-  val parser = new scopt.OptionParser[Config]("Holdout Cross validation techniques example") {
+  val parser = new scopt.OptionParser[Config]("Cross validation techniques example") {
 
-    head(" Holdout Cross validation techniques example")
+    head("Cross validation techniques example")
 
     opt[String]('i', "input").required().valueName("<path>").
       action((x, c) => c.copy(in = x)).
       text("path to file that contains the data")
+
+    opt[String]('t', "technique").required().valueName("{holdout | bootstrapping | kFold}").
+      action((x, c) => c.copy(technique = x)).
+      text("cross validation techniques")
+
+    opt[Int]("k").optional().valueName("<value>").
+      action((x, c) => {
+        c.copy(k = x)
+      }).
+      text("The k value (used only for technique'kFold')")
+
+    checkConfig(c =>
+      if (c.technique == "kFold" && c.k == 0) failure("Option --k-Fold must not be empty if technique 'kFold	' is set")
+      else success)
 
     help("help").text("prints this usage text")
   }
