8a6e0bb340374c3d287c115e19408c96733a95c2
Rajjat Dadwal <Rajjat@users.noreply.github.com>
2017-12-18 04:47:36 +0100

    Broadcast commented.

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/outliers/anomalydetection/Similarity.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/outliers/anomalydetection/Similarity.scala
@@ -3,7 +3,7 @@ package net.sansa_stack.ml.spark.outliers.anomalydetection
 import org.apache.spark.sql.SparkSession
 import org.apache.jena.graph.Node
 object Similarity {
-  def sim(seq1: Seq[String], seq2: Seq[String]): Double = {
+  def sim(seq1: Set[String], seq2: Set[String]): Double = {
 
     val sparkSession = SparkSession.builder
       .master("local[*]")
@@ -12,15 +12,15 @@ object Similarity {
       .getOrCreate()
     
    
-    val broadcastSeq1 = sparkSession.sparkContext.broadcast(seq1)
-    val broadcastSeq2 = sparkSession.sparkContext.broadcast(seq2)
+//    val broadcastSeq1 = sparkSession.sparkContext.broadcast(seq1)
+//    val broadcastSeq2 = sparkSession.sparkContext.broadcast(seq2)
+//
+//    val broadcastSet1 = broadcastSeq1.value.toSet
+//    val broadcastSet2 = broadcastSeq2.value.toSet
 
-    val broadcastSet1 = broadcastSeq1.value.toSet
-    val broadcastSet2 = broadcastSeq2.value.toSet
+    val intersect_cnt = seq1.intersect(seq2).size
 
-    val intersect_cnt = broadcastSet1.intersect(broadcastSet2).size
-
-    val union_count = broadcastSet1.union(broadcastSet2).size
+    val union_count = seq1.union(seq2).size
     val jSimilarity = intersect_cnt / (union_count).toDouble
    
     
