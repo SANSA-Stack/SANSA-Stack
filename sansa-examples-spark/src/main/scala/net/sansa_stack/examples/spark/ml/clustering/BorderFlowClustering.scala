b8ea13ffb4a0c15f865425fa2c8dc8b9898cce04
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-01-04 14:24:06 +0100

    Update BorderFlowClustering example

--- sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/clustering/BorderFlowClustering.scala
+++ sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/clustering/BorderFlowClustering.scala
@@ -3,34 +3,34 @@ package net.sansa_stack.examples.spark.ml.clustering
 import scala.collection.mutable
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{ Level, Logger }
-import net.sansa_stack.ml.spark.clustering.{BorderFlow,FirstHardeninginBorderFlow}
+import net.sansa_stack.ml.spark.clustering.{ BorderFlow, FirstHardeninginBorderFlow }
 
 object BorderFlowClustering {
 
   def main(args: Array[String]) {
     parser.parse(args, Config()) match {
       case Some(config) =>
-        run(config.in, config.alg)
+        run(config.alg, config.in, config.out, config.outevlsoft, config.outevlhard)
       case None =>
         println(parser.usage)
     }
   }
 
-  def run(input: String, algName:String): Unit = {
+  def run(algName: String, input: String, output: String, outputevlsoft: String, outputevlhard: String): Unit = {
 
     val spark = SparkSession.builder
-      .appName(s"BorderFlow example ( $input )")
+      .appName(s"BorderFlow example: $algName ( $input )")
       .master("local[*]")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
 
     println("============================================")
-    println("| Border Flow example                      |")
+    println(s"| Border Flow example    ($algName)       |")
     println("============================================")
-    
+
     val borderflow = algName match {
-      case "borderflow"       => BorderFlow(spark, input)
-      case "firsthardening" => FirstHardeninginBorderFlow(spark, input)
+      case "borderflow"     => BorderFlow(spark, input, output, outputevlsoft, outputevlhard)
+      case "firsthardening" => FirstHardeninginBorderFlow(spark, input, output, outputevlsoft)
       case _ =>
         throw new RuntimeException("'" + algName + "' - Not supported, yet.")
     }
@@ -39,20 +39,35 @@ object BorderFlowClustering {
 
   }
 
-  case class Config(in: String = "", alg:String ="borderflow")
+  case class Config(alg: String = "borderflow", in: String = "", out: String = "", outevlsoft: String = "", outevlhard: String = "")
 
   val parser = new scopt.OptionParser[Config]("BorderFlow") {
 
     head("BorderFlow: an example BorderFlow app.")
 
-    opt[String]('i', "input").required().valueName("<path>").
-      action((x, c) => c.copy(in = x)).
-      text("path to file contains the input files")
-      
     opt[String]('a', "algName").required().valueName("{borderflow | firsthardening }").
       action((x, c) => c.copy(alg = x)).
       text("BorderFlow algorithm type")
 
+    opt[String]('i', "input").required().valueName("<path>").
+      action((x, c) => c.copy(in = x)).
+      text("path to file contains the input files")
+
+    opt[String]('o', "out").required().valueName("<directory>").
+      action((x, c) => c.copy(out = x)).
+      text("the output directory")
+
+    opt[String]('e', "outevlsoft").required().valueName("<directory>").
+      action((x, c) => c.copy(outevlsoft = x)).
+      text("the outevlsoft directory")
+
+    opt[String]("outevlhard").optional().valueName("<directory>").
+      action((x, c) => c.copy(outevlhard = x)).
+      text("the outevlhard directory (used only for alg 'firsthardening')")
+
     help("help").text("prints this usage text")
+    checkConfig(c =>
+      if (c.alg == "firsthardening" && c.outevlhard.isEmpty) failure("Option --outevlhard must not be empty if alg 'firsthardening' is set")
+      else success)
   }
 }
