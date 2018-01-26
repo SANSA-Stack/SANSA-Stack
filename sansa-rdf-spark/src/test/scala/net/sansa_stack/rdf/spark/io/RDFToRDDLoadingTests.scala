d39320a40d649ef3d326a5e24c6c328ac21ae260
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-01-26 14:41:50 +0100

    Update RDFToRDDLoadingTests

--- sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/io/RDFToRDDLoadingTests.scala
+++ sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/io/RDFToRDDLoadingTests.scala
@@ -2,16 +2,17 @@ package net.sansa_stack.rdf.spark.io
 
 import java.nio.file.Paths
 
-import com.holdenkarau.spark.testing.SharedSparkContext
 import org.apache.jena.riot.Lang
 import org.scalatest.FunSuite
+import org.apache.spark.sql._
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
 
 /**
   * Tests for loading triples from either N-Triples are Turtle files into an [[org.apache.spark.rdd.RDD]].
   *
   * @author Lorenz Buehmann
   */
-class RDFToRDDLoadingTests extends FunSuite with SharedSparkContext {
+class RDFToRDDLoadingTests extends FunSuite with DataFrameSuiteBase {
 
   import net.sansa_stack.rdf.spark.io.rdf._
 
@@ -20,7 +21,7 @@ class RDFToRDDLoadingTests extends FunSuite with SharedSparkContext {
     val path = getClass.getResource("/loader/data.nt").getPath
     val lang: Lang = Lang.NTRIPLES
 
-    val triples = sc.rdf(lang, allowBlankLines = true)(path)
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
 
     val cnt = triples.count()
     assert(cnt == 9)
@@ -30,7 +31,7 @@ class RDFToRDDLoadingTests extends FunSuite with SharedSparkContext {
     val path = getClass.getResource("/loader/data.ttl").getPath
     val lang: Lang = Lang.TURTLE
 
-    val triples = sc.rdf(lang)(path)
+    val triples = spark.rdf(lang)(path)
 
     val cnt = triples.count()
     assert(cnt == 12)
@@ -39,7 +40,7 @@ class RDFToRDDLoadingTests extends FunSuite with SharedSparkContext {
   test("loading RDF/XML file into RDD should result in 9 triples") {
     val path = getClass.getResource("/loader/data.rdf").getPath
 
-    val triples = sc.rdfxml(path)
+    val triples = spark.rdfxml(path)
 
     val cnt = triples.count()
     assert(cnt == 9)
@@ -48,7 +49,7 @@ class RDFToRDDLoadingTests extends FunSuite with SharedSparkContext {
   test("loading RDF/XML file into RDD should result in 122 triples") {
     val path = Paths.get("/tmp/lubm/100").toAbsolutePath.toString
 
-    val triples = sc.rdfxml(path)
+    val triples = spark.rdfxml(path)
 
     val cnt = triples.count()
     println(cnt)
