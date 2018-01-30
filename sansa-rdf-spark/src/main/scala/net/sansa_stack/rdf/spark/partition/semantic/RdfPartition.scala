cf28dfe07a556c21049b22b810abce662e066453
Imran Khan <imransilvake@gmail.com>
2018-01-30 15:49:53 +0100

    Update Partition

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/semantic/RdfPartition.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/semantic/RdfPartition.scala
@@ -38,38 +38,45 @@ class RdfPartition(
     // partition the data
     val partitionedData = nTriplesRDD
       .distinct
-      .filter(
-        line => {
-          // ignore SUBJECT with empty URI
-          line.getSubject.getURI.nonEmpty
-        })
+      .filter(line => line.getSubject.getURI.nonEmpty) // ignore SUBJECT with empty URI
       .map(line => {
-        // SUBJECT, PREDICATE and OBJECT
-        val getSubject = line.getSubject
-        val getPredicate = line.getPredicate
-        val getObject = line.getObject
+          // SUBJECT, PREDICATE and OBJECT
+          val getSubject = line.getSubject
+          val getPredicate = line.getPredicate
+          val getObject = line.getObject
 
-        var filteredPredicate: Any = getPredicate
-        var filteredObject: Any = ()
+          var filteredPredicate: Any = getPredicate
+          var filteredObject: Any = getObject
 
-        // filter out PREDICATE
-        if (getPredicate.isURI && getPredicate.getURI.contains(this.symbol("hash"))) {
-          filteredPredicate = getPredicate.getURI.split(this.symbol("hash"))(1)
+          // set: PREDICATE
+          if (getPredicate.isURI && getPredicate.getURI.contains(this.symbol("hash"))) {
+              filteredPredicate = getPredicate.getURI.split(this.symbol("hash"))(1)
 
-          // filter out OBJECT where PREDICATE is a "type"
-          if (filteredPredicate.equals("type") && getObject.isURI && getObject.getURI.contains(this.symbol("hash"))) {
-            filteredObject = this.symbol("colon") + getObject.getURI.split(this.symbol("hash"))(1)
-          } else if (!getObject.isURI) {
-            filteredObject = getObject
+              // set: OBJECT where PREDICATE is a "type"
+              if (filteredPredicate.equals("type") && getObject.isURI && getObject.getURI.contains(this.symbol("hash")))
+                  filteredObject = this.symbol("colon") + getObject.getURI.split(this.symbol("hash"))(1)
+              else if (getObject.isURI)
+                  filteredObject = this.symbol("less-than") + getObject + this.symbol("greater-than")
+              else
+                  filteredObject = getObject
+
+              // add colon at the start
+              filteredPredicate = this.symbol("colon") + filteredPredicate
           } else {
-            filteredObject = this.symbol("less-than") + getObject + this.symbol("greater-than")
-          }
-        }
+              // PREDICATE
+              if (getPredicate.isURI)
+                  filteredPredicate = this.symbol("less-than") + getPredicate + this.symbol("greater-than")
 
-        // (K,V) pair
-        (
-          this.symbol("less-than") + getSubject + this.symbol("greater-than"),
-          this.symbol("colon") + filteredPredicate + this.symbol("space") + filteredObject + this.symbol("space"))
+              // OBJECT
+              if (getObject.isURI)
+                  filteredObject = this.symbol("less-than") + getObject + this.symbol("greater-than")
+          }
+
+          // (K,V) pair
+          (
+              this.symbol("less-than") + getSubject + this.symbol("greater-than"),
+              filteredPredicate + this.symbol("space") + filteredObject + this.symbol("space")
+          )
       })
       .reduceByKey(_ + _) // group based on key
       .sortBy(x => x._1) // sort by key
