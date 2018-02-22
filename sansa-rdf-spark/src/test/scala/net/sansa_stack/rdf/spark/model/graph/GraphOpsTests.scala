1315d616562e2184725bce3a67cae4d59456849f
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-02-22 14:40:00 +0100

    Add GraphOpsTests suite, related to #23

---
+++ sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/model/graph/GraphOpsTests.scala
@@ -0,0 +1,37 @@
+package net.sansa_stack.rdf.spark.model.graph
+import org.scalatest.FunSuite
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.jena.riot.Lang
+import net.sansa_stack.rdf.spark.io.rdf._
+
+class GraphOpsTests extends FunSuite with DataFrameSuiteBase {
+
+  import net.sansa_stack.rdf.spark.graph._
+
+  test("loading N-Triples file into Graph should result in size 7") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val graph = triples.asGraph()
+    val size = graph.size()
+
+    assert(size == 7)
+  }
+
+  test("conversation of Graph into RDD should result in 9 triples") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val graph = triples.asGraph()
+
+    val graph2rdd = graph.toRDD()
+
+    val cnt = graph2rdd.count()
+    assert(cnt == 7)
+  }
+
+}
