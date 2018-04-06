a9150accc640a55e9d14b8f2f4c1452d7fc36340
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-04-06 14:03:24 +0200

    Add further unit tests for TripleOps

--- sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/model/rdd/TripleOpsTests.scala
+++ sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/model/rdd/TripleOpsTests.scala
@@ -4,6 +4,7 @@ import org.scalatest.FunSuite
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.apache.jena.riot.Lang
 import net.sansa_stack.rdf.spark.io._
+import org.apache.jena.graph.{ Node, Triple, NodeFactory }
 
 class TripleOpsTests extends FunSuite with DataFrameSuiteBase {
 
@@ -92,8 +93,8 @@ class TripleOpsTests extends FunSuite with DataFrameSuiteBase {
 
     assert(size == 0)
   }
-  
-   test("filtering objects which are literalsshould result in size 9") {
+
+  test("filtering objects which are literals should result in size 9") {
     val path = getClass.getResource("/loader/data.nt").getPath
     val lang: Lang = Lang.NTRIPLES
 
@@ -104,6 +105,119 @@ class TripleOpsTests extends FunSuite with DataFrameSuiteBase {
 
     assert(size == 0)
   }
-   
+
+  test("union of two RDF graph should result in size 18") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val other = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val graph = triples.union(other)
+
+    val size = graph.count()
+
+    assert(size == 18)
+  }
+
+  test("difference of two RDF graph should result in size 2") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val other = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val graph = triples.difference(other)
+
+    val size = graph.count()
+
+    assert(size == 2)
+  }
+
+  test("intersection of two RDF graph should result in size 6") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val other = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val graph = triples.intersection(other)
+
+    val size = graph.count()
+
+    assert(size == 6)
+  }
+
+  test("add a statement to the RDF graph should result in size 10") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triple = Triple.create(
+      NodeFactory.createURI("<http://dbpedia.org/resource/Guy_de_Maupassant>"),
+      NodeFactory.createURI("<http://xmlns.com/foaf/0.1/givenName>"),
+      NodeFactory.createLiteral("Guy De"))
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val graph = triples.add(triple)
+
+    val size = graph.count()
+
+    assert(size == 10)
+  }
+
+  test("add a list of statements to the RDF graph should result in size 12") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triple1 = Triple.create(
+      NodeFactory.createURI("<http://dbpedia.org/resource/Guy_de_Maupassant>"),
+      NodeFactory.createURI("<http://xmlns.com/foaf/0.1/givenName>"),
+      NodeFactory.createLiteral("Guy De"))
+
+    val triple2 = Triple.create(
+      NodeFactory.createURI("<http://dbpedia.org/resource/Guy_de_Maupassant>"),
+      NodeFactory.createURI("<http://dbpedia.org/ontology/influenced>"),
+      NodeFactory.createURI("<http://dbpedia.org/resource/Tobias_Wolff>"))
+
+    val triple3 = Triple.create(
+      NodeFactory.createURI("<http://dbpedia.org/resource/Guy_de_Maupassant>"),
+      NodeFactory.createURI("<http://xmlns.com/foaf/0.1/givenName>"),
+      NodeFactory.createURI("<http://dbpedia.org/resource/Henry_James>"))
+
+    val statements = Seq(triple1, triple2, triple3)
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    val graph = triples.addAll(statements)
+
+    val size = graph.count()
+
+    assert(size == 12)
+  }
+
+  test("remove a statement from the RDF graph should result in size 9") {
+    val path = getClass.getResource("/loader/data.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triple = Triple.create(
+      NodeFactory.createURI("<http://example.org/show/218>"),
+      NodeFactory.createURI("<http://www.w3.org/2000/01/rdf-schema#label>"),
+      NodeFactory.createLiteral("That Seventies Show"))
+
+    val triples = spark.rdf(lang, allowBlankLines = true)(path)
+
+    triples.foreach(println)
+
+    val graph = triples.remove(triple)
+    graph.foreach(println)
+
+    val size = graph.count()
+
+    assert(size == 9)
+  }
 
 }
