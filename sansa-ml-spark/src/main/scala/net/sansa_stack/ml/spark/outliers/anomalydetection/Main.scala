771333fe1f1eb8ea8df219110eb581a2a90c6db4
Rajjat Dadwal <Rajjat@users.noreply.github.com>
2018-05-19 12:50:59 +0200

    Update Main.scala
    Main class with four different implementation of Anomaly detection

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/outliers/anomalydetection/Main.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/outliers/anomalydetection/Main.scala
@@ -1,15 +1,20 @@
 package net.sansa_stack.ml.spark.outliers.anomalydetection
 
-import net.sansa_stack.rdf.spark.io.NTripleReader
+import _root_.net.sansa_stack.rdf.spark.io.NTripleReader
 import java.net.{ URI => JavaURI }
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql._
 import scopt.OptionParser
+import org.apache.spark.rdd.RDD
+import org.apache.jena.graph.Triple
 import org.apache.log4j.Logger
 import java.nio.file._
 import java.nio.file.attribute.BasicFileAttributes
 import java.io.IOException
 import java.util.concurrent.TimeUnit
+import org.apache.jena.riot.Lang
+import org.apache.spark.storage.StorageLevel
+
 object Main {
   @transient lazy val consoleLog: Logger = Logger.getLogger(getClass.getName)
   def main(args: Array[String]): Unit = {
@@ -20,25 +25,28 @@ object Main {
 
     parser.parse(args, Config()) match {
       case Some(config) =>
-        run(config.in, config.threshold, config.anomalyListLimit, config.numofpartition, config.out)
+        run(config.in, config.threshold, config.anomalyListLimit, config.numofpartition, config.out, config.optionChange)
       case None =>
         consoleLog.warn(parser.usage)
     }
   }
 
-  case class Config(in: String = "", threshold: Double = 0.0, anomalyListLimit: Int = 0, numofpartition: Int = 0, out: String = "")
+  case class Config(in: String = "", threshold: Double = 0.0, anomalyListLimit: Int = 0, numofpartition: Int = 0, out: String = "", optionChange: Int = 0)
 
   val parser: OptionParser[Config] = new scopt.OptionParser[Config]("SANSA -Outlier Detection") {
     head("Detecting Numerical Outliers in dataset")
 
+    //input file path
     opt[String]('i', "input").required().valueName("<path>").
       action((x, c) => c.copy(in = x)).
       text("path to file that contains RDF data (in N-Triples format)")
 
+    //Jaccard similarity threshold value
     opt[Double]('t', "threshold").required().
       action((x, c) => c.copy(threshold = x)).
       text("the Jaccard Similarity value")
 
+    //number of partition
     opt[Int]('a', "numofpartition").required().
       action((x, c) => c.copy(numofpartition = x)).
       text("Number of partition")
@@ -47,9 +55,15 @@ object Main {
       action((x, c) => c.copy(anomalyListLimit = x)).
       text("the outlier List Limit")
 
+    //List limit for calculating IQR
     opt[String]('o', "output").required().valueName("<directory>").
       action((x, c) => c.copy(out = x)).
       text("the output directory")
+
+    //option for changing class
+    opt[Int]('z', "optionChange").required().
+      action((x, c) => c.copy(optionChange = x)).
+      text("Option Number for class")
 
   }
   // remove path files
@@ -69,21 +83,25 @@ object Main {
     }
   }
 
-  def run(input: String, JSimThreshold: Double, anomalyListLimit: Int, numofpartition: Int, output: String): Unit = {
+  def run(input: String, JSimThreshold: Double, anomalyListLimit: Int, numofpartition: Int, output: String, optionChange: Int): Unit = {
+
     removePathFiles(Paths.get(output))
+
     val sparkSession = SparkSession.builder
-    //  .master("spark://172.18.160.16:3077")
-      .master("local[*]")
+      .master("spark://172.18.160.16:3077")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.sql.shuffle.partitions", "400")
+      .config("spark.sql.autoBroadcastJoinThreshold", "304857600")
+      .config("spark.executor.overhead.memory", "2048")
+      .config("spark.driver.overhead.memory", "2048")
       .appName("Anomaly Detection")
       .getOrCreate()
-    println("JSimThreshold=" + JSimThreshold)
-    println("anomalyListLimit=" + anomalyListLimit)
-    println("input=" + input)
-    println("number of partition" + numofpartition)
-    
+
+    val wikiList = List("wikiPageRevisionID,wikiPageID")
+
     //N-Triples Reader
-    val triplesRDD = NTripleReader.load(sparkSession, JavaURI.create(input)).repartition(numofpartition).persist()
+    val triplesRDD = NTripleReader.load(sparkSession, JavaURI.create(input)).repartition(numofpartition)
+    triplesRDD.persist()
 
     val objList = List("http://www.w3.org/2001/XMLSchema#double",
       "http://www.w3.org/2001/XMLSchema#integer",
@@ -105,34 +123,89 @@ object Main {
     //hypernym URI                      
     val hypernym = "http://purl.org/linguistics/gold/hypernym"
 
-    val startTime = System.nanoTime()
+    var clusterOfSubject: RDD[(Set[(String, String, Object)])] = null
 
-    val outDetection = new AnomalyDetection(triplesRDD, objList, triplesType, JSimThreshold, listSuperType, sparkSession, hypernym, numofpartition)
+    if (optionChange == 0) {
 
-    //run method will return the KV pair-Key as property and value as clustered numerical triples
-    //example: population,Set(India,population,200000000,Germany,population,800000,........)
-    val clusterOfSubject = outDetection.run()
+      println("you are in AnomalyDetection-using CountVectorizerModel,ApproxSimilarity")
+      val outDetection = new AnomalyDetectionWithCountVetcorizerModel(triplesRDD, objList, triplesType, JSimThreshold, listSuperType, sparkSession, hypernym, numofpartition)
+      val startTime = System.nanoTime()
+      clusterOfSubject = outDetection.run()
 
-    clusterOfSubject.take(10).foreach(println)
-    
-    val setData = clusterOfSubject.repartition(numofpartition).persist.map(f => f._2.toSeq)
+      val setData = clusterOfSubject.repartition(1000).persist(StorageLevel.MEMORY_AND_DISK)
+      val setDataStore = setData.map(f => f.toSeq)
 
-    //calculating IQR and saving output to the file
-    val listofDataArray = setData.collect()
-    var a: Dataset[Row] = null
+      val setDataSize = setDataStore.filter(f => f.size > anomalyListLimit)
+      val test = setDataSize.map(f => outDetection.iqr2(f, anomalyListLimit))
+      val testfilter = test.filter(f => f.size > 0).distinct()
+      val inputTrim = input.trim()
+      testfilter.coalesce(1).saveAsTextFile(output + "_" + optionChange + "_" + input.substring(input.lastIndexOf("/") + 1))
 
-    for (listofDatavalue <- listofDataArray) {
-      a = outDetection.iqr1(listofDatavalue, anomalyListLimit)
-      if (a != null)
-        a.select("dt").coalesce(1).write.format("text").mode(SaveMode.Append) save (output)
+      setData.unpersist()
+      runTime(System.nanoTime() - startTime)
+
+    } else if (optionChange == 1) {
+
+      println("you are in AnomalyWithHashingTF-using HashingTF,approxSimilarityJoin ")
+      val outDetection1 = new AnomalyWithHashingTF(triplesRDD, objList, triplesType, JSimThreshold, listSuperType, sparkSession, hypernym, numofpartition)
+      val startTime = System.nanoTime()
+      clusterOfSubject = outDetection1.run()
+
+      val setData = clusterOfSubject.repartition(1000).persist(StorageLevel.MEMORY_AND_DISK)
+      val setDataStore = setData.map(f => f.toSeq)
+
+      val setDataSize = setDataStore.filter(f => f.size > anomalyListLimit)
+
+      val test = setDataSize.map(f => outDetection1.iqr2(f, anomalyListLimit))
+      test.first
+      
+      val testfilter = test.filter(f => f.size > 0) //.distinct()
+       val testfilterDistinct = testfilter.flatMap(f => f)
+      testfilterDistinct.saveAsTextFile(output)
+      setData.unpersist()
+      runTime(System.nanoTime() - startTime)
+
+    }  else if (optionChange == 2) {
+      println("Cartesian product using dataframe crossJoin")
+      val outDetection2 = new AnomalWithDataframe(triplesRDD, objList, triplesType, JSimThreshold, listSuperType, sparkSession, hypernym, numofpartition)
+      val startTime = System.nanoTime()
+      clusterOfSubject = outDetection2.run()
+      val setData = clusterOfSubject.repartition(1000).persist(StorageLevel.MEMORY_AND_DISK)
+      val setDataStore = setData.map(f => f.toSeq)
+
+      val setDataSize = setDataStore.filter(f => f.size > anomalyListLimit)
+      val test = setDataSize.map(f => outDetection2.iqr2(f, anomalyListLimit))
+      val testfilter = test.filter(f => f.size > 0).distinct()
+      testfilter.coalesce(1).saveAsTextFile(output + "_" + optionChange + "_" + input.substring(input.lastIndexOf("/") + 1))
+
+      setData.unpersist()
+      runTime(System.nanoTime() - startTime)
+
     }
-    setData.unpersist()
-    runTime(System.nanoTime() - startTime)
-    // scala.io.StdIn.readLine()
+    else if (optionChange == 3) {
+      println("Implementation with collect-tested with big file-Not working")
+      val outDetection2 = new AnomalydetectWithCollect(triplesRDD, objList, triplesType,
+                              JSimThreshold, listSuperType, sparkSession, hypernym, numofpartition, anomalyListLimit)
+      val startTime = System.nanoTime()
+      clusterOfSubject = outDetection2.run()
+      val setData = clusterOfSubject.repartition(1000).persist(StorageLevel.MEMORY_AND_DISK)
+      val setDataStore = setData.map(f => f.toSeq)
+
+      val setDataSize = setDataStore.filter(f => f.size > anomalyListLimit)
+      val test = setDataSize.map(f => outDetection2.iqr2(f, anomalyListLimit))
+      val testfilter = test.filter(f => f.size > 0).distinct()
+      testfilter.coalesce(1).saveAsTextFile(output + "_" + optionChange + "_" + input.substring(input.lastIndexOf("/") + 1))
+
+      setData.unpersist()
+      runTime(System.nanoTime() - startTime)
+
+    }
+
+   
     sparkSession.stop()
 
   }
-  
+
   def runTime(processedTime: Long): Unit = {
     val milliseconds = TimeUnit.MILLISECONDS.convert(processedTime, TimeUnit.NANOSECONDS)
     val seconds = Math.floor(milliseconds / 1000d + .5d).toInt
@@ -150,5 +223,10 @@ object Main {
       }
     }
   }
-
+  def searchWiki(x: String, y: List[String]): Boolean = {
+    if (y.exists(x.contains)) {
+      true
+    } else
+      false
+  }
 }
