8faceb99c43b1973101e314f6708d06677b4788a
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-17 01:44:47 +0200

    Refactor reprconciseness metrics and make Scala-style compliant

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/metrics/reprconciseness/ShortURIs.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/metrics/reprconciseness/ShortURIs.scala
@@ -1,33 +1,29 @@
 package net.sansa_stack.rdf.spark.qualityassessment.metrics.reprconciseness
 
-import org.apache.spark.rdd.RDD
-import org.apache.jena.graph.{ Triple, Node }
 import net.sansa_stack.rdf.spark.qualityassessment.utils.NodeUtils._
+import org.apache.jena.graph.{ Node, Triple }
+import org.apache.spark.rdd.RDD
 
 /**
  * @author Gezim Sejdiu
  */
 object ShortURIs {
-  implicit class ShortURIsFunctions(dataset: RDD[Triple]) extends Serializable {
 
-    /**
-     * This metric calculates the number of long URIs.
-     * It computes the ratio between the number of all long URIs
-     * and the total number of URIs on the dataset.
-     */
-    def assessShortURIs() = {
+  /**
+   * This metric calculates the number of long URIs.
+   * It computes the ratio between the number of all long URIs
+   * and the total number of URIs on the dataset.
+   */
+  def assessShortURIs(dataset: RDD[Triple]): Double = {
 
-      val entities = dataset.filter(_.getSubject.isURI()).map(_.getSubject)
-        .union(dataset.filter(_.getPredicate.isURI()).map(_.getPredicate))
-        .union(dataset.filter(_.getObject.isURI()).map(_.getObject))
-        .distinct()
-      entities.cache()
+    val entities = dataset.filter(_.getSubject.isURI()).map(_.getSubject)
+      .union(dataset.filter(_.getPredicate.isURI()).map(_.getPredicate))
+      .union(dataset.filter(_.getObject.isURI()).map(_.getObject))
+      .distinct()
 
-      val allEntities = entities.count
-      val shorestURIs = entities.filter(resourceTooLong(_)).count().toDouble
+    val allEntities = entities.count
+    val shorestURIs = entities.filter(resourceTooLong(_)).count().toDouble
 
-      if (allEntities >= 0.0) shorestURIs / allEntities else 0
-    }
-
+    if (allEntities >= 0.0) shorestURIs / allEntities else 0
   }
 }
