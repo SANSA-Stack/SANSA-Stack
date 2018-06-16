3876b0a414a53245ed87f00d1b048469c7267501
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-17 01:43:19 +0200

    Refactor interlinking metrics and make Scala-style compliant

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/metrics/interlinking/ExternalSameAsLinks.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/metrics/interlinking/ExternalSameAsLinks.scala
@@ -1,9 +1,9 @@
 package net.sansa_stack.rdf.spark.qualityassessment.metrics.interlinking
 
-import org.apache.spark.rdd.RDD
-import org.apache.jena.graph.{ Triple, Node }
 import net.sansa_stack.rdf.spark.qualityassessment.utils.NodeUtils._
-import net.sansa_stack.rdf.spark.utils.StatsPrefixes._
+import org.apache.jena.graph.{ Node, Triple }
+import org.apache.jena.vocabulary.OWL
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -11,34 +11,31 @@ import org.apache.spark.sql.SparkSession
  */
 object ExternalSameAsLinks {
 
-  implicit class ExternalSameAsLinksFunctions(dataset: RDD[Triple]) extends Serializable {
-    // @transient var spark: SparkSession = _
-    @transient var spark: SparkSession = SparkSession.builder.getOrCreate()
-  
-    def assessExternalSameAsLinks() = {
+  @transient var spark: SparkSession = SparkSession.builder.getOrCreate()
 
-      val sameAsTriples = dataset.filter(f => checkLiteral(f.getPredicate).equals(OWL_SAME_AS))
+  def assessExternalSameAsLinks(dataset: RDD[Triple]): Double = {
 
-      val triples = dataset.count().toDouble
+    val sameAsTriples = dataset.filter(f => checkLiteral(f.getPredicate).equals(OWL.sameAs))
 
-      val sameAsCount = spark.sparkContext.longAccumulator("sameAsCount")
+    val triples = dataset.count().toDouble
 
-      sameAsTriples.filter(_.getObject.isURI()).foreach { f =>
-        // if <local> owl:sameAs <external> or <external> owl:sameAs <local>
-        val subjectIsLocal = isInternal(f.getSubject)
-        val objectIsExternal = isExternal(f.getObject)
-        
-        if ((subjectIsLocal && objectIsExternal)
-          || (!subjectIsLocal && !objectIsExternal)) {
-          sameAsCount.add(1)
-        }
+    val sameAsCount = spark.sparkContext.longAccumulator("sameAsCount")
+
+    sameAsTriples.filter(_.getObject.isURI()).foreach { f =>
+      // if <local> owl:sameAs <external> or <external> owl:sameAs <local>
+      val subjectIsLocal = isInternal(f.getSubject)
+      val objectIsExternal = isExternal(f.getObject)
+
+      if ((subjectIsLocal && objectIsExternal)
+        || (!subjectIsLocal && !objectIsExternal)) {
+        sameAsCount.add(1)
       }
-
-      val value = if (triples > 0.0)
-        sameAsCount.value / triples;
-      else 0
-
-      value
     }
+
+    val value = if (triples > 0.0) {
+      sameAsCount.value / triples
+    } else 0
+
+    value
   }
 }
