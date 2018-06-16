bace6b9e618861fe9ccfb2c6d068f44457e133f3
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-17 00:25:33 +0200

    Refactor completeness metrics and make Scala-style compliant

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/metrics/completeness/PropertyCompleteness.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/metrics/completeness/PropertyCompleteness.scala
@@ -1,43 +1,41 @@
 package net.sansa_stack.rdf.spark.qualityassessment.metrics.completeness
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.rdd.RDD
-import org.apache.jena.graph.{ Triple, Node }
 import net.sansa_stack.rdf.spark.qualityassessment.utils.DatasetUtils._
+import org.apache.jena.graph.{ Node, Triple }
+import org.apache.spark.rdd.RDD
 
 /**
  * @author Gezim Sejdiu
  */
 object PropertyCompleteness {
-  implicit class PropertyCompletenessFunctions(dataset: RDD[Triple]) extends Serializable {
-    /**
-     * This metric measures the property completeness by checking
-     * the missing object values for the given predicate and given class of subjects.
-     * A user specifies the RDF class and the RDF predicate, then it checks for each pair
-     * whether instances of the given RDF class contain the specified RDF predicate.
-     */
-    def assessPropertyCompleteness() = {
 
-      /*
-     -->Rule->Filter-->
-   		"select (?s) where ?s p=rdf:type o=Type; p2=Property ?o2
-			select (?s2) where ?s2 p=rdf:type o=Type"
-			-->Action-->
-			S+=?s && S2+=?s2
-			-->Post-processing-->
-			|S| / |S2|
+  /**
+   * This metric measures the property completeness by checking
+   * the missing object values for the given predicate and given class of subjects.
+   * A user specifies the RDF class and the RDF predicate, then it checks for each pair
+   * whether instances of the given RDF class contain the specified RDF predicate.
    */
-      val s2 = dataset.filter(f =>
-        f.getPredicate.getLocalName.contains("type")
-          && f.getObject.getLiteralLexicalForm.contains(subject)).cache()
-      val s = s2.filter(_.getPredicate.getLiteralLexicalForm.contains(property))
+  def assessPropertyCompleteness(dataset: RDD[Triple]): Long = {
 
-      val S = s.map(_.getSubject).distinct().count()
-      val S2 = s2.map(_.getSubject).distinct().count()
+    /**
+     * -->Rule->Filter-->
+     * "select (?s) where ?s p=rdf:type o=Type; p2=Property ?o2
+     * select (?s2) where ?s2 p=rdf:type o=Type"
+     * -->Action-->
+     * S+=?s && S2+=?s2
+     * -->Post-processing-->
+     * |S| / |S2|
+     */
+    val s2 = dataset.filter(f =>
+      f.getPredicate.getLocalName.contains("type")
+        && f.getObject.getLiteralLexicalForm.contains(subject)).cache()
+    val s = s2.filter(_.getPredicate.getLiteralLexicalForm.contains(property))
 
-      if (S2 > 0) S / S2
-      else 0
-    }
+    val S = s.map(_.getSubject).distinct().count()
+    val S2 = s2.map(_.getSubject).distinct().count()
 
+    if (S2 > 0) S / S2
+    else 0
   }
+
 }
--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/package.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/qualityassessment/package.scala
@@ -1,16 +1,14 @@
 
 package net.sansa_stack.rdf.spark
 
-import org.apache.spark.rdd.RDD
+import net.sansa_stack.rdf.spark.qualityassessment.metrics.availability.DereferenceableUris
+import net.sansa_stack.rdf.spark.qualityassessment.metrics.completeness._
 import org.apache.jena.graph.{ Triple, Node }
-import net.sansa_stack.rdf.spark.qualityassessment.metrics.completeness.InterlinkingCompleteness._
-import net.sansa_stack.rdf.spark.qualityassessment.metrics.completeness.PropertyCompleteness._
-import net.sansa_stack.rdf.spark.qualityassessment.metrics.completeness.SchemaCompleteness._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 
 import net.sansa_stack.rdf.spark.qualityassessment.metrics.syntacticvalidity.LiteralNumericRangeChecker._
 import net.sansa_stack.rdf.spark.qualityassessment.metrics.syntacticvalidity.XSDDatatypeCompatibleLiterals._
-
-import net.sansa_stack.rdf.spark.qualityassessment.metrics.availability.DereferenceableUris._
 
 import net.sansa_stack.rdf.spark.qualityassessment.metrics.relevancy.CoverageDetail._
 import net.sansa_stack.rdf.spark.qualityassessment.metrics.relevancy.CoverageScope._
@@ -24,24 +22,72 @@ import net.sansa_stack.rdf.spark.qualityassessment.metrics.licensing.HumanReadab
 import net.sansa_stack.rdf.spark.qualityassessment.metrics.licensing.MachineReadableLicense._
 
 import net.sansa_stack.rdf.spark.qualityassessment.metrics.conciseness.ExtensionalConciseness._
-import org.apache.spark.sql.SparkSession
 
 /**
  * @author Gezim Sejdiu
  */
 package object qualityassessment {
-  
-  implicit def AssessInterlinkingCompletenessFunctions(dataset: RDD[Triple]) = new InterlinkingCompletenessFunctions(dataset)
 
-  implicit def AssessPropertyCompletenessFunctions(dataset: RDD[Triple]) = new PropertyCompletenessFunctions(dataset)
+  implicit class QualityAssessmentReader(triples: RDD[Triple]) {
 
-  implicit def AssessSchemaCompletenessFunctions(dataset: RDD[Triple]) = new SchemaCompletenessFunctions(dataset)
+    /**
+     * This metric calculates the number of valid redirects of URI.
+     * It computes the ratio between the number of all valid redirects
+     * (subject + predicates + objects)a.k.a dereferencedURIS and
+     * the total number of URIs on the dataset.
+     */
+    def assessDereferenceableUris() =
+      DereferenceableUris.assessDereferenceableUris(triples)
+
+    /**
+     * This metric measures the extent to which a resource includes
+     * all triples from the dataset that have the resource's URI as the object.
+     * The ratio computed is the number of objects that are "back-links"
+     * (are part of the resource's URI) and the total number of objects.
+     */
+    def assessDereferenceableBackLinks() =
+      DereferenceableUris.assessDereferenceableBackLinks(triples)
+
+    /**
+     * This metric measures the extent to which a resource includes
+     * all triples from the dataset that have the resource's URI as the subject.
+     * The ratio computed is the number of subjects that are "forward-links"
+     * (are part of the resource's URI) and the total number of subjects.
+     */
+    def assessDereferenceableForwardLinks() =
+      DereferenceableUris.assessDereferenceableForwardLinks(triples)
+
+    /**
+     * This metric measures the interlinking completeness. Since any resource of a
+     * dataset can be interlinked with another resource of a foreign dataset this
+     * metric makes a statement about the ratio of interlinked resources to
+     * resources that could potentially be interlinked.
+     */
+    def assessInterlinkingCompleteness() =
+      InterlinkingCompleteness.assessInterlinkingCompleteness(triples)
+
+    /**
+     * This metric measures the property completeness by checking
+     * the missing object values for the given predicate and given class of subjects.
+     * A user specifies the RDF class and the RDF predicate, then it checks for each pair
+     * whether instances of the given RDF class contain the specified RDF predicate.
+     */
+    def assessPropertyCompleteness() =
+      PropertyCompleteness.assessPropertyCompleteness(triples)
+
+    /**
+     * This metric measures the ratio of the number of classes and relations
+     * of the gold standard existing in g, and the number of classes and
+     * relations in the gold standard.
+     */
+    def assessSchemaCompleteness() =
+      SchemaCompleteness.assessSchemaCompleteness(triples)
+
+  }
 
   implicit def AssessLiteralNumericRangeCheckerFunctions(dataset: RDD[Triple]) = new LiteralNumericRangeCheckerFunctions(dataset)
 
   implicit def AssessXSDDatatypeCompatibleLiteralsFunctions(dataset: RDD[Triple]) = new XSDDatatypeCompatibleLiteralsFunctions(dataset)
-
-  implicit def AssessDereferenceableUrisFunctions(dataset: RDD[Triple]) = new DereferenceableUrisFunctions(dataset)
 
   implicit def AssessCoverageDetailFunctions(dataset: RDD[Triple]) = new CoverageDetailFunctions(dataset)
   implicit def AssessCoverageScopeFunctions(dataset: RDD[Triple]) = new CoverageScopeFunctions(dataset)
@@ -55,10 +101,7 @@ package object qualityassessment {
 
   implicit def AssessHumanReadableLicenseFunctions(dataset: RDD[Triple]) = new HumanReadableLicenseFunctions(dataset)
   implicit def AssessMachineReadableLicenseFunctions(dataset: RDD[Triple]) = new MachineReadableLicenseFunctions(dataset)
-  
+
   implicit def AssessExtensionalConcisenessFunctions(dataset: RDD[Triple]) = new ExtensionalConcisenessFunctions(dataset)
-  
-  
-  
 
 }
