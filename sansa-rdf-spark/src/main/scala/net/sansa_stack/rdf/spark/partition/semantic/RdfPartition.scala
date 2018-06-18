e8a649a63e4b5c076fe39dd791418962ece7cc52
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-18 21:23:29 +0200

    Make semantic partition strategie Scala-style compliant

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/semantic/RdfPartition.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/semantic/RdfPartition.scala
@@ -1,6 +1,7 @@
 package net.sansa_stack.rdf.spark.partition.semantic
 
 import java.util.concurrent.TimeUnit
+
 import org.apache.jena.graph.Triple
 import org.apache.spark.rdd._
 
@@ -11,8 +12,8 @@ import org.apache.spark.rdd._
  * @return - semantic partition data.
  */
 class RdfPartition(
-  symbol:              Map[String, String],
-  nTriplesRDD:         RDD[Triple],
+  symbol: Map[String, String],
+  nTriplesRDD: RDD[Triple],
   partitionedDataPath: String,
   numOfFilesPartition: Int) extends Serializable {
 
@@ -23,43 +24,45 @@ class RdfPartition(
       .distinct
       .filter(line => line.getSubject.getURI.nonEmpty) // ignore SUBJECT with empty URI
       .map(line => {
-          // SUBJECT, PREDICATE and OBJECT
-          val getSubject = line.getSubject
-          val getPredicate = line.getPredicate
-          val getObject = line.getObject
+        // SUBJECT, PREDICATE and OBJECT
+        val getSubject = line.getSubject
+        val getPredicate = line.getPredicate
+        val getObject = line.getObject
 
-          var filteredPredicate: Any = getPredicate
-          var filteredObject: Any = getObject
+        var filteredPredicate: Any = getPredicate
+        var filteredObject: Any = getObject
 
-          // set: PREDICATE
-          if (getPredicate.isURI && getPredicate.getURI.contains(this.symbol("hash"))) {
-              filteredPredicate = getPredicate.getURI.split(this.symbol("hash"))(1)
+        // set: PREDICATE
+        if (getPredicate.isURI && getPredicate.getURI.contains(this.symbol("hash"))) {
+          filteredPredicate = getPredicate.getURI.split(this.symbol("hash"))(1)
 
-              // set: OBJECT where PREDICATE is a "type"
-              if (filteredPredicate.equals("type") && getObject.isURI && getObject.getURI.contains(this.symbol("hash")))
-                  filteredObject = this.symbol("colon") + getObject.getURI.split(this.symbol("hash"))(1)
-              else if (getObject.isURI)
-                  filteredObject = this.symbol("less-than") + getObject + this.symbol("greater-than")
-              else
-                  filteredObject = getObject
-
-              // add colon at the start
-              filteredPredicate = this.symbol("colon") + filteredPredicate
+          // set: OBJECT where PREDICATE is a "type"
+          if (filteredPredicate.equals("type") && getObject.isURI && getObject.getURI.contains(this.symbol("hash"))) {
+            filteredObject = this.symbol("colon") + getObject.getURI.split(this.symbol("hash"))(1)
+          } else if (getObject.isURI) {
+            filteredObject = this.symbol("less-than") + getObject + this.symbol("greater-than")
           } else {
-              // PREDICATE
-              if (getPredicate.isURI)
-                  filteredPredicate = this.symbol("less-than") + getPredicate + this.symbol("greater-than")
-
-              // OBJECT
-              if (getObject.isURI)
-                  filteredObject = this.symbol("less-than") + getObject + this.symbol("greater-than")
+            filteredObject = getObject
           }
 
-          // (K,V) pair
-          (
-              this.symbol("less-than") + getSubject + this.symbol("greater-than"),
-              filteredPredicate + this.symbol("space") + filteredObject + this.symbol("space")
-          )
+          // add colon at the start
+          filteredPredicate = this.symbol("colon") + filteredPredicate
+        } else {
+          // PREDICATE
+          if (getPredicate.isURI) {
+            filteredPredicate = this.symbol("less-than") + getPredicate + this.symbol("greater-than")
+          }
+
+          // OBJECT
+          if (getObject.isURI) {
+            filteredObject = this.symbol("less-than") + getObject + this.symbol("greater-than")
+          }
+        }
+
+        // (K,V) pair
+        (
+          this.symbol("less-than") + getSubject + this.symbol("greater-than"),
+          filteredPredicate + this.symbol("space") + filteredObject + this.symbol("space"))
       })
       .reduceByKey(_ + _) // group based on key
       .sortBy(x => x._1) // sort by key
@@ -72,8 +75,8 @@ class RdfPartition(
 
 object RdfPartition {
   def apply(
-    symbol:              Map[String, String],
-    nTriplesRDD:         RDD[Triple],
+    symbol: Map[String, String],
+    nTriplesRDD: RDD[Triple],
     partitionedDataPath: String,
-    numOfFilesPartition: Int) = new RdfPartition(symbol, nTriplesRDD, partitionedDataPath, numOfFilesPartition)
+    numOfFilesPartition: Int): RdfPartition = new RdfPartition(symbol, nTriplesRDD, partitionedDataPath, numOfFilesPartition)
 }
