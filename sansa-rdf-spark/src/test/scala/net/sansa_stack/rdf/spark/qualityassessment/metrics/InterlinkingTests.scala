ec523e50f8f003b154c233fabf5ddd9f1f0a1c78
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-18 10:58:19 +0200

    Provice basic unit tests for interlinking metrics

---
+++ sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/qualityassessment/metrics/InterlinkingTests.scala
@@ -0,0 +1,23 @@
+package net.sansa_stack.rdf.spark.qualityassessment.metrics
+
+import org.scalatest.FunSuite
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.jena.riot.Lang
+import net.sansa_stack.rdf.spark.io._
+import net.sansa_stack.rdf.spark.qualityassessment._
+
+class InterlinkingTests extends FunSuite with DataFrameSuiteBase {
+  
+   test("assessing the external SameAs links should result in value 0.0") {
+
+    val path = getClass.getResource("/rdf.nt").getPath
+    val lang: Lang = Lang.NTRIPLES
+
+    val triples = spark.rdf(lang)(path)
+
+    val value = triples.assessExternalSameAsLinks()
+    assert(value == 0.0)
+  }
+   
+  
+}
