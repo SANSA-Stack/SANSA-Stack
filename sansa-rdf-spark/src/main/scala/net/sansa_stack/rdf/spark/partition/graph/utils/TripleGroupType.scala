b4216607a37971b65c3ec6aca7055e14553fc656
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-18 21:24:14 +0200

    Make graph partition strategie Scala-style compliant

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/graph/utils/TripleGroupType.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/graph/utils/TripleGroupType.scala
@@ -1,6 +1,6 @@
 package net.sansa_stack.rdf.spark.partition.graph.utils
 
-object TripleGroupType extends Enumeration{
+object TripleGroupType extends Enumeration {
   type TripleGroupType = Value
   val s, o, so = Value
 }
