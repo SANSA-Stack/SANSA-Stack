e46a3f4612dc5137e43db1d6294f3ff36c5c813f
CescWang1991 <cescwang1991@gmail.com>
2018-06-22 21:59:30 +0200

    delete unused resources

--- sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/graph/jena/serialization/Registrator.scala
+++ sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/graph/jena/serialization/Registrator.scala
@@ -5,7 +5,6 @@ import net.sansa_stack.query.spark.graph.jena.Ops
 import net.sansa_stack.query.spark.graph.jena.expression.Expression
 import net.sansa_stack.query.spark.graph.jena.util._
 import org.apache.jena.graph.Node
-import org.apache.jena.graph.Triple
 import org.apache.spark.serializer.KryoRegistrator
 
 /**
@@ -14,10 +13,6 @@ import org.apache.spark.serializer.KryoRegistrator
 class Registrator extends KryoRegistrator{
 
   override def registerClasses(kryo: Kryo): Unit = {
-
-    // Vertex and edge Attributes
-    kryo.register(classOf[Node])
-    kryo.register(classOf[Triple])
 
     // Query triple pattern
     kryo.register(classOf[TriplePattern])
