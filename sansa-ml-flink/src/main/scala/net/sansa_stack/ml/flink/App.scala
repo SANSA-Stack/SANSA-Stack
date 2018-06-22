2a72b06e17c2f3c50dec8d73115800323b19eee2
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-06-22 16:08:46 +0200

    Make flink module Scala-style compliant

--- sansa-ml-flink/src/main/scala/net/sansa_stack/ml/flink/App.scala
+++ sansa-ml-flink/src/main/scala/net/sansa_stack/ml/flink/App.scala
@@ -1,26 +1,30 @@
 package net.sansa_stack.ml.flink
 
 import scala.collection.mutable
+
 import org.apache.flink.api.scala.ExecutionEnvironment
+
 import net.sansa_stack.ml.flink.clustering.RDFByModularityClustering
 
 /**
- * @author ${user.name}
+ * @author Gezim Sejdiu
  */
 object App {
   def main(args: Array[String]) {
-    /* if (args.length < 3) {
-      System.err.println(
-        "Usage: RDFByModularityClustering <input> <output> <numIterations>")
-      System.exit(1)
-    }*/
+    /**
+     * if (args.length < 3) {
+     * System.err.println(
+     * "Usage: RDFByModularityClustering <input> <output> <numIterations>")
+     * System.exit(1)
+     * }
+     */
     val graphFile = "Clustering_sampledata.nt" // args(0)
-    val outputFile = "output" //args(1)
-    val numIterations = 5 //args(2).toInt
+    val outputFile = "output" // args(1)
+    val numIterations = 5 // args(2).toInt
     val optionsList = args.drop(3).map { arg =>
       arg.dropWhile(_ == '-').split('=') match {
         case Array(opt, v) => (opt -> v)
-        case _             => throw new IllegalArgumentException("Invalid argument: " + arg)
+        case _ => throw new IllegalArgumentException("Invalid argument: " + arg)
       }
     }
     val options = mutable.Map(optionsList: _*)
