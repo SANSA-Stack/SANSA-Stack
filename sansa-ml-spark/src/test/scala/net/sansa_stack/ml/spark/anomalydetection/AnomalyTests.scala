b9dfe0000415b5ce5cb6d04501339debfc046c97
HajiraJabeen <hajirajabeen@hotmail.com>
2018-06-25 12:23:20 +0200

    skeleton tests

---
+++ sansa-ml-spark/src/test/scala/net/sansa_stack/ml/spark/anomalydetection/AnomalyTests.scala
@@ -0,0 +1,13 @@
+package net.sansa_stack.ml.spark.anomalydetection
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.scalatest.FunSuite
+import net.sf.extjwnl.data._
+class AnomalyTests extends FunSuite with DataFrameSuiteBase {
+
+  test("Anomaly Detection in RDF") {
+    assert(true)
+    
+  }
+  
+}
---
+++ sansa-ml-spark/src/test/scala/net/sansa_stack/ml/spark/kernel/KernelTests.scala
@@ -0,0 +1,12 @@
+package net.sansa_stack.ml.spark.kernel
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.scalatest.FunSuite
+import net.sf.extjwnl.data._
+class KernelTests extends FunSuite with DataFrameSuiteBase {
+
+  test("Kernel functions for RDF") {
+    assert(true)
+  }
+  
+}
