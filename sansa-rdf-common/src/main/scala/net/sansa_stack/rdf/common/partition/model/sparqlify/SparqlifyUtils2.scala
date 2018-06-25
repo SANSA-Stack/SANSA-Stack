8ed778ccb3da83e2d4afafdd7743c2938e25738d
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-06-25 16:21:30 +0200

    fixes for the latest dep versions

--- sansa-rdf-common/src/main/scala/net/sansa_stack/rdf/common/partition/model/sparqlify/SparqlifyUtils2.scala
+++ sansa-rdf-common/src/main/scala/net/sansa_stack/rdf/common/partition/model/sparqlify/SparqlifyUtils2.scala
@@ -1,20 +1,20 @@
 package net.sansa_stack.rdf.common.partition.model.sparqlify
 
-import java.util.{ ArrayList, Arrays }
+import java.util
 
-import scala.reflect.runtime.universe._
-
+import com.google.common.collect.ImmutableMap
 import net.sansa_stack.rdf.common.partition.core.RdfPartitionDefault
 import org.aksw.jena_sparql_api.utils.Vars
 import org.aksw.jena_sparql_api.views.E_RdfTerm
-import org.aksw.sparqlify.algebra.sql.nodes.SqlOpTable
+import org.aksw.obda.domain.api.Constraint
+import org.aksw.obda.domain.impl.LogicalTableTableName
 import org.aksw.obda.jena.domain.impl.ViewDefinition
-import org.aksw.sparqlify.config.syntax.ViewTemplateDefinition
 import org.apache.jena.graph.NodeFactory
-import org.apache.jena.sparql.core.{ Quad, QuadPattern, Var }
-import org.apache.jena.sparql.expr.{ E_Equals, Expr, ExprVar, NodeValue }
-
+import org.apache.jena.sparql.core.{Quad, Var}
+import org.apache.jena.sparql.expr.{Expr, ExprVar, NodeValue}
+import collection.JavaConverters._
 import scala.language.implicitConversions
+import scala.reflect.runtime.universe._
 
 object SparqlifyUtils2 {
   implicit def newExprVar(varName: String): ExprVar = new ExprVar(Var.alloc(varName))
@@ -32,34 +32,39 @@ object SparqlifyUtils2 {
     // items.foreach(x => println("Item: " + x))
 
     val t = p.layout.schema
-    val attrNames = t.members.sorted.collect({ case m: MethodSymbol if m.isCaseAccessor => m.name.toString }).toList
+    val attrNames = t.members.sorted.collect({ case m: MethodSymbol if m.isCaseAccessor => m.name.toString })
 
     // println("Counting the dataset: " + ds.count())
     val pred = p.predicate
 
     // For now let's just use the full predicate as the uri
     // val predPart = pred.substring(pred.lastIndexOf("/") + 1)
-    val predPart = pred;
+    val predPart = pred
     val pn = NodeFactory.createURI(p.predicate)
 
     val dt = p.datatype
     val dtPart = if (dt != null && !dt.isEmpty) "_" + dt.substring(dt.lastIndexOf("/") + 1) else ""
     val langPart = if (p.langTagPresent) "_lang" else ""
 
-    val tableName = (predPart + dtPart + langPart) // .replace("#", "__").replace("-", "_")
+    val tableName = predPart + dtPart + langPart // .replace("#", "__").replace("-", "_")
 
     val quad = new Quad(Quad.defaultGraphIRI, Vars.s, pn, Vars.o)
-    val quadPattern = new QuadPattern()
-    quadPattern.add(quad)
+    // val quadPattern = new QuadPattern()
+    // quadPattern.add(quad)
 
     val sTerm = createExprForNode(0, attrNames, p.subjectType, "", p.langTagPresent)
     val oTerm = createExprForNode(1, attrNames, p.objectType, p.datatype, p.langTagPresent)
 
-    val se = new E_Equals(new ExprVar(Vars.s), sTerm)
-    val oe = new E_Equals(new ExprVar(Vars.o), oTerm)
-    val varDefs = new ArrayList[Expr] // new ExprList()
-    varDefs.add(se)
-    varDefs.add(oe)
+    // val se = new E_Equals(new ExprVar(Vars.s), sTerm)
+    // val oe = new E_Equals(new ExprVar(Vars.o), oTerm)
+    val varDefs = ImmutableMap.builder[Var, Expr]()
+      .put(Vars.s, sTerm)
+      .put(Vars.o, oTerm)
+      .build()
+
+    // val varDefs = new ArrayList[Expr] // new ExprList()
+    // varDefs.add(se)
+    // varDefs.add(oe)
 
     // val typeMap = basicTableInfo.getRawTypeMap.asScala.map({ case (k, v) => (k, TypeToken.alloc(v)) }).asJava
 
@@ -67,9 +72,9 @@ object SparqlifyUtils2 {
 
     // println("Schema: " + schema)
 
-    val sqlOp = new SqlOpTable(null, tableName)
-    val vtd = new ViewTemplateDefinition(quadPattern, varDefs)
-    val vd = new ViewDefinition(tableName, vtd, sqlOp, Arrays.asList())
+    val logicalTable = new LogicalTableTableName(tableName)
+
+    val vd = new ViewDefinition(tableName, List(quad).asJava, varDefs, new util.HashMap[Var, Constraint], logicalTable)
 
     vd
   }
