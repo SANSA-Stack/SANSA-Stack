ca3386e2008f1e899b8deac4022fb824554146be
mnmami <mohamed.nadjib.mami@gmail.com>
2018-11-12 19:01:14 +0100

    Add a separate Main class to test SANSA-DataLake separately from SANSA- Query; it requires passing 4 run arguments.

---
+++ sansa-datalake-spark/src/main/scala/net/sansa_stack/datalake/spark/Main.scala
@@ -0,0 +1,25 @@
+package net.sansa_stack.datalake.spark
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+/**
+  * Created by mmami on 26.01.17.
+  */
+object Main extends App {
+
+    var queryFile = args(0)
+    val mappingsFile = args(1)
+    val configFile = args(2)
+    val executorID = args(3)
+
+    val spark = SparkSession.builder.master(executorID).appName("Sparkall").getOrCreate
+
+    val hadoopConfig = spark.conf
+
+    val executor : SparkExecutor = new SparkExecutor(spark, mappingsFile)
+    // val finalResults = executor.getType()
+
+    val run = new Run[DataFrame](executor)
+    run.application(queryFile, mappingsFile, configFile)
+
+}
