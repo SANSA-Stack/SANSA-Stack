c40c3cb0b86a378b4789c78d3618d3a41213feee
Heba <heba.a.ibrahim@gmail.com>
2018-12-04 12:21:42 +0100

    sparksession update

--- sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/forwardchaining/axioms/ForwardRuleReasonerRDFSTest.scala
+++ sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/forwardchaining/axioms/ForwardRuleReasonerRDFSTest.scala
@@ -1,24 +1,15 @@
 package net.sansa_stack.inference.spark.forwardchaining.axioms
 
+import com.holdenkarau.spark.testing.{ SharedSparkContext, DataFrameSuiteBase }
+// import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import net.sansa_stack.owl.spark.rdd.FunctionalSyntaxOWLAxiomsRDDBuilder
 import net.sansa_stack.owl.spark.rdd.OWLAxiomsRDD
 import org.scalatest.FunSuite
 import org.semanticweb.owlapi.model.OWLAxiom
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 
-class ForwardRuleReasonerRDFSTest extends FunSuite {
-
-
-  val sparkSession = SparkSession.builder
-        .master("local[*]")
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-        .appName("OWL Axioms Forward Rule Reasoner")
-        .getOrCreate()
-
-  val sc: SparkContext = sparkSession.sparkContext
+class ForwardRuleReasonerRDFSTest extends FunSuite with SharedSparkContext with DataFrameSuiteBase {
 
   val reasoner = new ForwardRuleReasonerRDFS(sc, 4)
 
@@ -26,7 +17,7 @@ class ForwardRuleReasonerRDFSTest extends FunSuite {
 
       val input = getClass.getResource("/ont_functional.owl").getPath
 
-      var owlAxiomsRDD: OWLAxiomsRDD = FunctionalSyntaxOWLAxiomsRDDBuilder.build(sparkSession, input)
+      var owlAxiomsRDD: OWLAxiomsRDD = FunctionalSyntaxOWLAxiomsRDDBuilder.build(spark, input)
       val reasoner: RDD[OWLAxiom] = new ForwardRuleReasonerRDFS(sc, 4).apply(owlAxiomsRDD, input)
 
       assert(reasoner.count() == 16)
