bc07aff12995378128d1f1713b1c233c01dfdb0e
dgraux <damien.graux@iais.fraunhofer.de>
2018-12-06 00:44:34 +0100

    Unit tests for R2RML mapping functions

---
+++ sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/mappings/R2RMLMappingsTests.scala
@@ -0,0 +1,42 @@
+package net.sansa_stack.rdf.spark.mappings
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import net.sansa_stack.rdf.spark.io._
+import net.sansa_stack.rdf.spark.mappings._
+import org.apache.jena.riot.Lang
+import org.scalatest.FunSuite
+
+class R2RMLMappingsTests extends FunSuite with DataFrameSuiteBase {
+
+  test("creating the CREATE TABLE instructions from data: 25") {
+    val path = getClass.getResource("/rdf.nt").getPath
+    val creation = R2RMLMappings.loadSQLTables( path, spark)
+
+    creation.foreach(println)
+    val size = creation.size
+
+    assert(size == 25)
+  }
+
+  test("creating the INSERT INTO instructions from data: 100") {
+    val path = getClass.getResource("/rdf.nt").getPath
+    val insertion = R2RMLMappings.insertSQLTables( path, spark)
+
+    println("Printing the first 10 instructions:")
+    insertion.take(10).foreach(println)
+    val size = insertion.count
+
+    assert(size == 100)
+  }
+
+  test("creating the R2RML mappings from data: 25") {
+    val path = getClass.getResource("/rdf.nt").getPath
+    val mappings = R2RMLMappings.generateR2RMLMappings( path, spark)
+
+    mappings.foreach(println)
+    val size = mappings.size
+
+    assert(size == 25)
+  }
+
+}
