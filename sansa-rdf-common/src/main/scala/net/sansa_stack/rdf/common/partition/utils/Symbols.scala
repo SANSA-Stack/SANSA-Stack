04003e7d48305dfff354b1bc201062fe51d80755
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-12-06 09:51:03 +0100

    Clean up and move Semantic partitioning symbols to common module

---
+++ sansa-rdf-common/src/main/scala/net/sansa_stack/rdf/common/partition/utils/Symbols.scala
@@ -0,0 +1,30 @@
+package net.sansa_stack.rdf.common.partition.utils
+
+/**
+ * Define symbol characters for the Semantic partitioning.
+ */
+object Symbols {
+
+  val symbol = Map(
+    "space" -> " " * 5,
+    "blank" -> " ",
+    "tabs" -> "\t",
+    "newline" -> "\n",
+    "colon" -> ":",
+    "comma" -> ",",
+    "hash" -> "#",
+    "slash" -> "/",
+    "question-mark" -> "?",
+    "exclamation-mark" -> "!",
+    "curly-bracket-left" -> "{",
+    "curly-bracket-right" -> "}",
+    "round-bracket-left" -> "(",
+    "round-bracket-right" -> ")",
+    "less-than" -> "<",
+    "greater-than" -> ">",
+    "at" -> "@",
+    "dot" -> ".",
+    "dots" -> "...",
+    "asterisk" -> "*",
+    "up-arrows" -> "^^")
+}
