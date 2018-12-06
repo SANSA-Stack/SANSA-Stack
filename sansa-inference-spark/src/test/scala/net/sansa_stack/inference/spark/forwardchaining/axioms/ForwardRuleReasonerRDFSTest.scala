c56ad2cbd710efff8504d0ee727e8d7d52ea4514
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-12-06 09:51:59 +0100

    Added test for rdfs2 rule

--- sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/forwardchaining/axioms/ForwardRuleReasonerRDFSTest.scala
+++ sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/forwardchaining/axioms/ForwardRuleReasonerRDFSTest.scala
@@ -30,4 +30,36 @@ class ForwardRuleReasonerRDFSTest extends FunSuite with SharedSparkContext with 
       assert(reasoner.count() == 16)
 
   }
+
+  /**
+    * rdfs2
+    *   Condition:
+    *     aaa rdfs:domain xxx .
+    *     yyy aaa zzz .
+    *   Consequence:
+    *     yyy rdf:type xxx .
+    */
+  test("Rule rdfs2 should return correct results") {
+    val cls01 = df.getOWLClass(defaultPrefix + "Cls01")
+    val cls02 = df.getOWLClass(defaultPrefix + "Cls02")
+    val cls03 = df.getOWLClass(defaultPrefix + "Cls03")
+    val indivB = df.getOWLNamedIndividual(defaultPrefix + "indivB")
+    val indivD = df.getOWLNamedIndividual(defaultPrefix + "indivD")
+    val indivF = df.getOWLNamedIndividual(defaultPrefix + "indivF")
+
+    val input = getClass.getResource(resourcePath + "test_rdfs2.owl").getPath
+
+    val axiomsRDD = spark.owl(Syntax.FUNCTIONAL)(input)
+    val reasoner = new ForwardRuleReasonerRDFS(sc, sc.defaultMinPartitions)
+    val inferred: Seq[OWLAxiom] = reasoner.apply(axiomsRDD).collect()
+
+    // Three axioms should be inferred:
+    // ClassAssertion(:Cls01 :indivB)
+    // ClassAssertion(:Cls02 :indivD)
+    // ClassAssertion(:Cls03 :indivF)
+    assert(inferred.size == 3)
+    assert(inferred.contains(df.getOWLClassAssertionAxiom(cls01, indivB)))
+    assert(inferred.contains(df.getOWLClassAssertionAxiom(cls02, indivD)))
+    assert(inferred.contains(df.getOWLClassAssertionAxiom(cls03, indivF)))
+  }
 }
