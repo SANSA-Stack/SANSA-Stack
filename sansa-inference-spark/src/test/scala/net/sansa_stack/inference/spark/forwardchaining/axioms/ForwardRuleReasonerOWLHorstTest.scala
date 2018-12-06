7bdd5e7710fbda6532941a640ea4f79dc5b3a73b
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-12-06 10:16:25 +0100

    Clean-up in axioms.ForwardRuleReasonerOWLHorstTest

--- sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/forwardchaining/axioms/ForwardRuleReasonerOWLHorstTest.scala
+++ sansa-inference-spark/src/test/scala/net/sansa_stack/inference/spark/forwardchaining/axioms/ForwardRuleReasonerOWLHorstTest.scala
@@ -1,12 +1,26 @@
 package net.sansa_stack.inference.spark.forwardchaining.axioms
 
-import com.holdenkarau.spark.testing.{ SharedSparkContext, DataFrameSuiteBase }
+import com.holdenkarau.spark.testing.{DataFrameSuiteBase, SharedSparkContext}
 import net.sansa_stack.owl.spark.rdd.FunctionalSyntaxOWLAxiomsRDDBuilder
-import org.scalatest.FunSuite
-import org.semanticweb.owlapi.model.OWLAxiom
 import org.apache.spark.rdd.RDD
+import org.scalatest.FunSuite
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model.OWLAxiom
 
+import scala.collection.JavaConverters._
+
+
+/**
+  * Rule names refer to the name scheme used in
+  * 'RORS: Enhanced Rule-based OWL Reasoning on Spark' by Liu, Feng, Zhang,
+  * Wang, Rao
+  */
 class ForwardRuleReasonerOWLHorstTest extends FunSuite with SharedSparkContext with DataFrameSuiteBase {
+  private val resourcePath = "/forward_chaining/axioms/"
+  private val defaultPrefix = "http://ex.com/default#"
+  private val df = OWLManager.getOWLDataFactory
+
+  import net.sansa_stack.owl.spark.owl._
 
   val reasoner = new ForwardRuleReasonerOWLHorst(sc, 4)
 
@@ -16,7 +30,7 @@ class ForwardRuleReasonerOWLHorstTest extends FunSuite with SharedSparkContext w
 
     var owlAxiomsRDD = FunctionalSyntaxOWLAxiomsRDDBuilder.build(spark, input)
     val reasoner = new ForwardRuleReasonerOWLHorst(sc, 4)
-    val res: RDD[OWLAxiom] = reasoner(owlAxiomsRDD, input)
+    val res: RDD[OWLAxiom] = reasoner(owlAxiomsRDD)
 
     assert(res.count() == 211)
 
