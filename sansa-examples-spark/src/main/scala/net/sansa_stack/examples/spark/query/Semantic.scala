2f1d4995fcc4b27f8ca4c0db691010d88ec7bb58
Gezim Sejdiu <g.sejdiu@gmail.com>
2018-12-09 23:51:31 +0100

    Update SANSA versions

--- sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/query/Semantic.scala
+++ sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/query/Semantic.scala
@@ -5,9 +5,9 @@ import java.nio.file.{ Files, FileVisitResult, Path, Paths, SimpleFileVisitor }
 import java.nio.file.attribute.BasicFileAttributes
 
 import net.sansa_stack.query.spark.semantic.QuerySystem
+import net.sansa_stack.rdf.common.partition.utils.Symbols
 import net.sansa_stack.rdf.spark.io._
 import net.sansa_stack.rdf.spark.partition._
-import net.sansa_stack.rdf.common.partition.utils.Symbols
 import org.apache.jena.graph.Triple
 import org.apache.jena.riot.Lang
 import org.apache.spark.rdd.RDD
@@ -59,7 +59,6 @@ object Semantic {
     println("----------------------")
 
     val qs = new QuerySystem(
-      symbol = Symbols.symbol,
       partitionData,
       input,
       output,
