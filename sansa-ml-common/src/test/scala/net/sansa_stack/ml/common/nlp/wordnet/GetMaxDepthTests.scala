198661cb8f760ef603928e347d71c62cd0dedbbd
Gezim Sejdiu <g.sejdiu@gmail.com>
2019-03-01 13:00:54 +0100

    Clean up common.nlp.wordnet package

--- sansa-ml-common/src/test/scala/net/sansa_stack/ml/common/nlp/wordnet/GetMaxDepthTests.scala
+++ sansa-ml-common/src/test/scala/net/sansa_stack/ml/common/nlp/wordnet/GetMaxDepthTests.scala
@@ -3,22 +3,21 @@ package net.sansa_stack.ml.common.nlp.wordnet
 import net.sf.extjwnl.data._
 import org.scalatest.FunSuite
 
-class TestGetSynsets extends FunSuite {
+class GetMaxDepthTests extends FunSuite {
 
-  test("If The WordNet dictionary is correctly installed synsets must not be null ") {
+  test("Test the function that gets the maximum depth of dataset graph ") {
 
     try {
       val wn = new WordNet
       val dict = wn.getDict
-      // getting a synset by a word and index
 
       val thing1 = wn.getSynset("thing", POS.NOUN, 1).head
+      val dog = wn.getSynset("dog", POS.NOUN, 1).head
 
-      // getting a list of synsets by a word
-      val thing2 = wn.getSynsets("thing", POS.NOUN).head
 
-      assert(thing1 != null)
-      assert(thing2 != null)
+      val dogD = wn.maxDepth(dog)
+      val dogD2 = wn.minDepth(dog)
+      assert(dogD != 0)
     } catch {
       case e: ExceptionInInitializerError => println("The WordNet dictionary is not installed, please check the readme for instructions to enable it.")
     }
