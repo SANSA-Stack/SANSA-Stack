217442503102cde2d0d82f6a5a34161c99bd3529
Gezim Sejdiu <g.sejdiu@gmail.com>
2019-03-15 11:13:41 +0100

    Remove riot parser code occurring 2 times in two different modules, closes #78

--- sansa-rdf-common/src/main/scala/net/sansa_stack/rdf/common/partition/schema/SchemaStringStringLang.scala
+++ sansa-rdf-common/src/main/scala/net/sansa_stack/rdf/common/partition/schema/SchemaStringStringLang.scala
@@ -3,6 +3,4 @@ package net.sansa_stack.rdf.common.partition.schema
 /**
  * Created by Simon Bin on 09/06/17.
  */
-case class SchemaStringStringLang(val s: String, val o: String, val l: String) {
-
-}
+case class SchemaStringStringLang(val s: String, val o: String, val l: String)
