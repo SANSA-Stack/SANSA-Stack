935922b9c95f781b4908f58e748d01911723afc5
Gezim Sejdiu <g.sejdiu@gmail.com>
2019-03-15 15:26:58 +0100

    Provide basic unit-test for common.partition.utils

---
+++ sansa-rdf-common/src/test/scala/net/sansa_stack/rdf/common/partition/utils/RdfTermTests.scala
@@ -0,0 +1,25 @@
+package net.sansa_stack.rdf.common.partition.utils
+
+import org.scalatest.FunSuite
+
+import net.sansa_stack.rdf.common.partition.utils.RdfTerm._
+import org.apache.jena.graph.{ Node, NodeFactory, Triple }
+
+class RdfTermTests extends FunSuite {
+
+  val triple = Triple.create(
+    NodeFactory.createURI("http://dbpedia.org/resource/Germany"),
+    NodeFactory.createURI("http://dbpedia.org/ontology/populationTotal"),
+    NodeFactory.createLiteral("82175700"))
+
+  test("converting rdf term to node should pass") {
+    val term = nodeToTerm(triple.getSubject)
+    assert(termToNode(term).equals(triple.getSubject))
+  }
+
+  test("converting node to rdf term should pass") {
+    val requiredRDFTerm = new RdfTerm(1, "http://dbpedia.org/resource/Germany", null, null)
+    assert(nodeToTerm(triple.getSubject).equals(requiredRDFTerm))
+  }
+
+}
