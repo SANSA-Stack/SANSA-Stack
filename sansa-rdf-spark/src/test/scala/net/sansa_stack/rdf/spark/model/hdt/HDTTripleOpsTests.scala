3f7fca3dcd30dba4ed3e3c779d4625b8ce7eba96
Gezim Sejdiu <g.sejdiu@gmail.com>
2019-05-10 12:53:34 +0200

    Provide basic unit-test for the hdt

--- sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/model/hdt/HDTTripleOpsTests.scala
+++ sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/model/hdt/HDTTripleOpsTests.scala
@@ -2,92 +2,61 @@ package net.sansa_stack.rdf.spark.model.hdt
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.apache.jena.graph
+import org.apache.jena.riot.Lang
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
 import org.scalatest.FunSuite
 
+class HDTTripleOpsTests extends FunSuite with DataFrameSuiteBase {
 
-class TripleOpsTest extends FunSuite with DataFrameSuiteBase
- {
-  // val spark: SparkSession = new sql.SparkSession.Builder().master("local[*]").getOrCreate()
-  val tripleOps: TripleOps = new TripleOps()
-  val inputTestFile: String = getClass.getResource("/loader/compression-data.nt").getPath
-  val result: RDD[graph.Triple] = tripleOps.readRDFFromFile(inputTestFile)
-  val ROW_COUNT = 7
-  val DISTINCT_SUBJECT_COUNT = 3
-  val DISTINCT_OBJECT_COUNT = 6
-  val DISTINCT_PREDICATE_COUNT = 5
+  import net.sansa_stack.rdf.spark.io._
+  import net.sansa_stack.rdf.spark.model._
 
-  test("testRegisterDictionariesAsView") {
-    tripleOps.registerDictionariesAsView(tripleOps.getDistinctSubjectDictDF(result), tripleOps.getDistinctObjectDictDF(result), tripleOps.getDistinctPredicateDictDF(result))
-    val subjectDFCount = spark.sql(s"select * from ${TripleOps.SUBJECT_TABLE}").count()
-    val objectDFCount = spark.sql(s"select * from ${TripleOps.OBJECT_TABLE}").count()
-    val predicateDFCount = spark.sql(s"select * from ${TripleOps.PREDICATE_TABLE}").count()
+  val path = getClass.getResource("/loader/compression-data.nt").getPath
 
-    assert(subjectDFCount == DISTINCT_SUBJECT_COUNT)
-    assert(objectDFCount == DISTINCT_OBJECT_COUNT)
-    assert(predicateDFCount == DISTINCT_PREDICATE_COUNT)
+  test("converting RDD of triples into DataFrame of hdt should pass") {
 
+    val triples = spark.rdf(Lang.NTRIPLES)(path)
+
+    val hdt = triples.asHDT()
+
+    val size = hdt.size()
+
+    assert(size == 7)
   }
 
-  test("testCreateOrReadDataSet") {
-      tripleOps.createOrReadDataSet(inputTestFile, null)
-      assert(tripleOps.tripleFactTable.count() == result.count())
+  test("getting subjects from hdt subject table should match") {
+
+    val triples = spark.rdf(Lang.NTRIPLES)(path)
+
+    val hdt = triples.getHDT()
+
+    val subjects = spark.sql(s"select * from subjects_hdt").count()
+
+    assert(subjects == 3)
   }
 
-  test("testConvertRDDGraphToDF") {
-    val resuleDF = tripleOps.convertRDDGraphToDF(result)
-    assert(resuleDF.count() == result.count())
-    assert(resuleDF.schema.fieldNames.contains("s"))
-    assert(resuleDF.schema.fieldNames.contains("o"))
-    assert(resuleDF.schema.fieldNames.contains("p"))
+  test("getting predicates from hdt predicate table should match") {
+
+    val triples = spark.rdf(Lang.NTRIPLES)(path)
+
+    val hdt = triples.getHDT()
+
+    val predicates = spark.sql(s"select * from predicates_hdt").count()
+
+    assert(predicates == 5)
   }
 
-  test("testGetDistinctSubjectDictDF") {
-    val subjectDF = tripleOps.getDistinctSubjectDictDF(result)
-    assert(subjectDF.schema.fieldNames.contains("name"))
-    assert(subjectDF.schema.fieldNames.contains("index"))
-    assert(subjectDF.count()==DISTINCT_SUBJECT_COUNT)
-  }
+  test("getting objects from hdt object table should match") {
 
-  test("testGetDistinctObjectDictDF") {
-    val objectDF = tripleOps.getDistinctObjectDictDF(result)
-    assert(objectDF.schema.fieldNames.contains("name"))
-    assert(objectDF.schema.fieldNames.contains("index"))
-    assert(objectDF.count()==DISTINCT_OBJECT_COUNT)
-  }
+    val triples = spark.rdf(Lang.NTRIPLES)(path)
 
-  test("testGetDistinctPredicateDictDF") {
-    val predicateDF = tripleOps.getDistinctPredicateDictDF(result)
-    assert(predicateDF.schema.fieldNames.contains("name"))
-    assert(predicateDF.schema.fieldNames.contains("index"))
-    assert(predicateDF.count()==DISTINCT_PREDICATE_COUNT)
-  }
-  test("testTripleSchemaSize Test") {
-    assert(tripleOps.tripleSchema.fieldNames.length == 3)
-  }
+    val hdt = triples.getHDT()
 
-  test("testTripleSchema Field Name") {
-    assert(tripleOps.tripleSchema.fieldNames(0).equalsIgnoreCase("s"))
-    assert(tripleOps.tripleSchema.fieldNames(1).equalsIgnoreCase("o"))
-    assert(tripleOps.tripleSchema.fieldNames(2).equalsIgnoreCase("p"))
+    val objects = spark.sql(s"select * from objects_hdt").count()
+
+    assert(objects == 6)
   }
-
-  test("testDictionarySchema Size Test") {
-    assert(tripleOps.dictionarySchema.fieldNames.length == 2)
-  }
-
-  test("DictionarySchema Name Test") {
-    assert(tripleOps.dictionarySchema.fieldNames(0).equalsIgnoreCase("name"))
-    assert(tripleOps.dictionarySchema.fieldNames(1).equalsIgnoreCase("index"))
-  }
-
-  test("testReadRDFFromFile") {
-      val result = tripleOps.readRDFFromFile(inputTestFile)
-      assert(result.count() == ROW_COUNT)
-  }
-
-
 
 }
