33968c9783ad27d2ebd8fdd1de896ae2578168c5
Gezim Sejdiu <g.sejdiu@gmail.com>
2019-05-10 12:52:45 +0200

    Refactor and clean-up the hdt.TripleOps object

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/model/hdt/TripleOps.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/model/hdt/TripleOps.scala
@@ -1,194 +1,141 @@
 package net.sansa_stack.rdf.spark.model.hdt
 
 import net.sansa_stack.rdf.spark.io._
-import org.apache.jena.graph
+import org.apache.jena.graph.Triple
 import org.apache.jena.riot.Lang
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
-import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.{ DataFrame, Row, SaveMode, SparkSession }
+import org.apache.spark.sql.types.{ LongType, StringType, StructField, StructType }
 
+object TripleOps {
 
-class TripleOps {
-  var tripleFactTable : DataFrame = _
-  var objectDF : DataFrame = _
-  var predicateDF : DataFrame = _
-  var subjectDF : DataFrame = _
-  private val spark : SparkSession = SparkSession.builder().getOrCreate()
+  private val spark: SparkSession = SparkSession.builder().getOrCreate()
 
   /**
-    * Function returns the Schema of Indexed Triple Fact table.
-    * @return StructType
-    */
-  def tripleSchema : StructType = {
+   * Function returns the Schema of Indexed Triple Fact table.
+   * @return StructType
+   */
+  def hdtSchema: StructType = {
     StructType(
-      Seq( StructField(name = TripleOps.SUBJECT_COL, dataType = StringType, nullable = false),
-        StructField(name = TripleOps.OBJECT_COL, dataType = StringType, nullable = false),
-        StructField(name = TripleOps.PREDICATE_COL, dataType = StringType, nullable = false)))
+      Seq(
+        StructField(name = "s", dataType = StringType, nullable = false),
+        StructField(name = "p", dataType = StringType, nullable = false),
+        StructField(name = "o", dataType = StringType, nullable = false)))
   }
 
   /**
-    * Function returns the Schema of Dictionary Dataframe.
-    * @return Schema of Dictionary
-     */
-  def dictionarySchema : StructType = {
+   * Function returns the Schema of Dictionary Dataframe.
+   * @return Schema of Dictionary
+   */
+  def dictionarySchema: StructType = {
     StructType(
-      Seq( StructField(name = TripleOps.NAME_COL, dataType = StringType, nullable = false),
-        StructField(name = TripleOps.INDEX_COL, dataType = LongType, nullable = false)))
+      Seq(
+        StructField(name = "name", dataType = StringType, nullable = false),
+        StructField(name = "index", dataType = LongType, nullable = false)))
   }
-
-    /**
-    * Function converts RDD[graph.Triple] to DataFrame [Subject,Object,Predicate] by extracting SOP  value from each record
-    * @param triple: Input raw RDD[graph.Triple]
-    * @return Returns DataFrame [Subject,Object,Predicate]
-      */
-  def convertRDDGraphToDF(triple : RDD[graph.Triple]) : DataFrame = {
-    spark.createDataFrame(triple.map(t => Row(t.getSubject.toString, t. getObject.toString(), t.getPredicate.toString())), tripleSchema)
-  }
-
-
-   /**
-    * Registered input Subject, Object and Predicate Dataframe as Spark in-memory Views
-    * @param subjectDictDF Dictionary Dataframe of Subject
-    * @param objectDictDF Dictionary Dataframe of Object
-    * @param predicateDict Dictionary Dataframe of Predicate
-    */
-  def registerDictionariesAsView(subjectDictDF : DataFrame, objectDictDF : DataFrame, predicateDict : DataFrame) : Unit = {
-    subjectDictDF.createOrReplaceTempView(TripleOps.SUBJECT_TABLE)
-    objectDictDF.createOrReplaceTempView(TripleOps.OBJECT_TABLE)
-    predicateDict.createOrReplaceTempView(TripleOps.PREDICATE_TABLE)
-  }
-
 
   /**
-    * Return Dataframe of Index + Subject by retrieving the unique subjects from RDD[Triple] and zip it with undex
-    * @param triples RDD[Triple] conversion of input file
-    * @return DataFrame Subject dictionary of [index,subject]
-    */
-  def getDistinctSubjectDictDF(triples : RDD[graph.Triple]) : DataFrame = {
+   * Function converts RDD[graph.Triple] to DataFrame [Subject,Object,Predicate] by extracting SOP  value from each record
+   * @param triple: Input raw RDD[graph.Triple]
+   * @return Returns DataFrame [Subject,Object,Predicate]
+   */
+  def asHDT(triple: RDD[Triple]): DataFrame = {
+    spark.createDataFrame(triple.map(t => Row(t.getSubject.toString, t.getObject.toString(), t.getPredicate.toString())), hdtSchema)
+  }
+
+  /**
+   * Return Dataframe of Index + Subject by retrieving the unique subjects from RDD[Triple] and zip it with undex
+   * @param triples RDD[Triple] conversion of input file
+   * @return DataFrame Subject dictionary of [index,subject]
+   */
+  def getDistinctSubjectDictDF(triples: RDD[Triple]): DataFrame = {
     spark.createDataFrame(triples.map(_.getSubject.toString()).distinct().zipWithIndex().map(t => Row(t._1, t._2)), dictionarySchema).cache()
   }
 
   /**
-    * Return Dataframe of Index + Predicate by retrieving the unique predicate from RDD[Triple] and zip it with undex
-    * @param triples RDD[Triple] conversion of input file
-    * @return DataFrame Predicate dictionary of [index,Prediate]
-    */
-  def getDistinctPredicateDictDF(triples : RDD[graph.Triple]) : DataFrame = {
+   * Return Dataframe of Index + Predicate by retrieving the unique predicate from RDD[Triple] and zip it with undex
+   * @param triples RDD[Triple] conversion of input file
+   * @return DataFrame Predicate dictionary of [index,Prediate]
+   */
+  def getDistinctPredicateDictDF(triples: RDD[Triple]): DataFrame = {
     spark.createDataFrame(triples.map(_.getPredicate.toString()).distinct().zipWithIndex().map(t => Row(t._1, t._2)), dictionarySchema).cache()
   }
 
   /**
-    * Return Dataframe of Index + Object by retrieving the unique objects from RDD[Triple] and zip it with undex
-    * @param triples RDD[Triple] conversion of input file
-    * @return DataFrame Object dictionary of [index , object]
-    */
-  def getDistinctObjectDictDF(triples : RDD[graph.Triple]) : DataFrame = {
-    spark.createDataFrame( triples.map(_.getObject.toString()).distinct().zipWithIndex().map(t => Row(t._1, t._2)), dictionarySchema).cache()
+   * Return Dataframe of Index + Object by retrieving the unique objects from RDD[Triple] and zip it with undex
+   * @param triples RDD[Triple] conversion of input file
+   * @return DataFrame Object dictionary of [index , object]
+   */
+  def getDistinctObjectDictDF(triples: RDD[Triple]): DataFrame = {
+    spark.createDataFrame(triples.map(_.getObject.toString()).distinct().zipWithIndex().map(t => Row(t._1, t._2)), dictionarySchema).cache()
   }
-
 
   /**
-    * Reads the input rdf file and returns spark RDD[Triple]
-    * @param input Input RDF file path
-    * @return returns RDD[Tripe]
-    */
-  def readRDFFromFile( input : String) : RDD[graph.Triple] = {
-    val triples : RDD[graph.Triple] = spark.rdf(Lang.NTRIPLES)(input)
-    triples
-  }
+   * This is key function of TripleOps that read RDF file and create Dictionaries and Index Table and register them as Spark In memory Table
+   * @param input Input RDF File Path [Either One of the input is require]
+   * @param compressedDir Input compressed-directory Path to read compressed data directly [Either One of the input is require]
+   * @param registerAsTable If true, it register all the DF as Spark table
+   * @return Returns the Tuple4 [IndexDataFrame,SubjectDictDataFrame,ObjectDictDataFrame,PredicateDictDataFrame]
+   */
+  def getHDT(triples: RDD[Triple]): DataFrame = {
 
+    val hdtDF = asHDT(triples)
+
+    val object_hdt = getDistinctObjectDictDF(triples).createOrReplaceTempView("objects_hdt")
+    val predicate_hdt = getDistinctPredicateDictDF(triples).createOrReplaceTempView("predicates_hdt")
+    val subjectHDT = getDistinctSubjectDictDF(triples).createOrReplaceTempView("subjects_hdt")
+
+    hdtDF.createOrReplaceTempView("triples_hdt")
+
+    val sqlQuery = """
+        SELECT subjects_hdt.index as s, predicates_hdt.index as p, objects_hdt.index as o
+        FROM triples_hdt
+             JOIN subjects_hdt ON triples_hdt.s = subjects_hdt.name
+             JOIN objects_hdt ON triples_hdt.o = objects_hdt.name
+             JOIN predicates_hdt ON triples_hdt.p =predicates_hdt.name
+        """
+
+    // Creating Fact table from Subject,Predicate and Object index. Fact table contains unique ID of Subject/Object/Predicate
+    val hdt = spark.sql(sqlQuery)
+    hdt.createOrReplaceTempView("hdt")
+
+    hdt
+  }
 
   /**
-    * This is key function of TripleOps that read RDF file and create Dictionaries and Index Table and register them as Spark In memory Table
-    * @param input Input RDF File Path [Either One of the input is require]
-    * @param compressedDir Input compressed-directory Path to read compressed data directly [Either One of the input is require]
-    * @param registerAsTable If true, it register all the DF as Spark table
-    * @return Returns the Tuple4 [IndexDataFrame,SubjectDictDataFrame,ObjectDictDataFrame,PredicateDictDataFrame]
-    */
-  def createOrReadDataSet(input : String, compressedDir : String, registerAsTable : Boolean = true) : (DataFrame, DataFrame, DataFrame, DataFrame) = {
-    if (input != null && !input.isEmpty) {
-      val rddGraph : RDD[graph.Triple] = readRDFFromFile(input)
+   * Read hdt data from disk.
+   * @param input -- path to hdt data.
+   * @retun DataFrame of hdt, subject, predicate, and object view.
+   */
+  def readHDTFromDisk(input: String): (DataFrame, DataFrame, DataFrame, DataFrame) = {
 
-      val triplesDF = convertRDDGraphToDF(rddGraph)
-      triplesDF.cache()
+    val hdt = spark.read.schema(hdtSchema).csv(input + "/triples")
+    hdt.createOrReplaceTempView("hdt")
 
-      objectDF = getDistinctObjectDictDF(rddGraph)
+    val subjectDF = spark.read.schema(dictionarySchema)
+      .csv(input + "/subject")
+    subjectDF.createOrReplaceTempView("subjects_hdt")
 
-      predicateDF = getDistinctPredicateDictDF(rddGraph)
+    val objectDF = spark.read.schema(dictionarySchema)
+      .csv(input + "/object")
+    objectDF.createOrReplaceTempView("objects_hdt")
 
-      subjectDF = getDistinctSubjectDictDF(rddGraph)
+    val predicateDF = spark.read.schema(dictionarySchema)
+      .csv(input + "/predicate")
+    predicateDF.createOrReplaceTempView("predicates_hdt")
 
-      registerDictionariesAsView(subjectDF, objectDF, predicateDF)
-
-      triplesDF.createOrReplaceTempView(TripleOps.TRIPLE_RAW_TABLE)
-
-      // Creating Fact table from Subject,Predicate and Object index. Fact table contains unique ID of Subject/Object/Predicate
-      tripleFactTable = spark.sqlContext.sql(s"select ${TripleOps.SUBJECT_TABLE}.${TripleOps.INDEX_COL} as ${TripleOps.SUBJECT_COL}, ${TripleOps.PREDICATE_TABLE}.${TripleOps.INDEX_COL} as " +
-        s"${TripleOps.PREDICATE_COL}, ${TripleOps.OBJECT_TABLE}.${TripleOps.INDEX_COL} as ${TripleOps.OBJECT_COL} from ${TripleOps.TRIPLE_RAW_TABLE} " +
-        s"join ${TripleOps.SUBJECT_TABLE} on ${TripleOps.TRIPLE_RAW_TABLE}.${TripleOps.SUBJECT_COL} " +
-        s"=${TripleOps.SUBJECT_TABLE}.${TripleOps.NAME_COL} " +
-        s"join ${TripleOps.OBJECT_TABLE} on ${TripleOps.TRIPLE_RAW_TABLE}.${TripleOps.OBJECT_COL}=${TripleOps.OBJECT_TABLE}.${TripleOps.NAME_COL} " +
-        s"join ${TripleOps.PREDICATE_TABLE} on ${TripleOps.TRIPLE_RAW_TABLE}.${TripleOps.PREDICATE_COL}=${TripleOps.PREDICATE_TABLE}.${TripleOps.NAME_COL}")
-
-      if (registerAsTable) {
-        tripleFactTable.createOrReplaceTempView(TripleOps.TRIPLE_TABLE)
-      }
-    }
-    else if ( compressedDir != null && !compressedDir.isEmpty) {
-
-      tripleFactTable = spark.read.schema(tripleSchema).csv(compressedDir + TripleOps.TRIPLE_DIR)
-      subjectDF = spark.read.schema(dictionarySchema).csv(compressedDir + TripleOps.SUBJECT_DIR)
-      objectDF = spark.read.schema(dictionarySchema).csv(compressedDir + TripleOps.OBJECT_DIR)
-      predicateDF = spark.read.schema(dictionarySchema).csv(compressedDir + TripleOps.PREDICATE_DIR)
-      if(registerAsTable)
-      {
-        registerDictionariesAsView(subjectDF, objectDF, predicateDF)
-        tripleFactTable.createOrReplaceTempView(TripleOps.TRIPLE_TABLE)
-      }
-
-    }
-    else {
-      throw  new IllegalArgumentException("Input File or Compressed Directory is not provided. Please provide any one input.")
-    }
-    (tripleFactTable, subjectDF, objectDF, predicateDF)
+    (hdt, subjectDF, objectDF, predicateDF)
   }
-
 
   /**
-    * Function saves the Index and Dictionaries Dataframes into given location
-    * @param outputDir Path to be written
-    * @param mode SaveMode of Write
-    */
-  def saveAsCSV(outputDir : String, mode : SaveMode) : Unit =
-  {
-    tripleFactTable.write.mode(mode).csv(outputDir + TripleOps.TRIPLE_DIR)
-    subjectDF.write.mode(mode).csv(outputDir + TripleOps.SUBJECT_DIR)
-    objectDF.write.mode(mode).csv(outputDir + TripleOps.OBJECT_DIR)
-    predicateDF.write.mode(mode).csv(outputDir + TripleOps.PREDICATE_DIR)
+   * Function saves the Index and Dictionaries Dataframe into given location
+   * @param output Path to be written
+   * @param mode SaveMode of Write
+   */
+  def saveAsCSV(hdt: DataFrame, subjectDF: DataFrame, predicateDF: DataFrame, objectDF: DataFrame, output: String, mode: SaveMode): Unit = {
+    hdt.write.mode(mode).csv(output + "/triples")
+    subjectDF.write.mode(mode).csv(output + "/subject")
+    objectDF.write.mode(mode).csv(output + "/object")
+    predicateDF.write.mode(mode).csv(output + "/predicate")
   }
-
-}
-
-
-
-object TripleOps {
-  // Constants Output directory names
-  val TRIPLE_DIR = "/triples"
-  val OBJECT_DIR = "/object"
-  val PREDICATE_DIR = "/predicate"
-  val SUBJECT_DIR = "/subject"
-
-  // Spark SQL Temp view names
-  val OBJECT_TABLE = "object"
-  val SUBJECT_TABLE = "subject"
-  val PREDICATE_TABLE = "predicate"
-  val TRIPLE_RAW_TABLE = "tripleRaw"
-  val TRIPLE_TABLE = "triple_fact"
-
-  val INDEX_COL = "index"
-  val NAME_COL = "name"
-
-  val SUBJECT_COL = "s"
-  val OBJECT_COL = "o"
-  val PREDICATE_COL = "p"
 }
