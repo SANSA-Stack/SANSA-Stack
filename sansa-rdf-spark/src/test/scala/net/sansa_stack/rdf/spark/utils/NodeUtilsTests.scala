bfaaa85ecd547a037caf0178469f47cb3eb10a73
Gezim Sejdiu <g.sejdiu@gmail.com>
2019-05-28 13:40:50 +0200

    Provide basic unit-test for Node utils

---
+++ sansa-rdf-spark/src/test/scala/net/sansa_stack/rdf/spark/utils/NodeUtilsTests.scala
@@ -0,0 +1,22 @@
+package net.sansa_stack.rdf.spark.utils
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.jena.graph.{ Node, NodeFactory, Triple }
+import org.scalatest.FunSuite
+
+class NodeUtilsTests extends FunSuite with DataFrameSuiteBase {
+  test("getting the value of the node should match") {
+
+    val triple = Triple.create(
+      NodeFactory.createURI("http://dbpedia.org/resource/Guy_de_Maupassant"),
+      NodeFactory.createURI("http://xmlns.com/foaf/0.1/givenName"),
+      NodeFactory.createLiteral("Guy De"))
+
+    val subject = triple.getSubject.toString()
+
+    val nodeValue = NodeUtils.getNodeValue(triple.getSubject)
+
+    assert(nodeValue == subject)
+  }
+
+}
