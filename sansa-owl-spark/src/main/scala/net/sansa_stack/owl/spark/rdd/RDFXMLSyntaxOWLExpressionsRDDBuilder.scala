92d8dba9187c0e5e9fb11f26bc08ee02b65e1761
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-06-11 08:45:21 +0200

    Minor clean-up in RDFXMLSyntaxOWLExpr.RDDBuilder

--- sansa-owl-spark/src/main/scala/net/sansa_stack/owl/spark/rdd/RDFXMLSyntaxOWLExpressionsRDDBuilder.scala
+++ sansa-owl-spark/src/main/scala/net/sansa_stack/owl/spark/rdd/RDFXMLSyntaxOWLExpressionsRDDBuilder.scala
@@ -1,20 +1,24 @@
 package net.sansa_stack.owl.spark.rdd
 
-import collection.JavaConverters._
+import scala.collection.JavaConverters._
+
+import com.typesafe.scalalogging.{Logger => ScalaLogger}
 import org.apache.hadoop.io.Text
 import org.apache.hadoop.mapred.JobConf
-import org.apache.log4j.{Level, Logger}
+import org.apache.log4j.{Level, Logger => Log4JLogger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.semanticweb.owlapi.model.OWLAxiom
 
 import net.sansa_stack.owl.common.parsing.{RDFXMLSyntaxParsing, RDFXMLSyntaxPrefixParsing}
 
+
 class RDFXMLSyntaxOWLExpressionsRDDBuilder extends Serializable with RDFXMLSyntaxPrefixParsing with RDFXMLSyntaxParsing {
+  private val logger = ScalaLogger(this.getClass)
 
   /**
-    * Builds a snippet conforming to the RDFXML syntax which then can
-    * be parsed by the OWLAPI RDFXML syntax parser.
+    * Builds a snippet conforming to the RDF/XML syntax which then can
+    * be parsed by the OWL API RDF/XML syntax parser.
     * A single expression, e.g.
     *
     * <rdf:Description rdf:about="http://swat.cse.lehigh.edu/onto/univ-bench.owl#Person">
@@ -33,8 +37,6 @@ class RDFXMLSyntaxOWLExpressionsRDDBuilder extends Serializable with RDFXMLSynta
     * @param filePath the path to the input file
     * @return The set of axioms corresponding to the expression.
     */
-
-
   def build(spark: SparkSession, filePath: String): OWLAxiomsRDD = {
 
     val conf = new JobConf()
@@ -51,53 +53,50 @@ class RDFXMLSyntaxOWLExpressionsRDDBuilder extends Serializable with RDFXMLSynta
     org.apache.hadoop.mapred.FileInputFormat.addInputPaths(conf, filePath)
     org.apache.hadoop.mapred.FileInputFormat.addInputPaths(prefixes, filePath)
 
-    // RDF XML Record
-    // read data and save in RDD as block- RDFXML Record
-    val RDFXML_Record: RDD[(Text, Text)] = spark.sparkContext.hadoopRDD(conf,
+    // RDF/XML Record
+    // read data and save in RDD as block-RDF/XML Record
+    val rdfXMLRecord: RDD[(Text, Text)] = spark.sparkContext.hadoopRDD(conf,
       classOf[org.apache.hadoop.streaming.StreamInputFormat],
       classOf[org.apache.hadoop.io.Text],
       classOf[org.apache.hadoop.io.Text])
 
-    // Convert the block- RDFXML Record to String DataType
-    val rawRDD: RDD[String] = RDFXML_Record.map { case (x, y) => (x.toString()) }
+    // Convert the block-RDF/XML record to String
+    val rawRDD: RDD[String] = rdfXMLRecord.map { case (x, _) => x.toString }
 
-    // RDF XML Prefixes
-    // read data and save in RDD as block- RDFXML Prefixes
-    val RDFXML_Prefixes = spark.sparkContext.hadoopRDD(prefixes,
+    // RDF/XML prefixes
+    // Read data and save in RDD as block-RDF/XML prefixes
+    val rdfXMLPrefixes = spark.sparkContext.hadoopRDD(prefixes,
       classOf[org.apache.hadoop.streaming.StreamInputFormat],
       classOf[org.apache.hadoop.io.Text],
       classOf[org.apache.hadoop.io.Text])
 
-    // Convert the block- RDFXML Prefixes to String DataType
-    var tmp_Prefixes = RDFXML_Prefixes.map { case (x, y) => (x.toString()) }.distinct()
+    // Convert the block-RDF/XML prefixes to String
+    val tmpPrefixes = rdfXMLPrefixes.map { case (x, _) => x.toString }.distinct()
+    val prefixesString: String = tmpPrefixes.reduce((a, b) => a + "\n" + b)
 
-    val pref: String = tmp_Prefixes.reduce((a, b) => a + "\n" + b)
+    val owlAxioms: RDD[OWLAxiom] =
+      rawRDD.map(record => parseRecord(record, prefixesString))
+        .filter(x => !x.isEmpty)
+        .flatMap(axioms => axioms.asScala)
+        .distinct()
 
-//    val tmp: Array[(String, String)] = rawRDD.filter(isPrefix(_)).map(parsePrefix).collect()
-//    val prefixesMap: Map[String, String] = tmp.toMap
-
-    val OWLAxiomsList = rawRDD.map(record => RecordParse(record, pref))
-                              .filter(x => !x.isEmpty)
-
-    val OWLAxiomsRDD: RDD[OWLAxiom] = OWLAxiomsList.flatMap(line => line.iterator().asScala)
-                        .distinct()
-
-    val refinedRDD = refineOWLAxioms (spark.sparkContext, OWLAxiomsRDD)
-    println("Axioms count = " + refinedRDD.count())
+    val refinedRDD = refineOWLAxioms(spark.sparkContext, owlAxioms)
+//    logger.debug(s"Axioms count = ${refinedRDD.count()}")
 
     refinedRDD
   }
 }
 
 object RDFXMLSyntaxOWLExpressionsRDDBuilder {
+  private val logger = ScalaLogger(this.getClass)
 
   def main(args: Array[String]): Unit = {
 
     val input: String = getClass.getResource("/univ-bench.rdf").getPath
 
-    println("================================")
-    println("|        RDF/XML Parser        |")
-    println("================================")
+    logger.info("================================")
+    logger.info("|        RDF/XML Parser        |")
+    logger.info("================================")
 
     @transient val sparkSession = SparkSession.builder
       .master("local[*]")
@@ -105,15 +104,13 @@ object RDFXMLSyntaxOWLExpressionsRDDBuilder {
       .appName("RDF/XML Parser")
       .getOrCreate()
 
-    Logger.getLogger("akka").setLevel(Level.OFF)
-    Logger.getLogger(this.getClass).setLevel(Level.ERROR)
+    Log4JLogger.getLogger("akka").setLevel(Level.OFF)
+    Log4JLogger.getLogger(this.getClass).setLevel(Level.ERROR)
 
     val RDFXMLBuilder = new RDFXMLSyntaxOWLExpressionsRDDBuilder
-    val rdd = RDFXMLBuilder.build(sparkSession, input)
+    val rdd: OWLAxiomsRDD = RDFXMLBuilder.build(sparkSession, input)
+    rdd.foreach(axiom => logger.info(axiom.toString))
 
     sparkSession.stop
   }
 }
-
-
-
