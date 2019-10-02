1302e97de0e7b8587b48861124de2a15db386069
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-10-02 22:58:47 +0200

    Everything is better with curry(ing)...

--- sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/conjure/MainConjure.scala
+++ sansa-query-spark/src/main/scala/net/sansa_stack/query/spark/conjure/MainConjure.scala
@@ -4,14 +4,17 @@ import java.io.{ByteArrayOutputStream, File}
 import java.net.{BindException, InetAddress, URL}
 import java.util.concurrent.TimeUnit
 
+import com.google.common.base.StandardSystemProperty
+import com.google.common.collect.{ImmutableRangeSet, Range, RangeSet}
 import org.aksw.jena_sparql_api.ext.virtuoso.HealthcheckRunner
 import org.apache.jena.fuseki.FusekiException
 import org.apache.jena.fuseki.main.FusekiServer
 import org.apache.jena.query.DatasetFactory
-import org.apache.jena.rdfconnection.RDFConnectionRemote
+import org.apache.jena.rdfconnection.{RDFConnection, RDFConnectionRemote}
 import org.apache.jena.riot.{RDFDataMgr, RDFFormat}
 import org.apache.jena.vocabulary.RDF
 import org.apache.spark.TaskContext
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.SparkSession
 
 
@@ -41,13 +44,13 @@ object MainConjure {
     // If the server is already up, we are done
     var url: URL = null
     //    try {
-//      HealthcheckRunner.checkUrl(url)
-//    }
-//    catch {
-//      case _: Throwable =>
+    //      HealthcheckRunner.checkUrl(url)
+    //    }
+    //    catch {
+    //      case _: Throwable =>
 
     var retry = 10
-    while(retry > 0) {
+    while (retry > 0) {
       url = HealthcheckRunner.createUrl("http://localhost:" + port + "/" + name)
       val ds = DatasetFactory.createTxnMem
 
@@ -73,7 +76,7 @@ object MainConjure {
             println(TaskContext.getPartitionId() + " BIND EXCEPTION")
             port = port + 1
             retry = retry - 1 /* already running */
-            if(retry <= 0) throw new RuntimeException("Giving up")
+            if (retry <= 0) throw new RuntimeException("Giving up")
           case e => throw new RuntimeException(e)
         }
       }
@@ -96,14 +99,18 @@ object MainConjure {
   def main(args: Array[String]): Unit = {
 
 
-    val tempDirStr = System.getProperty("java.io.tmpdir")
-    if (tempDirStr == null) {
+    val tmpDirStr = StandardSystemProperty.JAVA_IO_TMPDIR.value()
+    if (tmpDirStr == null) {
       throw new RuntimeException("Could not obtain temporary directory")
     }
-    val sparkEventsDir = new File(tempDirStr + "/spark-events")
+    val sparkEventsDir = new File(tmpDirStr + "/spark-events")
     if (!sparkEventsDir.exists()) {
       sparkEventsDir.mkdirs()
     }
+
+    // Lambda that maps host names to allowed port ranges (for the triple store)
+    val hostToPortRanges: Function[String, RangeSet[Integer]] =
+      hostName => new ImmutableRangeSet.Builder[Integer].add(Range.closed(3030, 3040)).build()
 
     // File.createTempFile("spark-events")
 
@@ -122,7 +129,11 @@ object MainConjure {
 
     sparkSession.conf.set("spark.sql.crossJoin.enabled", "true")
 
-    val it = Seq.range(0, 10000)
+    val hostToPortRangesBroadcast: Broadcast[String => RangeSet[Integer]] =
+      sparkSession.sparkContext.broadcast(hostToPortRanges)
+
+
+    val it = Seq.range(0, 1000)
       .map(i => s"CONSTRUCT WHERE { ?s$i ?p ?o }")
 
     val rdd = sparkSession.sparkContext.parallelize(it)
@@ -137,54 +148,18 @@ object MainConjure {
     println("Hostnames: " + workerHostNames)
 
     val statusReports = rdd
-      .mapPartitions(it => {
-        val (server, url) = startSparqlEndpoint()
-
-        println(TaskContext.getPartitionId()  + " Got endpoint at " + url)
-
-        val conn = RDFConnectionRemote.create()
-          .destination(url.toString)
-          .build()
-
-        val onClose = () => {
-          println(TaskContext.getPartitionId() + " stopping server")
-          server.stop()
-          conn.close()
-        }
-
-
-        val effective: Iterator[String] = (it ++ Iterator("close"))
-          .map(x => {
-            if (x == "close") {
-              onClose()
-            }
-            x
-          })
-          .filter(x => x != "close")
-
-// val r: Iterator[(String, String)] =
-        // try {
-        effective.map(item => {
-            try {
-                println(TaskContext.getPartitionId() + " processing " + item)
-                val model = conn.queryConstruct(item)
-                val baos = new ByteArrayOutputStream
-                RDFDataMgr.write(baos, model, RDFFormat.TURTLE_PRETTY)
-                val str = baos.toString("UTF-8")
-                (item, str)
-            } catch {
-              case e: Throwable => println(e); (item, e.toString)
-            }
-          })
-        // }
-        // catch {
-        //  case f: Throwable => onClose()
-        // }
-      })
+      .mapPartitions(it => mapWithConnection(hostToPortRangesBroadcast)(it)((item, conn) => {
+        println(TaskContext.getPartitionId() + " processing " + item)
+        val model = conn.queryConstruct(item)
+        val baos = new ByteArrayOutputStream
+        RDFDataMgr.write(baos, model, RDFFormat.TURTLE_PRETTY)
+        val str = baos.toString("UTF-8")
+        (item, str)
+      }))
       .collect
 
     println("RESULTS: ----------------------------")
-    for(item <- statusReports) {
+    for (item <- statusReports) {
       // println(item)
     }
 
@@ -194,5 +169,50 @@ object MainConjure {
 
 
     println("Done")
+  }
+
+
+  //  def wrapperFactory[T, X]
+
+  //def mapWithConnectionFactory[T, X](hostToPortRangesBroadcast: Broadcast[String => RangeSet[Integer]]): (Iterator[T], (T, RDFConnection) => X) => Iterator[X] =
+ //   (it: Iterator[T], fn: (T, RDFConnection) => X) => mapWithConnection[T, X](hostToPortRangesBroadcast, it, fn)
+
+  def mapWithConnection[T, X](hostToPortRangesBroadcast: Broadcast[String => RangeSet[Integer]])
+                             (it: Iterator[T])
+                             (fn: (T, RDFConnection) => X): Iterator[X] = {
+    val hostName = InetAddress.getLocalHost.getHostName
+
+    val hostToPortRanges = hostToPortRangesBroadcast.value
+    val portRanges = hostToPortRanges(hostName)
+    println("Port ranges: " + portRanges)
+
+    val (server, url) = startSparqlEndpoint()
+
+    println(TaskContext.getPartitionId()  + " Got endpoint at " + url)
+
+    val conn = RDFConnectionRemote.create()
+      .destination(url.toString)
+      .build()
+
+    val onClose = () => {
+      println(TaskContext.getPartitionId() + " stopping server")
+      server.stop()
+      conn.close()
+    }
+
+    // Construct an iterator that releases resources upon encountering the last item
+    // Temporarily pairs each item with an 'isLastItem' flag
+    val wrapperIt: Iterator[T] =
+      (it.map(x => (x, false)) ++ Iterator((null.asInstanceOf[T], true)))
+      .filter(x => if (x._2 == true) { onClose(); false } else true)
+      .map(x => x._1)
+
+    // Free resources on exception
+    wrapperIt.map(item => try {
+      val s: X = fn.apply(item, conn)
+      s
+    } catch {
+      case e => onClose; throw new RuntimeException(e)
+    })
   }
 }
