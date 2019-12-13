0ef8af656d5c0a8ae66de7d46d1217e7e7edf6e3
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2019-12-13 09:54:07 +0100

    formatting

--- sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/core/RdfPartitionUtilsSpark.scala
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/core/RdfPartitionUtilsSpark.scala
@@ -2,28 +2,31 @@ package net.sansa_stack.rdf.spark.partition.core
 
 import scala.reflect.ClassTag
 
-import net.sansa_stack.rdf.common.partition.core.{ RdfPartition, RdfPartitioner, RdfPartitionerDefault }
 import org.apache.jena.graph.Triple
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 
+import net.sansa_stack.rdf.common.partition.core.{RdfPartition, RdfPartitioner, RdfPartitionerDefault}
+
 object RdfPartitionUtilsSpark extends Serializable {
 
-  implicit def partitionGraph[P <: RdfPartition: ClassTag](graphRdd: RDD[Triple], partitioner: RdfPartitioner[P] = RdfPartitionerDefault): Map[P, RDD[Row]] = {
-    val map = Map(partitionGraphArray(graphRdd, partitioner): _*)
-    map
+  implicit def partitionGraph[P <: RdfPartition : ClassTag](graphRdd: RDD[Triple],
+                                                            partitioner: RdfPartitioner[P] = RdfPartitionerDefault): Map[P, RDD[Row]] = {
+    Map(partitionGraphArray(graphRdd, partitioner): _*)
   }
 
-  implicit def partitionGraphArray[P <: RdfPartition: ClassTag](graphRdd: RDD[Triple], partitioner: RdfPartitioner[P] = RdfPartitionerDefault): Array[(P, RDD[Row])] = {
-    val partitions = graphRdd.map(x => partitioner.fromTriple(x)).distinct.collect
+  implicit def partitionGraphArray[P <: RdfPartition : ClassTag](graphRdd: RDD[Triple],
+                                                                 partitioner: RdfPartitioner[P] = RdfPartitionerDefault): Array[(P, RDD[Row])] = {
+    val partitions = graphRdd.map(partitioner.fromTriple).distinct.collect
 
     val array = partitions map { p =>
       (
         p,
         graphRdd
-        .filter(p.matches)
-        .map(t => Row(p.layout.fromTriple(t).productIterator.toList: _*))
-        .persist())
+          .filter(p.matches)
+          .map(t => Row(p.layout.fromTriple(t).productIterator.toList: _*))
+//          .map(t => Row(p.layout.fromTriple(t).productIterator.toList: _*))
+          .persist())
     }
 
     array
