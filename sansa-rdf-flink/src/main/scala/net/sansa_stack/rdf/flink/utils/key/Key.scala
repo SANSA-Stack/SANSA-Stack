b493b2e43bc4a3de3ee38ba4020debb018cf8323
Gezim Sejdiu <g.sejdiu@gmail.com>
2019-12-14 23:37:05 +0100

    Add key type wrapper (Flink), fixes #89
    Co-authored-by: Lorenz Bühmann <buehmann@informatik.uni-leipzig.de>

---
+++ sansa-rdf-flink/src/main/scala/net/sansa_stack/rdf/flink/utils/key/Key.scala
@@ -0,0 +1,18 @@
+package net.sansa_stack.rdf.flink.utils.key
+
+/**
+  * Base of a tuple-like generic key.
+  *
+  * @tparam T The type of the concrete key type.
+  */
+abstract class Key[T <: Key[T]] extends Comparable[T] {
+  /**
+    * Gets the i-th element of the tuple-like key.
+    *
+    * @param pos The position.
+    * @return The element at that key position;
+    */
+  def get(pos: Int): Any
+}
+
+
