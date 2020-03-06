423295631e3126c537e14da03099830558ff576d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2020-03-06 22:51:16 +0100

    Minor import clean-up

--- sansa-owl-spark/src/main/scala/net/sansa_stack/owl/spark/writers/SANSARioRenderer.scala
+++ sansa-owl-spark/src/main/scala/net/sansa_stack/owl/spark/writers/SANSARioRenderer.scala
@@ -1,7 +1,7 @@
 package net.sansa_stack.owl.spark.writers
 
 import org.eclipse.rdf4j.rio.helpers.AbstractRDFWriter
-import org.semanticweb.owlapi.formats.{RDFJsonLDDocumentFormat, RioRDFNonPrefixDocumentFormat}
+import org.semanticweb.owlapi.formats.RioRDFNonPrefixDocumentFormat
 import org.semanticweb.owlapi.model.OWLOntology
 import org.semanticweb.owlapi.rio.RioRenderer
 
