e441028301ad1bdd53f4d1f5708a291594f1bb2d
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2020-03-16 13:12:27 +0100

    Spark-Bench workload for quad parsing

---
+++ sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/io/benchmark/quad/QuadParsing.scala
@@ -0,0 +1,62 @@
+package net.sansa_stack.rdf.spark.io.benchmark.quad
+
+import java.net.URI
+
+import com.ibm.sparktc.sparkbench.utils.{SaveModes, SparkBenchException}
+import com.ibm.sparktc.sparkbench.workload.{Workload, WorkloadDefaults}
+import com.ibm.sparktc.sparkbench.utils.GeneralFunctions._
+import org.apache.hadoop.io.LongWritable
+import org.apache.jena.query.Dataset
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import net.sansa_stack.rdf.common.io.hadoop.TrigFileInputFormat
+import net.sansa_stack.rdf.spark.io.nquads.NQuadReader
+
+import scala.collection.JavaConverters._
+
+
+/**
+ * @author Lorenz Buehmann
+ */
+object QuadParsing extends WorkloadDefaults {
+  override val name: String = "Quad parsing workload"
+
+  // keys in map have been toLowerCase()'d for consistency
+  override def apply(m: Map[String, Any]): Workload =
+    QuadParsingWorkload(input = Option(getOrThrowT[String](m, "input")),
+                        format = getOrThrowT[String](m, "format"))
+}
+
+
+case class QuadParsingCountResult(name: String,
+                                  format: String,
+                                  timestamp: Long,
+                                  totalRuntime: Long,
+                                  numQuads: Long)
+
+case class QuadParsingWorkload(input: Option[String] = None,
+                               output: Option[String] = None,
+                               format: String,
+                               saveMode: String = SaveModes.error)
+  extends Workload {
+
+  override def doWorkload(df: Option[DataFrame], spark: SparkSession): DataFrame = {
+    val startTime = System.currentTimeMillis()
+
+    val (runtime, numQuads) = time {
+      val rdd = format match {
+        case "nquad" => NQuadReader.load(spark, URI.create(input.get))
+        case "trig" =>
+          val hadoopConf = spark.sparkContext.hadoopConfiguration
+          spark.sparkContext.newAPIHadoopFile(input.get, classOf[TrigFileInputFormat],
+            classOf[LongWritable], classOf[Dataset], hadoopConf)
+            .map(_._2)
+            .flatMap(_.asDatasetGraph().find().asScala)
+        case other: String => throw SparkBenchException(s"Unsupported data format for quads: $other")
+      }
+      rdd.count()
+    }
+
+    spark.createDataFrame(Seq(QuadParsingCountResult(QuadParsing.name, format, startTime, runtime, numQuads)))
+  }
+}
