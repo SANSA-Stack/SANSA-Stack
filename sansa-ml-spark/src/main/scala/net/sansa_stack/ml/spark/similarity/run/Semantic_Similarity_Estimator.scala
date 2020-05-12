e4c7714b4bfde7c66b130ae29778b8918401c07e
Carsten <carsten.draschner@gmail.com>
2020-05-12 21:37:47 +0200

    folder structure, minimal worksheet and object, nt file in ressource

---
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/run/Semantic_Similarity_Estimator.scala
@@ -0,0 +1,8 @@
+package net.sansa_stack.ml.spark.similarity.run
+
+object Semantic_Similarity_Estimator {
+
+  def main(args: Array[String]): Unit = {
+    print("Hello World! I'm yor growing semantic similarity estimator")
+  }
+}
