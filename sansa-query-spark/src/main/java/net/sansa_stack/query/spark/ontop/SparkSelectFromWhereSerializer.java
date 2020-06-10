d1aa216f4b5abcfa9a4272d34a8e218496631cce
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2020-06-10 14:13:45 +0200

    Adapt Ontop changes

--- sansa-query-spark/src/main/java/net/sansa_stack/query/spark/ontop/SparkSelectFromWhereSerializer.java
+++ sansa-query-spark/src/main/java/net/sansa_stack/query/spark/ontop/SparkSelectFromWhereSerializer.java
@@ -1,13 +1,10 @@
 package net.sansa_stack.query.spark.ontop;
 
 import com.google.inject.Inject;
-import it.unibz.inf.ontop.answering.reformulation.generation.algebra.SelectFromWhereWithModifiers;
-import it.unibz.inf.ontop.answering.reformulation.generation.dialect.SQLDialectAdapter;
-import it.unibz.inf.ontop.answering.reformulation.generation.serializer.SQLTermSerializer;
-import it.unibz.inf.ontop.answering.reformulation.generation.serializer.impl.DefaultSelectFromWhereSerializer;
 import it.unibz.inf.ontop.dbschema.DBParameters;
-import it.unibz.inf.ontop.dbschema.QuotedIDFactoryStandardSQL;
-import it.unibz.inf.ontop.dbschema.impl.BasicDBParametersImpl;
+import it.unibz.inf.ontop.generation.algebra.SelectFromWhereWithModifiers;
+import it.unibz.inf.ontop.generation.serializer.impl.DefaultSelectFromWhereSerializer;
+import it.unibz.inf.ontop.generation.serializer.impl.SQLTermSerializer;
 
 /**
  * @author Lorenz Buehmann
@@ -15,7 +12,56 @@ import it.unibz.inf.ontop.dbschema.impl.BasicDBParametersImpl;
 public class SparkSelectFromWhereSerializer extends DefaultSelectFromWhereSerializer {
 
     @Inject
-    protected SparkSelectFromWhereSerializer(SQLTermSerializer sqlTermSerializer, SQLDialectAdapter dialectAdapter) {
-        super(sqlTermSerializer, dialectAdapter);
+    protected SparkSelectFromWhereSerializer(SQLTermSerializer sqlTermSerializer) {
+        super(sqlTermSerializer);
+    }
+
+    @Override
+    public QuerySerialization serialize(SelectFromWhereWithModifiers selectFromWhere, DBParameters dbParameters) {
+        return selectFromWhere.acceptVisitor(
+                new DefaultRelationVisitingSerializer(dbParameters.getQuotedIDFactory()) {
+
+                    @Override
+                    protected String serializeLimit(long limit) {
+                        if (limit < 0) {
+                            return "";
+                        } else {
+                            return String.format("LIMIT %d", limit);
+                        }
+                    }
+
+                    @Override
+                    protected String serializeLimitOffset(long limit, long offset) {
+                        if (limit == 0) {
+                            return "LIMIT 0";
+                        }
+
+                        if (limit < 0) {
+                            if (offset <= 0) {
+                                return "";
+                            } else {
+                                return String.format("OFFSET %d ROWS", offset);
+                            }
+                        } else {
+                            if (offset < 0) {
+                                // If the offset is not specified
+                                return String.format("LIMIT %d", limit);
+                            } else {
+                                return String.format("OFFSET %d ROWS\nLIMIT %d", offset, limit);
+                            }
+                        }
+                    }
+
+                    @Override
+                    protected String serializeOffset(long offset) {
+                        if (offset < 0) {
+                            // If the offset is not specified
+                            return "";
+                        } else {
+                            return String.format("OFFSET %d ROWS", offset);
+                        }
+                    }
+                }
+        );
     }
 }
