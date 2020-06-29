857ae9227de13da33a439eb06c82e80e3a4cfb27
Carsten <carsten.draschner@gmail.com>
2020-06-29 14:37:09 +0200

    future idea for class RDF minhash

---
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/RDF_MinHashLSH.scala
@@ -0,0 +1,9 @@
+package net.sansa_stack.ml.spark.similarity
+
+class RDF_MinHashLSH {
+  // TODO constructor
+  // TODO fit method
+  // TODO transform
+  // TODO example call with e.g. approxSimilarityJoin
+
+}
