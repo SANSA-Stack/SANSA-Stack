20c0000b40a1399be597d227e2abc38f48cd28d4
Carsten <carsten.draschner@gmail.com>
2020-07-06 22:06:32 +0200

    placeholder which is now replaced by minhash

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/RDF_MinHashLSH.scala
+++
@@ -1,9 +0,0 @@
-package net.sansa_stack.ml.spark.similarity
-
-class RDF_MinHashLSH {
-  // TODO constructor
-  // TODO fit method
-  // TODO transform
-  // TODO example call with e.g. approxSimilarityJoin
-
-}
