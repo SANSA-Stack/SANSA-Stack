756615c7be0f93639abea3b95e999513ba9ee533
Carsten <carsten.draschner@gmail.com>
2020-07-06 17:25:48 +0200

    rearranged parameter, removed stringsfrom central uri of experiment, and fixed bug RDD creation. now tested and working

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/utils/Similarity_Experiment_Meta_Graph_Factory.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/utils/Similarity_Experiment_Meta_Graph_Factory.scala
@@ -11,24 +11,27 @@ class Similarity_Experiment_Meta_Graph_Factory {
 
   val spark = SparkSession.builder.getOrCreate()
 
-  def transform(df: DataFrame)
-               (metagraph_experiment_name: String,
-                metagraph_experiment_type: String,
-                metagraph_experiment_measurement_type: String
-               )
-               (metagraph_element_relation: String = "element",
-                metagraph_value_relation: String = "value",
-                metagraph_experiment_type_relation: String = "experiment_type",
-                metagraph_experiment_name_relation: String = "experiment_name",
-                metagraph_experiment_measurement_type_relation: String = "experiment_measurement_type",
-                metagraph_experiment_datetime_relation: String = "experiment_datetime"): RDD[Triple] = {
+  def transform(
+               df: DataFrame
+             )(
+              metagraph_experiment_name: String,
+              metagraph_experiment_type: String,
+              metagraph_experiment_measurement_type: String
+             )(
+              metagraph_element_relation: String = "element",
+              metagraph_value_relation: String = "value",
+              metagraph_experiment_type_relation: String = "experiment_type",
+              metagraph_experiment_name_relation: String = "experiment_name",
+              metagraph_experiment_measurement_type_relation: String = "experiment_measurement_type",
+              metagraph_experiment_datetime_relation: String = "experiment_datetime"
+              ): RDD[Triple] = {
 
     val metagraph_evaluation_datetime = Calendar.getInstance().getTime()
       .toString // make string out of it, in future would be better to allow date nativly in rdf
       .replaceAll("\\s", "") // remove spaces to reduce confusions with some foreign file readers
       .replaceAll(":", "") // remove also double points for less confilcting chars.
     // create Central Node
-    val metagraph_experiment_hash: String = metagraph_experiment_type + metagraph_experiment_name + metagraph_evaluation_datetime
+    val metagraph_experiment_hash: String = (metagraph_experiment_type + metagraph_experiment_name + metagraph_evaluation_datetime).replaceAll("\\s", "")
     // Create all inforamtion for this central node
     val central_node_triples = List(
       Triple.create(
@@ -50,7 +53,7 @@ class Similarity_Experiment_Meta_Graph_Factory {
       )
     )
     // TODO now for each line also the other triples to be created
-    spark.sqlContext.sparkContext.parallelize(Seq(central_node_triples))
+    spark.sqlContext.sparkContext.parallelize(central_node_triples)
   }
 
 }
