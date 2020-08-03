f53a3a8480422773a765d3b03e8ea1eedd3d85cb
Carsten <carsten.draschner@gmail.com>
2020-08-03 23:48:50 +0200

    clearer structure, more aligned to scala camelCase and new handling of default values

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/similarity_measures/GenericSimilarityEstimatorModel.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/similarity_measures/GenericSimilarityEstimatorModel.scala
@@ -7,138 +7,135 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class GenericSimilarityEstimatorModel {
 
-  protected var _uri_column_name_dfA: String = "uri"
-  protected var _features_column_name_dfA: String = "uri"
-  protected var _uri_column_name_dfB: String = "features"
-  protected var _features_column_name_dfB: String = "features"
+  protected var _uriColumnNameDfA: String = "uri"
+  protected var _featuresColumnNameDfA: String = "vectorizedFeatures"
+  protected var _uriColumnNameDfB: String = "uri"
+  protected var _featuresColumnNameDfB: String = "vectorizedFeatures"
+
+  protected var _inputCol = "vectorizedFeatures"
 
   // values that have to be overwritten
-  protected var _similarity_estimation_column_name = "genericColumnName"
+  protected var _similarityEstimationColumnName = "estimation"
 
-  val estimator_name: String = "GenericSimilarityEstimator"
-  val estimator_measure_type: String = "distance, related or similarity"
+  val estimatorName: String = "GenericSimilarityEstimator"
+  val estimatorMeasureType: String = "distance, related or similarity"
 
   protected val similarityEstimation = udf( (a: Vector, b: Vector) => {
     throw new Exception("this function should not be called")
   })
-
-  def set_uri_column_name_dfA(uri_column_name: String): this.type = {
-    _uri_column_name_dfA = uri_column_name
+  def setInputCol(inputCol: String): this.type = {
+    _inputCol = inputCol
+    _featuresColumnNameDfA = inputCol
+    _featuresColumnNameDfB = inputCol
     this
   }
 
-  def set_uri_column_name_dfB(uri_column_name: String): this.type = {
-    _uri_column_name_dfB = uri_column_name
+  def setUriColumnNameDfA(uri_column_name: String): this.type = {
+    _uriColumnNameDfA = uri_column_name
     this
   }
 
-  def set_features_column_name_dfA(features_column_name: String): this.type = {
-    _features_column_name_dfA = features_column_name
+  def setUriColumnNameDfB(uri_column_name: String): this.type = {
+    _uriColumnNameDfB = uri_column_name
     this
   }
 
-  def set_features_column_name_dfB(features_column_name: String): this.type = {
-    _features_column_name_dfB = features_column_name
+  def setFeaturesColumnNameDfA(features_column_name: String): this.type = {
+    _featuresColumnNameDfA = features_column_name
     this
   }
 
-  protected def checkColumnNames(df_A: DataFrame, df_B: DataFrame): Unit = {
+  def setFeaturesColumnNameDfB(features_column_name: String): this.type = {
+    _featuresColumnNameDfB = features_column_name
+    this
+  }
+
+  protected def checkColumnNames(dfA: DataFrame, dfB: DataFrame): Unit = {
     // check if column names are set right or if setter method are used right way
-    if (df_A.columns.contains(_uri_column_name_dfA) == false) throw new Exception("current set uri column name: " + _uri_column_name_dfA + "does not align with the column names of given DataFrameA. please set column name with set_uri_column_name_dfA()!")
-    if (df_A.columns.contains(_uri_column_name_dfB) == false) throw new Exception("current set uri column name: " + _uri_column_name_dfB + "does not align with the column names of given DataFrameB. please set column name with set_uri_column_name_dfB()!")
-    if (df_A.columns.contains(_features_column_name_dfA) == false) throw new Exception("current set features column name: " + _features_column_name_dfA + "does not align with the column names of given DataFrameA. please set column name with set_features_column_name_dfA()!")
-    if (df_A.columns.contains(_features_column_name_dfA) == false) throw new Exception("current set features column name: " + _features_column_name_dfA + "does not align with the column names of given DataFrameA. please set column name with set_features_column_name_dfB()!")
-
+    if (dfA.columns.contains(_uriColumnNameDfA) == false) throw new Exception("current set uri column name: " + _uriColumnNameDfA + "does not align with the column names of given DataFrameA. please set column name with set_uri_column_name_dfA()!")
+    if (dfA.columns.contains(_uriColumnNameDfB) == false) throw new Exception("current set uri column name: " + _uriColumnNameDfB + "does not align with the column names of given DataFrameB. please set column name with set_uri_column_name_dfB()!")
+    if (dfA.columns.contains(_featuresColumnNameDfA) == false) throw new Exception("current set features column name: " + _featuresColumnNameDfA + "does not align with the column names of given DataFrameA. please set column name with set_features_column_name_dfA()!")
+    if (dfA.columns.contains(_featuresColumnNameDfA) == false) throw new Exception("current set features column name: " + _featuresColumnNameDfA + "does not align with the column names of given DataFrameA. please set column name with set_features_column_name_dfB()!")
   }
 
-  protected def createCrossJoinDF(df_A: DataFrame, df_B: DataFrame): DataFrame = {
+  protected def createCrossJoinDF(dfA: DataFrame, dfB: DataFrame): DataFrame = {
 
-    var new_uri_column_A: String = _uri_column_name_dfA
-    var new_uri_column_B: String = _uri_column_name_dfB
-
-    if (new_uri_column_A == new_uri_column_B) {
-      new_uri_column_A = new_uri_column_A + "_A"
-      new_uri_column_B = new_uri_column_B + "_B"
-      println("WARNING: names column names were changed because they were the same so added _A and _B, if you want to call nearest neighbors reinitialize!")
-    }
-
-    val new_features_column_A: String = _features_column_name_dfA + "_A"
-    val new_features_column_B: String = _features_column_name_dfB + "_B"
-
-    val cross_join_df: DataFrame =
-      df_A
-        .withColumnRenamed(_uri_column_name_dfA, new_uri_column_A)
-        .withColumnRenamed(_features_column_name_dfA, new_features_column_A)
+    val crossJoinDf: DataFrame =
+      dfA
+        .withColumnRenamed(_uriColumnNameDfA, "uriA")
+        .withColumnRenamed(_featuresColumnNameDfA, "featuresA")
         .crossJoin(
-          df_B
-            .withColumnRenamed(_uri_column_name_dfB, new_uri_column_B)
-            .withColumnRenamed(_features_column_name_dfB, new_features_column_B))
+          dfB
+            .withColumnRenamed(_uriColumnNameDfB, "uriB")
+            .withColumnRenamed(_featuresColumnNameDfB, "featuresB"))
 
-    _uri_column_name_dfA = new_uri_column_A
-    _uri_column_name_dfB = new_uri_column_B
-    _features_column_name_dfA = new_features_column_A
-    _features_column_name_dfB = new_features_column_B
+    /* _uriColumnNameDfA = newUriColumnA
+    _uriColumnNameDfB = newUriColumnB
+    _featuresColumnNameDfA = newFeaturesColumnA
+    _featuresColumnNameDfB = newFeaturesColumnB */
 
-    cross_join_df
+    crossJoinDf
   }
 
-  protected def createNnDF(df: DataFrame, key: Vector, key_uri: String): DataFrame = {
-    var uri = key_uri
-    if (key_uri == "generic_key_uri") uri = uri + key.toString
-
-    _uri_column_name_dfB = "key_uri"
-    _features_column_name_dfB = "key_feature_vector"
+  protected def createNnDF(df: DataFrame, key: Vector, keyUri: String = "unknown"): DataFrame = {
+    var uri = keyUri
+    if (keyUri == "unknown") uri = "genericKeyUri" + key.toString
 
     df
-      .withColumn(_uri_column_name_dfB, lit(key_uri))
-      .withColumn(_features_column_name_dfB, typedLit(key))
-      .select(_uri_column_name_dfB, _uri_column_name_dfA, _features_column_name_dfB, _features_column_name_dfA)
+      .withColumnRenamed(_uriColumnNameDfA, "uriA")
+      .withColumnRenamed(_featuresColumnNameDfA, "featuresA")
+      .withColumn("uriB", lit(uri))
+      .withColumn("featuresB", typedLit(key))
+      .select( "uriA", "uriB", "featuresA", "featuresB")
   }
 
-  protected def set_similarity_estimation_column_name(value_column_name: String): Unit = {
-    _similarity_estimation_column_name = value_column_name
+  protected def setSimilarityEstimationColumnName(valueColumnName: String): Unit = {
+    _similarityEstimationColumnName = valueColumnName
   }
 
-  protected def reduce_join_df(sim_df: DataFrame, threshold: Double): DataFrame = {
-    val tmp_df = sim_df.select(_uri_column_name_dfA, _uri_column_name_dfB, _similarity_estimation_column_name)
+  protected def reduceJoinDf(simDf: DataFrame, threshold: Double): DataFrame = {
+    val tmpDf = simDf.select("uriA", "uriB", _similarityEstimationColumnName)
 
     if (threshold == -1.0) {
-      tmp_df
+      tmpDf
     }
     else {
-      val filtered_df = if (estimator_measure_type == "distance") tmp_df.filter(col(_similarity_estimation_column_name) <= threshold) else tmp_df.filter(col(_similarity_estimation_column_name) >= threshold)
+      val filtered_df = if (estimatorMeasureType == "distance") tmpDf.filter(col(_similarityEstimationColumnName) <= threshold) else tmpDf.filter(col(_similarityEstimationColumnName) >= threshold)
       filtered_df
     }
   }
 
-  protected def reduce_nn_df(sim_df: DataFrame, k: Int, keep_key_uri_column: Boolean): DataFrame = {
-    val tmp_df = if (keep_key_uri_column) sim_df.select(_uri_column_name_dfB, _uri_column_name_dfA, _similarity_estimation_column_name) else sim_df.select(_uri_column_name_dfA, _similarity_estimation_column_name)
-    val ordered_df = if (estimator_measure_type == "distance") tmp_df.orderBy(col(_similarity_estimation_column_name).asc) else tmp_df.orderBy(col(_similarity_estimation_column_name).desc)
-    val clipped_df: DataFrame = ordered_df.limit(k)
-    clipped_df
+  protected def reduceNnDf(simDf: DataFrame, k: Int, keepKeyUriColumn: Boolean): DataFrame = {
+    val tmpDf = if (keepKeyUriColumn) simDf.select("uriA", "uriB", _similarityEstimationColumnName) else simDf.select("uriA", _similarityEstimationColumnName)
+    val orderedDf = if (estimatorMeasureType == "distance") tmpDf.orderBy(col(_similarityEstimationColumnName).asc) else tmpDf.orderBy(col(_similarityEstimationColumnName).desc)
+    val clippedDf: DataFrame = orderedDf.limit(k)
+    clippedDf
   }
 
-  def similarityJoin(df_A: DataFrame, df_B: DataFrame, threshold: Double = -1.0, value_column: String = "generic_similarity"): DataFrame = {
+  def similarityJoin(dfA: DataFrame, dfB: DataFrame, threshold: Double = -1.0, valueColumn: String): DataFrame = {
 
-    val cross_join_df = createCrossJoinDF(df_A: DataFrame, df_B: DataFrame)
+    val crossJoinDf = createCrossJoinDF(dfA: DataFrame, dfB: DataFrame)
 
-    set_similarity_estimation_column_name(value_column)
+    setSimilarityEstimationColumnName(valueColumn)
 
-    val join_df: DataFrame = cross_join_df.withColumn(
-      _similarity_estimation_column_name,
-      similarityEstimation(col(_features_column_name_dfA), col(_features_column_name_dfB))
+    val join_df: DataFrame = crossJoinDf.withColumn(
+      _similarityEstimationColumnName,
+      // similarityEstimation(col(_featuresColumnNameDfA), col(_featuresColumnNameDfB))
+      similarityEstimation(col("featuresA"), col("featuresB"))
     )
-    reduce_join_df(join_df, threshold)
+    reduceJoinDf(join_df, threshold)
   }
 
-  def nearestNeighbors(df_A: DataFrame, key: Vector, k: Int, key_uri: String = "generic_key_uri", value_column: String = "generic_similarity", keep_key_uri_column: Boolean = false): DataFrame = {
+  def nearestNeighbors(dfA: DataFrame, key: Vector, k: Int, keyUri: String, valueColumn: String, keepKeyUriColumn: Boolean = false): DataFrame = {
 
-    set_similarity_estimation_column_name(value_column)
+    setSimilarityEstimationColumnName(valueColumn)
 
-    val nn_setup_df = createNnDF(df_A, key, key_uri)
-    val nn_df = nn_setup_df
-      .withColumn(_similarity_estimation_column_name, similarityEstimation(col(_features_column_name_dfB), col(_features_column_name_dfA)))
+    val nnSetupDf: DataFrame = createNnDF(dfA, key, keyUri)
 
-    reduce_nn_df(nn_df, k, keep_key_uri_column)
+
+    val nnDf = nnSetupDf
+      .withColumn(_similarityEstimationColumnName, similarityEstimation(col("featuresA"), col("featuresB")))
+
+    reduceNnDf(nnDf, k, keepKeyUriColumn)
   }
 }
