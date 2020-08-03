9a107f9d8c073a3dfe16fe138a95d5f9417b40e5
Carsten <carsten.draschner@gmail.com>
2020-08-03 23:55:31 +0200

    fixed bugs, alligned scala style and set default value handling

--- sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/similarity_measures/JaccardModel.scala
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/similarity_measures/JaccardModel.scala
@@ -1,49 +1,52 @@
 package net.sansa_stack.ml.spark.similarity.similarity_measures
 
+import org.apache.spark
 import org.apache.spark.ml.linalg.Vector
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.sql.functions.{col, lit, typedLit, udf}
+import org.apache.spark.sql.types.{ArrayType, StringType, StructField, StructType}
 
 class JaccardModel extends GenericSimilarityEstimatorModel {
 
   protected val jaccard = udf( (a: Vector, b: Vector) => {
-    val feature_indices_a = a.toSparse.indices
-    val feature_indices_b = b.toSparse.indices
-    val f_set_a = feature_indices_a.toSet
-    val f_set_b = feature_indices_b.toSet
-    val jaccard = f_set_a.intersect(f_set_b).size.toDouble / f_set_a.union(f_set_b).size.toDouble
+    val featureIndicesA = a.toSparse.indices
+    val featureIndicesB = b.toSparse.indices
+    val fSetA = featureIndicesA.toSet
+    val fSetB = featureIndicesB.toSet
+    val jaccard = fSetA.intersect(fSetB).size.toDouble / fSetA.union(fSetB).size.toDouble
     jaccard
   })
 
-  override val estimator_name: String = "JaccardSimilarityEstimator"
-  override val estimator_measure_type: String = "similarity"
+  override val estimatorName: String = "JaccardSimilarityEstimator"
+  override val estimatorMeasureType: String = "similarity"
 
   override val similarityEstimation = jaccard
 
-  override def similarityJoin(df_A: DataFrame, df_B: DataFrame, threshold: Double = -1.0, value_column: String = "jaccard_similarity"): DataFrame = {
+  override def similarityJoin(dfA: DataFrame, dfB: DataFrame, threshold: Double = -1.0, valueColumn: String = "jaccard_similarity"): DataFrame = {
 
-    val cross_join_df = createCrossJoinDF(df_A: DataFrame, df_B: DataFrame)
+    setSimilarityEstimationColumnName(valueColumn)
 
-    set_similarity_estimation_column_name(value_column)
+    val crossJoinDf = createCrossJoinDF(dfA: DataFrame, dfB: DataFrame)
 
-    val join_df: DataFrame = cross_join_df.withColumn(
-      _similarity_estimation_column_name,
-      similarityEstimation(col(_features_column_name_dfA), col(_features_column_name_dfB))
+    val joinDf: DataFrame = crossJoinDf.withColumn(
+      valueColumn,
+      similarityEstimation(col("featuresA"), col("featuresB"))
     )
-    reduce_join_df(join_df, threshold)
+    reduceJoinDf(joinDf, threshold)
   }
 
-  override def nearestNeighbors(df_A: DataFrame, key: Vector, k: Int, key_uri: String = "unknown", value_column: String = "jaccard_similarity", keep_key_uri_column: Boolean = false): DataFrame = {
+  override def nearestNeighbors(dfA: DataFrame, key: Vector, k: Int, keyUri: String = "unknown", valueColumn: String = "jaccardSimilarity", keepKeyUriColumn: Boolean = false): DataFrame = {
 
-    set_similarity_estimation_column_name(value_column)
+    setSimilarityEstimationColumnName(valueColumn)
 
-    val nn_setup_df = createNnDF(df_A, key, key_uri)
+    val nnSetupDf = createNnDF(dfA, key, keyUri)
 
-    val nn_df = nn_setup_df
+    val nnDf = nnSetupDf
       .withColumn(
-        _similarity_estimation_column_name,
-        similarityEstimation(col(_features_column_name_dfB), col(_features_column_name_dfA)))
+        valueColumn,
+        similarityEstimation(col("featuresA"), col("featuresB")))
 
-    reduce_nn_df(nn_df, k, keep_key_uri_column)
+    reduceNnDf(nnDf, k, keepKeyUriColumn)
   }
 }
