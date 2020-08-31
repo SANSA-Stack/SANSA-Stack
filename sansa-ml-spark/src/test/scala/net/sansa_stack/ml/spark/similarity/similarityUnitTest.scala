2593ad9f10247a175a86cff2658453f7f58e6a50
Carsten <carsten.draschner@gmail.com>
2020-08-31 16:49:20 +0200

    started with unit tests for semantic simialrity estimation

---
+++ sansa-ml-spark/src/test/scala/net/sansa_stack/ml/spark/similarity/similarityUnitTest.scala
@@ -0,0 +1,126 @@
+package net.sansa_stack.ml.spark.similarity
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import net.sansa_stack.ml.spark.similarity.similarityEstimationModels.{BatetModel, BraunBlanquetModel, DiceModel, JaccardModel, OchiaiModel, SimpsonModel, TverskyModel}
+import net.sansa_stack.ml.spark.utils.FeatureExtractorModel
+import org.apache.jena.riot.Lang
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.scalatest.FunSuite
+import net.sansa_stack.rdf.spark.io._
+import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel, MinHashLSH, MinHashLSHModel}
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.types.DataTypes
+
+class similarityUnitTest extends FunSuite with DataFrameSuiteBase {
+
+  // define inputpath if it is not parameter
+  val inputPath = "src/test/resources/similarity/movie.nt"
+  println(inputPath)
+
+
+  test("Read in Data and create a Feature DataFrame") {
+
+    // read in data as Data`Frame
+    val triplesDf: DataFrame = spark.read.rdf(Lang.NTRIPLES)(inputPath)
+    triplesDf.show()
+
+    val modesToTest = List("an", "at", "as", "or")
+
+    for (mode <- modesToTest) {
+      println("feature extraction with mode: " + mode)
+      val featureExtractorModel = new FeatureExtractorModel()
+        .setMode(mode)
+      val extractedFeaturesDataFrame = featureExtractorModel
+        .transform(triplesDf)
+        .filter(t => t.getAs[String]("uri").startsWith("m"))
+      extractedFeaturesDataFrame.show()
+    }
+  }
+
+
+
+  test("Test different similarity estimation Models") {
+
+    // read in data as Data`Frame
+    val triplesDf: DataFrame = spark.read.rdf(Lang.NTRIPLES)(inputPath)
+
+    /**
+     * for next tests we need the count vectorized feature representation
+     */
+    // feature extraction
+    val featureExtractorModel = new FeatureExtractorModel()
+      .setMode("at")
+    val extractedFeaturesDataFrame = featureExtractorModel
+      .transform(triplesDf)
+    // filter for relevant URIs e.g. only movies
+    val filteredFeaturesDataFrame = extractedFeaturesDataFrame.filter(t => t.getAs[String]("uri").startsWith("m"))
+
+    // count Vectorization
+    val cvModel: CountVectorizerModel = new CountVectorizer()
+      .setInputCol("extractedFeatures")
+      .setOutputCol("vectorizedFeatures")
+      .fit(filteredFeaturesDataFrame)
+    val tmpCvDf: DataFrame = cvModel.transform(filteredFeaturesDataFrame)
+    val isNoneZeroVector = udf({ v: Vector => v.numNonzeros > 0 }, DataTypes.BooleanType)
+    val countVectorizedFeaturesDataFrame: DataFrame = tmpCvDf.filter(isNoneZeroVector(col("vectorizedFeatures"))).select("uri", "vectorizedFeatures")
+
+    // similarity Estimations Overview
+    // for nearestNeighbors we need one key which is a Vector to search for NN
+    val sample_key: Vector = countVectorizedFeaturesDataFrame.take(1)(0).getAs[Vector]("vectorizedFeatures")
+
+    // minHash similarity estimation
+    val minHashModel: MinHashLSHModel = new MinHashLSH()
+      .setInputCol("vectorizedFeatures")
+      .setOutputCol("hashedFeatures")
+      .fit(countVectorizedFeaturesDataFrame)
+    minHashModel.approxNearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10, "minHashDistance").show()
+    minHashModel.approxSimilarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, 0.8, "distance").show()
+
+    // Jaccard similarity
+    val jaccardModel: JaccardModel = new JaccardModel()
+      .setInputCol("vectorizedFeatures")
+    jaccardModel.nearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10).show()
+    jaccardModel.similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, threshold = 0.5).show()
+
+    // Batet Distance
+    val batetModel: BatetModel = new BatetModel()
+      .setInputCol("vectorizedFeatures")
+    batetModel.nearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10).show()
+    batetModel.similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, threshold = 0.5).show()
+
+    // Braun Blanquet Similarity
+    val braunBlanquetModel: BraunBlanquetModel = new BraunBlanquetModel()
+      .setInputCol("vectorizedFeatures")
+    braunBlanquetModel.nearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10).show()
+    braunBlanquetModel.similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, threshold = 0.5).show()
+
+    // Dice Similarity
+    val diceModel: DiceModel = new DiceModel()
+      .setInputCol("vectorizedFeatures")
+    diceModel.nearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10).show()
+    diceModel.similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, threshold = 0.5).show()
+
+    // Ochiai Similarity
+    val ochiaiModel: OchiaiModel = new OchiaiModel()
+      .setInputCol("vectorizedFeatures")
+    ochiaiModel.nearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10).show()
+    ochiaiModel.similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, threshold = 0.5).show()
+
+    // Simpson Similarity
+    val simpsonModel: SimpsonModel = new SimpsonModel()
+      .setInputCol("vectorizedFeatures")
+    simpsonModel.nearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10).show()
+    simpsonModel.similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, threshold = 0.5).show()
+
+    // Tversky Similarity
+    val tverskyModel: TverskyModel = new TverskyModel()
+      .setInputCol("vectorizedFeatures")
+      .setAlpha(1.0)
+      .setBeta(1.0)
+    tverskyModel.nearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10).show()
+    tverskyModel.similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, threshold = 0.5).show()
+
+  }
+}
