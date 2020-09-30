d0cb5fd85638731ae6bb82ae9f71ca88ffe8416c
Carsten <carsten.draschner@gmail.com>
2020-09-30 20:58:50 +0200

    created minhash on basis of apache spark minhash lsh and added behavior as in other generic similarity estimators to allow better follow up handling of comulmns for e.g. metagraph creation

---
+++ sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/similarityEstimationModels/MinHashModel.scala
@@ -0,0 +1,40 @@
+package net.sansa_stack.ml.spark.similarity.similarityEstimationModels
+
+import org.apache.spark.ml.feature.{MinHashLSH, MinHashLSHModel}
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, lit, udf}
+
+class MinHashModel extends GenericSimilarityEstimatorModel {
+
+  override val estimatorName: String = "MinHashLSHSimilarityEstimator"
+  override val estimatorMeasureType: String = "distance"
+
+
+  override def similarityJoin(dfA: DataFrame, dfB: DataFrame, threshold: Double = -1.0, valueColumn: String = "distCol"): DataFrame = {
+
+    val minHashModel: MinHashLSHModel = new MinHashLSH()
+      .setInputCol("vectorizedFeatures")
+      .setOutputCol("hashedFeatures")
+      .fit(dfA)
+    // minHashModel.approxNearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10, "minHashDistance").show()
+    minHashModel
+      .approxSimilarityJoin(dfA, dfB, threshold, valueColumn)
+      .withColumn("uri_A", col("datasetA").getField("uri"))
+      .withColumn("uri_B", col("datasetB").getField("uri"))
+      .select("uri_A", "uri_B", valueColumn)
+  }
+
+  override def nearestNeighbors(dfA: DataFrame, key: Vector, k: Int, keyUri: String = "unknown", valueColumn: String = "jaccardSimilarity", keepKeyUriColumn: Boolean = false): DataFrame = {
+
+    val minHashModel: MinHashLSHModel = new MinHashLSH()
+      .setInputCol("vectorizedFeatures")
+      .setOutputCol("hashedFeatures")
+      .fit(dfA)
+    // minHashModel.approxNearestNeighbors(countVectorizedFeaturesDataFrame, sample_key, 10, "minHashDistance").show()
+    minHashModel
+      .approxNearestNeighbors(dfA, key, k, valueColumn)
+      .withColumn("key_column", lit("key_uri"))
+      .select("key_column", "uri", valueColumn)
+  }
+}
