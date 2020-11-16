a908c4ca3664b24e4b19afc00fa9ae9bfdac481f
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2020-11-16 15:11:53 +0100

    Cntd. char. sets

--- sansa-rdf/sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/characteristc_sets/CharacteristicSets.scala
+++ sansa-rdf/sansa-rdf-spark/src/main/scala/net/sansa_stack/rdf/spark/partition/characteristc_sets/CharacteristicSets.scala
@@ -1,18 +1,73 @@
 package net.sansa_stack.rdf.spark.partition.characteristc_sets
 
+import java.io.File
+
 import com.google.common.base.Charsets
 import com.google.common.hash.Hashing
+import com.mxgraph.util.mxCellRenderer
 import org.apache.jena.graph.{Node, Triple}
 import org.apache.jena.riot.Lang
 import org.apache.jena.sparql.serializer.SerializationContext
 import org.apache.jena.sparql.util.FmtUtils
+import org.apache.jena.util.SplitIRI
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.functions.collect_set
 import org.apache.spark.sql.types.{StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.jgrapht.alg.TransitiveReduction
+import org.jgrapht.graph.{DefaultDirectedGraph, DefaultEdge}
+import org.jgrapht.io.GraphMLExporter
 
-case class CharacteristicSet(properties: Set[Node]) {}
+case class CharacteristicSet(properties: Set[Node]) {
+  def isSubsetOf(cs: CharacteristicSet): Boolean = properties.subsetOf(cs.properties)
+  def subsumes(cs: CharacteristicSet): Boolean = cs.properties.subsetOf(properties)
+}
+
+object CharacteristicSetGraphBuilder {
+
+  def create(css: Set[CharacteristicSet]) : DefaultDirectedGraph[CharacteristicSet, DefaultEdge] = {
+    val g = new DefaultDirectedGraph[CharacteristicSet, DefaultEdge](classOf[DefaultEdge])
+
+    for {cs1 <- css
+         cs2 <- css
+         if cs1 != cs2} {
+      if (cs1.isSubsetOf(cs2)) {
+        g.addVertex(cs1)
+        g.addVertex(cs2)
+        g.addEdge(cs1, cs2)
+      }
+    }
+
+    TransitiveReduction.INSTANCE.reduce(g)
+
+    saveAsImage(g)
+    exportGraphML(g)
+
+    g
+  }
+
+  def saveAsImage(g: DefaultDirectedGraph[CharacteristicSet, DefaultEdge]): Unit = {
+    import javax.imageio.ImageIO
+    import java.awt.Color
+    import com.mxgraph.layout.mxFastOrganicLayout
+    val graphAdapter = new org.jgrapht.ext.JGraphXAdapter(g)
+    val layout = new mxFastOrganicLayout(graphAdapter)
+    layout.execute(graphAdapter.getDefaultParent)
+
+    val image = mxCellRenderer.createBufferedImage(graphAdapter, null, 2, Color.WHITE, true, null)
+    val imgFile = new File("/tmp/graph.png")
+    ImageIO.write(image, "PNG", imgFile)
+  }
+
+  def exportGraphML(g: DefaultDirectedGraph[CharacteristicSet, DefaultEdge]): Unit = {
+   val exporter = new GraphMLExporter[CharacteristicSet, DefaultEdge]()
+
+    exporter.setVertexLabelProvider(v => v.properties.map(n => SplitIRI.localname(n.getURI)).mkString(","))
+
+    exporter.exportGraph(g, new File("/tmp/graph.graphml"))
+  }
+}
 
 /**
  * @author Lorenz Buehmann
@@ -43,7 +98,7 @@ object CharacteristicSets {
 
   implicit val mirror = ru.runtimeMirror(getClass.getClassLoader)
 
-  private def asRow(subj: Node, predObjPairs: Iterable[(Node, Node)]): Row = {
+  private def asRow(subj: Node, predObjPairs: Iterable[(Node, Node)]): (Row, StructType) = {
     // the subject first
     var values: IndexedSeq[AnyRef] = IndexedSeq[AnyRef](getObject(subj))
 
@@ -64,12 +119,18 @@ object CharacteristicSets {
 
     println(schema)
 
-    Row.fromSeq(values)
+    Row.fromSeq(values) -> schema
   }
 
   private def getObject(node: Node): AnyRef = {
     if (node.isLiteral) {
-      node.getLiteral.getValue
+      // literals with no datatype or custom datatype do not have a Java class mapping
+      // in this case we use the string representation
+      if (node.getLiteralDatatype.getJavaClass == null) {
+        node.toString
+      } else {
+        node.getLiteralValue
+      }
     } else {
       FmtUtils.stringForNode(node, null.asInstanceOf[SerializationContext])
     }
@@ -103,28 +164,51 @@ object CharacteristicSets {
     val path = args(0)
     val triples = spark.rdf(Lang.NTRIPLES)(path)
 
-    val css = computeCharacteristicSets(triples)
+    val css = computeCharacteristicSets(triples).collect().toSet
+    println(s"found ${css.size} CSs")
+    css.foreach(println)
 
-    css.collect().foreach(println)
+    CharacteristicSetGraphBuilder.create(css)
 
 
+    // process each CS and get subjects having all properties in a CS, then get the p-o pairs
+    css.foreach { cs =>
 
-    css.collect().foreach { cs =>
+      val rowWithSchema = triples
+        .map(t => (t.getSubject, (t.getPredicate, t.getObject))) // t => (s, (p,o))
+        .groupByKey() // (s, [(p1,o1),...,(pn,on)])
+        .filter(e => cs.properties subsetOf e._2.map(_._1).toSet) // [(p1,o1),...,(pn,on)] contains cs ?
+        .map { case (s, po) => asRow(s, po) } // create rows
+        .take(1) // trigger action
 
-      val subjToPredObjPairs = triples
-        .filter(t => cs.properties.contains(t.getPredicate))
-        .map(t => (t.getSubject, (t.getPredicate, t.getObject)))
-        .groupByKey()
+      val hf = Hashing.sha256().newHasher()
+      val tableName = hf.putString(cs.properties.toSeq.sortBy(_.getURI).mkString(","), Charsets.UTF_8).hash().toString
+      spark.createDataFrame(spark.sparkContext.emptyRDD[Row], rowWithSchema.head._2).createOrReplaceTempView(tableName)
 
-      val rows = subjToPredObjPairs.map { case (s, po) => asRow(s, po) }
-      rows.count()
+      println(rowWithSchema.head._2.toDDL)
 
+////      val subjToPredObjPairs = triples
+////        .filter(t => cs.properties.contains(t.getPredicate))
+////        .map(t => (t.getSubject, (t.getPredicate, t.getObject)))
+////        .groupByKey()
+////
+////      val rows = subjToPredObjPairs.map { case (s, po) => asRow(s, po) }
+////      rows.count()
+//
 //      val hf = Hashing.sha256().newHasher()
 //      val tableName = hf.putString(cs.properties.toSeq.sortBy(_.getURI).mkString(","), Charsets.UTF_8).hash().toString
 
       //      val df = spark.createDataFrame(subjToPredObjPairs).createOrReplaceTempView(tableName)
 
     }
+
+    val db = spark.catalog.currentDatabase
+    val tables = spark.catalog.listTables()
+    tables.foreach {t =>
+      val ddl = spark.sql(s"SHOW CREATE TABLE ${t.name}")
+      println(ddl)
+    }
+
 
     spark.stop()
   }
