06bee897492995f901629decef0fefe6a6ef9f26
carstendraschner <carsten.draschner@gmail.com>
2020-12-05 16:13:42 +0100

    delete classes and folders of modules planned to be deleted from release 0.8

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/clustering/datatypes/DbStatusEnum.scala
+++
@@ -1,7 +0,0 @@
-package net.sansa_stack.ml.spark.clustering.datatypes
-
-object DbStatusEnum extends Enumeration {
-
-    type DBSTATUS = Value
-    val UNDEFINED, NOISE, PARTOFCLUSTER = Value
-}
--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/clustering/datatypes/MdsCoordinates.scala
+++
@@ -1,4 +0,0 @@
-package net.sansa_stack.ml.spark.clustering.datatypes
-
-case class MdsCoordinates(coordinates: Array[MdsCoordinate])
-
