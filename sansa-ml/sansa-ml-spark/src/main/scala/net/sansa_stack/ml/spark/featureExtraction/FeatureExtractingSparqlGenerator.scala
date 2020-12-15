c97cfa4b6783e1ca6830dda9906107699e27238d
carstendraschner <carsten.draschner@gmail.com>
2020-12-15 13:29:22 +0100

    adjusted literal identification

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/featureExtraction/FeatureExtractingSparqlGenerator.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/featureExtraction/FeatureExtractingSparqlGenerator.scala
@@ -99,6 +99,7 @@ object FeatureExtractingSparqlGenerator {
         // current paths are the ones we want to follow in next iteration. so it is reasonable if   TODO better literal identification
         val isLiteral = udf((cellElement: String) => {
           if (cellElement.startsWith("\"")) true
+          else if (cellElement.forall(_.isDigit)) true
           else if (cellElement.isInstanceOf[Int]) true
           else if (cellElement.isInstanceOf[Boolean]) true
           else if (cellElement.isInstanceOf[Float]) true
