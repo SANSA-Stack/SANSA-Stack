ac7b75cf2d687a67d430fa407967acb08daf357d
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2021-01-26 15:27:25 +0100

    SPARQL engine example improved

--- sansa-examples/sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/query/SPARQLEngineExample.scala
+++ sansa-examples/sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/query/SPARQLEngineExample.scala
@@ -3,11 +3,10 @@ package net.sansa_stack.examples.spark.query
 import java.awt.Desktop
 import java.net.URI
 
-import scala.collection.convert.ImplicitConversions.`iterator asScala`
-
 import org.aksw.jena_sparql_api.server.utils.FactoryBeanSparqlServer
-import org.apache.jena.query.QueryFactory
-import org.apache.jena.riot.Lang
+import org.apache.jena.query.{QueryFactory, ResultSetFormatter}
+import org.apache.jena.rdf.model.ModelFactory
+import org.apache.jena.riot.{Lang, RDFDataMgr}
 import org.apache.jena.sys.JenaSystem
 import org.apache.spark.sql.SparkSession
 
@@ -18,16 +17,23 @@ import net.sansa_stack.query.spark.sparqlify.QueryEngineFactorySparqlify
 import net.sansa_stack.rdf.spark.io._
 
 /**
-  * Run SPARQL queries over Spark using Ontop as SPARQL-to-SQL rewriter.
-  */
-object SPARQLEngine {
+ * This example shows how to run SPARQL queries over Spark using a SPARQL-to-SQL rewriter under the hood.
+ * The RDF data can be provided either as triples file or even as a pre-partitioned dataset maintained in tables
+ * of a Spark database and a corresponding R2RML mapping file.
+ *
+ * Two different modes for demonstrator purposes can be used:
+ * - run a single query
+ * - start a SPARQL endpoint with a Web UI as optional interface
+ *
+ */
+object SPARQLEngineExample {
 
-  JenaSystem.init
+  JenaSystem.init()
 
   def main(args: Array[String]) {
     parser.parse(args, Config()) match {
       case Some(config) =>
-        run(config.in, config.database, config.partitioningMetadataPath, config.queryEngine,
+        run(config.in, config.database, config.mappingsfile, config.queryEngine,
           config.sparql, config.runMode, config.port)
       case None =>
         println(parser.usage)
@@ -36,10 +42,10 @@ object SPARQLEngine {
 
   def run(input: String,
           database: String,
-          partitioningMetadataPath: URI,
+          mappingsFile: URI,
           queryEngine: SPARQLEngine.Value,
           sparqlQuery: String = "",
-          run: String = "cli",
+          mode: Option[String],
           port: Int = 7531): Unit = {
 
     println("======================================")
@@ -57,28 +63,39 @@ object SPARQLEngine {
       .enableHiveSupport()
       .getOrCreate()
 
-    // load the data into an RDD
-    val lang = Lang.NTRIPLES
-    val triples = spark.rdf(lang)(input)
-
+    // we build the query engine here
     val queryEngineFactory = queryEngine match {
       case Ontop => new QueryEngineFactoryOntop(spark)
       case Sparqlify => new QueryEngineFactorySparqlify(spark)
       case _ => throw new RuntimeException("Unsupported query engine")
     }
 
-    val qef = queryEngineFactory.create(triples)
+    // create the query execution factory
+    val qef =
+      if (database != null && mappingsFile != null) { // pre-partitioned case
+        // load the R2RML mappings
+        val mappings = ModelFactory.createDefaultModel()
+        RDFDataMgr.read(mappings, mappingsFile.toString)
+
+        queryEngineFactory.create(database, mappings)
+      } else {
+        // load the data into an RDD
+        val lang = Lang.NTRIPLES
+        val triples = spark.rdf(lang)(input)
+
+        queryEngineFactory.create(triples)
+      }
 
     // run i) a single SPARQL query and terminate or ii) host some SNORQL web UI
-    run match {
-      case "cli" =>
+    mode.get match {
+      case "batch" =>
         // only SELECT queries will be considered here
         val query = QueryFactory.create(sparqlQuery)
         val qe = qef.createQueryExecution(query)
 
-        val result = qe.execSelect()
+        val rs = qe.execSelect()
         // show bindings on command line
-        result.foreach(println)
+        ResultSetFormatter.out(rs)
       case "endpoint" =>
         val server = FactoryBeanSparqlServer.newInstance.setSparqlServiceFactory(qef).setPort(port).create()
         if (Desktop.isDesktopSupported) {
@@ -89,21 +106,20 @@ object SPARQLEngine {
     }
 
     spark.stop
-
   }
 
   implicit val sparqlEngineRead: scopt.Read[SPARQLEngine.Value] = scopt.Read.reads(SPARQLEngine.withName)
 
   case class Config(in: String = null,
                     database: String = null,
-                    partitioningMetadataPath: URI = null,
-                    queryEngine: SPARQLEngine.Value = null,
+                    mappingsfile: URI = null,
+                    queryEngine: SPARQLEngine.Value = SPARQLEngine.Ontop,
                     sparql: String = "SELECT * WHERE {?s ?p ?o} LIMIT 10",
-                    runMode: String = "cli",
+                    runMode: Option[String] = None,
                     port: Int = 7531,
                     browser: Boolean = true)
 
-  val parser = new scopt.OptionParser[Config]("SPARQL example") {
+  val parser = new scopt.OptionParser[Config]("SPARQLEngineExample") {
 
     head("SPARQL example")
 
@@ -112,46 +128,53 @@ object SPARQLEngine {
       action((x, c) => c.copy(in = x)).
       text("path to file that contains the data (in N-Triples format)")
 
-    opt[URI]( "metadata")
-      .action((x, c) => c.copy(partitioningMetadataPath = x))
-      .text("path to partitioning metadata")
-
     opt[String]("database")
       .abbr("db")
       .action((x, c) => c.copy(database = x))
       .text("the name of the Spark database used as KB")
 
+    opt[URI]( "mappings-file")
+      .action((x, c) => c.copy(mappingsfile = x))
+      .text("path to R2RML mappings")
+
     opt[SPARQLEngine]("sparql-engine")
       .action((x, c) => c.copy(queryEngine = x))
-      .text("the SPARQL backend ('Ontop', 'Sparqlify'")
-
-    opt[String]('q', "query").optional().valueName("<query>").
-      action((x, c) => c.copy(sparql = x)).
-      validate(x => try {
-        QueryFactory.create(x)
-        success
-      } catch {
-        case e: Exception =>
-          e.printStackTrace()
-          failure("Must be a valid SPARQL query.")
-      }).
-      text("a SPARQL query")
-
-    opt[String]('m', "mode").optional().valueName("run mode").
-      action((x, c) => c.copy(runMode = x)).
-      validate(x => if (x == "cli" || x == "endpoint") success
-                    else failure("wrong run mode: use either 'cli' or 'endpoint'")).
-      text("Runner mode ('cli', 'endpoint'). Default:'cli'")
-
-    opt[Int]('p', "port").optional().valueName("port").
-      action((x, c) => c.copy(port = x)).
-      text("port that SPARQL endpoint will be exposed, default:'7531'")
+      .optional()
+      .text("the SPARQL backend ('Ontop', 'Sparqlify')")
 
     checkConfig(c =>
-      if (c.runMode == "cli" && c.sparql.isEmpty) failure("Option --sparql must not be empty if cli is enabled")
-      else success)
+      if (c.runMode.isDefined) success else failure("A command is required.")
+    )
+
 
     help("help").text("prints this usage text")
+
+    cmd("endpoint")
+      .action((_, c) => c.copy(runMode = Some("endpoint")))
+      .text("start a SPARQL endpoint")
+      .children(
+        opt[Int]('p', "port").optional().valueName("<port-number>").
+          action((x, c) => c.copy(port = x)).
+          text("port that SPARQL endpoint will be exposed, default:'7531'")
+      )
+
+    cmd("batch")
+      .action((_, c) => c.copy(runMode = Some("batch")))
+      .text("run a single SPARQL query and show results")
+      .children(
+        opt[String]('q', "query").valueName("<query>").
+          action((x, c) => c.copy(sparql = x)).
+          validate(x => try {
+            val q = QueryFactory.create(x)
+            if (q.isSelectType) success else failure("Only SELECT queries are supported in the demo")
+          } catch {
+            case e: Exception =>
+              e.printStackTrace()
+              failure("Must be a valid SPARQL query.")
+          }).
+          text("a SPARQL query")
+    )
+
   }
 
 }
