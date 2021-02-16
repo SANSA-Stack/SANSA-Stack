b60a3c6909307025f87e89bd9944a32cbf6cc3e7
Claus Stadler <RavenArkadon@googlemail.com>
2021-02-17 00:32:47 +0100

    Fixed typos and bug in KryoArrayUtils.java

--- sansa-rdf/sansa-rdf-common/src/main/java/net/sansa_stack/rdf/common/kryo/jena/KryoArrayUtils.java
+++ sansa-rdf/sansa-rdf-common/src/main/java/net/sansa_stack/rdf/common/kryo/jena/KryoArrayUtils.java
@@ -6,22 +6,26 @@ import com.esotericsoftware.kryo.io.Output;
 
 import java.lang.reflect.Array;
 
+/** Utils to write arrays to kryo - probably not needed; the code that uses it is probably legacy ~ Claus Stadler */
 public class KryoArrayUtils {
 
-    public static <T> void write(Kryo kyro, Output output, T[] items) {
+    public static <T> void write(Kryo kryo, Output output, T[] items) {
         int len = items.length;
-        output.writeInt(len);
+        output.writeInt(len, true);
         for (T item : items) {
-            kyro.writeClassAndObject(output, item);
+            kryo.writeClassAndObject(output, item);
         }
     }
 
-    public static <T> T[] read(Kryo kyro, Input input, Class<T> clazz) {
+    public static <T> T[] read(Kryo kryo, Input input, Class<T> clazz) {
         int len = input.readInt(true);
+        @SuppressWarnings("unchecked")
         T[] result = (T[]) Array.newInstance(clazz, len);
         for (int i = 0; i < len; ++i) {
-            Object obj = kyro.readClassAndObject(input);
-            result[i] = (T)obj;
+            Object obj = kryo.readClassAndObject(input);
+            @SuppressWarnings("unchecked")
+            T t = (T)obj;
+            result[i] = t;
         }
         return result;
     }
