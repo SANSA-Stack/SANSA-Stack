a187ac13388e4c9bfa36a795005329e85077f0eb
carstendraschner <carsten.draschner@gmail.com>
2021-02-25 22:46:28 +0100

    MmDistSim span up exploded dataframe with feature ordering

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/MmDistSim/TryOutMmDistSim.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/MmDistSim/TryOutMmDistSim.scala
@@ -7,7 +7,8 @@ import net.sansa_stack.rdf.spark.io.NTripleReader
 import net.sansa_stack.rdf.spark.model.TripleOperations
 import org.apache.jena.sys.JenaSystem
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.functions.{collect_list, collect_set, struct}
+import org.apache.spark.sql.functions.{col, collect_list, collect_set, struct}
+import org.apache.spark.sql.types.{DataType, DoubleType, FloatType, IntegerType, LongType, StringType, StructField, StructType}
 
 import scala.collection.mutable.ListBuffer
 
@@ -126,7 +127,7 @@ object TryOutMmDistSim {
     val sparqlFrame = new SparqlFrame()
       .setSparqlQuery(queryString)
       .setQueryExcecutionEngine(SPARQLEngine.Sparqlify)
-    val res = sparqlFrame.transform(dataset)
+    val res = sparqlFrame.transform(dataset).limit(20).cache() // TODO remove limit
     res.show(false)
 
     /*
@@ -150,12 +151,12 @@ object TryOutMmDistSim {
     each entity has only one line
     also column names will encode feature type
      */
-    val cols = res.columns
     val keyColumnNameString: String = "movie"
-    val featureColumns = Set(res.columns: _*).filter(!Set(keyColumnNameString).contains(_))
+    val featureColumns: Seq[String] = List(res.columns: _*).filter(!Set(keyColumnNameString).contains(_)).toSeq
+    println(s"feature columns $featureColumns")
 
     // val listOfCollectedDataframes = ListBuffer[DataFrame]()
-    var collectedDataFrame = res.select(keyColumnNameString).dropDuplicates()
+    var collectedDataFrame = res.select(keyColumnNameString).dropDuplicates().limit(3).cache() // TODO remove limit if everthing is ready
     println("starting dataframe")
 
     for (currentFeatureColumnNameString <- featureColumns) {
@@ -168,8 +169,83 @@ object TryOutMmDistSim {
     }
 
     println(collectedDataFrame.schema)
-    collectedDataFrame.show(false)
+
+
+
+    val tmpDf1 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_1"): _*)
+    val tmpDf2 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_2"): _*)
+      /* for (featureColumnName <- featureColumns) {
+        tmpDf1 = tmpDf2.withColumnRenamed(featureColumnName, featureColumnName + "_1")
+        tmpDf2 = tmpDf2.withColumnRenamed(featureColumnName, featureColumnName + "_2")
+      }
+       */
+
+    val crossJoinColumnNames: Seq[String] = tmpDf1.columns.zip(tmpDf2.columns).flatMap(t => Seq(t._1, t._2)).toSeq
+
+    val crossJoinedDFs = tmpDf1.crossJoin(tmpDf2)
+      .filter(!(col(crossJoinColumnNames(0)) === col(crossJoinColumnNames(1))))
+      .select(crossJoinColumnNames.map(col(_)): _*)
+    crossJoinedDFs.show(false)
+
+    /*
+    Try out calculating single simialrity estimation of a pair of rows
+
+    val row1: Row = collectedDataFrame.limit(2).first()
+    val row2: Row = collectedDataFrame.limit(2).first()
+
+    println(row1)
+    println(row2)
+
+    def prototypeSimilairty(r1: Row, r2: Row, featureColumns: Seq[String], oldSchema: StructType): Double = {
+
+      val schemaR1: StructType = r1.schema
+      val schemaR2: StructType = r2.schema
+
+      println(schemaR1)
+      println(schemaR2)
+
+      for (currentFeatureColumnIndex <- 1 to featureColumns.size-1) {
+        val currentFeatureColumnNameString = featureColumns(currentFeatureColumnIndex)
+        println(currentFeatureColumnNameString)
+
+
+
+        val sfR1: StructField = schemaR1(currentFeatureColumnIndex)
+        val sfR2: StructField = schemaR2(currentFeatureColumnIndex)
+
+        val typeR1: DataType = sfR1.dataType
+        val typeR2: DataType = sfR2.dataType
+
+        println(typeR1, typeR2)
+        println(typeR1, typeR2)
+
+        val innerTypeR1 = oldSchema(currentFeatureColumnIndex).dataType match {
+          case StringType => String
+          case IntegerType => Int
+          case _ =>
+        }
+        println(f"inner data type $innerTypeR1")
+
+        println(r1, r2, r1.schema, r2.schema)
+
+        val fs1 = r1.getAs[collection.mutable.WrappedArray[innerTypeR1]](currentFeatureColumnIndex).toSeq
+        val fs2 = r2.getAs[collection.mutable.WrappedArray[innerTypeR1]](currentFeatureColumnIndex).toSeq
+        // println(currentFeatureColumnNameString)
+        println(fs1, fs2, fs1.getClass, fs2.getClass)
+        for (fe <- fs1) {
+          println(fe, fe.getClass)
+        }
+        for (fe <- fs2) {
+          println(fe, fe.getClass)
+        }
+      }
+      0.0
+    }
+
+    prototypeSimilairty(row1, row2, featureColumns, res.schema) */
   }
+
+
 }
 
 
