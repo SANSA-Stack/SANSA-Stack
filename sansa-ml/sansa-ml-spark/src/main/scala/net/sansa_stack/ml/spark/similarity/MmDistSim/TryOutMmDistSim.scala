239b23cb04f68cce0c4b125aaffa36cb6c292543
carstendraschner <carsten.draschner@gmail.com>
2021-02-26 21:32:43 +0100

    caclulate jaccard for each feature as baseline

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/MmDistSim/TryOutMmDistSim.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/MmDistSim/TryOutMmDistSim.scala
@@ -6,10 +6,12 @@ import net.sansa_stack.rdf.common.io.riot.error.{ErrorParseMode, WarningParseMod
 import net.sansa_stack.rdf.spark.io.NTripleReader
 import net.sansa_stack.rdf.spark.model.TripleOperations
 import org.apache.jena.sys.JenaSystem
+import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.functions.{col, collect_list, collect_set, struct}
+import org.apache.spark.sql.functions.{col, collect_list, collect_set, greatest, struct, udf}
 import org.apache.spark.sql.types.{DataType, DoubleType, FloatType, IntegerType, LongType, StringType, StructField, StructType}
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object TryOutMmDistSim {
@@ -66,9 +68,6 @@ object TryOutMmDistSim {
         | ?movie__down_runtime
         | ?movie__down_actor__down_actor_name
         | ?movie__down_genre__down_film_genre_name
-        | ?movie__down_country__down_country_name
-        | ?movie__down_country__down_country_languages
-        | ?movie__down_country__down_country_areaInSqKm
         |
         |WHERE {
         |	?movie <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://data.linkedmdb.org/movie/film> .
@@ -94,21 +93,6 @@ object TryOutMmDistSim {
         |		?movie <http://data.linkedmdb.org/movie/genre> ?movie__down_genre .
         |		?movie__down_genre <http://data.linkedmdb.org/movie/film_genre_name> ?movie__down_genre__down_film_genre_name .
         |	}
-        |
-        | OPTIONAL {
-        |		?movie <http://data.linkedmdb.org/movie/country> ?movie__down_country .
-        |		?movie__down_country <http://data.linkedmdb.org/movie/country_name> ?movie__down_country__down_country_name .
-        |	}
-        |
-        | OPTIONAL {
-        |		?movie <http://data.linkedmdb.org/movie/country> ?movie__down_country .
-        |		?movie__down_country <http://data.linkedmdb.org/movie/country_languages> ?movie__down_country__down_country_languages .
-        |	}
-        |
-        | OPTIONAL {
-        |		?movie <http://data.linkedmdb.org/movie/country> ?movie__down_country .
-        |		?movie__down_country <http://data.linkedmdb.org/movie/country_areaInSqKm> ?movie__down_country__down_country_areaInSqKm .
-        |	}
         |}
       """.stripMargin
 
@@ -127,7 +111,7 @@ object TryOutMmDistSim {
     val sparqlFrame = new SparqlFrame()
       .setSparqlQuery(queryString)
       .setQueryExcecutionEngine(SPARQLEngine.Sparqlify)
-    val res = sparqlFrame.transform(dataset).limit(20).cache() // TODO remove limit
+    val res = sparqlFrame.transform(dataset).filter(col("movie__down_genre__down_film_genre_name") === "Superhero").limit(10000).cache() // TODO remove limit
     res.show(false)
 
     /*
@@ -156,7 +140,7 @@ object TryOutMmDistSim {
     println(s"feature columns $featureColumns")
 
     // val listOfCollectedDataframes = ListBuffer[DataFrame]()
-    var collectedDataFrame = res.select(keyColumnNameString).dropDuplicates().limit(3).cache() // TODO remove limit if everthing is ready
+    var collectedDataFrame = res.select(keyColumnNameString).dropDuplicates().cache()
     println("starting dataframe")
 
     for (currentFeatureColumnNameString <- featureColumns) {
@@ -174,78 +158,48 @@ object TryOutMmDistSim {
 
     val tmpDf1 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_1"): _*)
     val tmpDf2 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_2"): _*)
-      /* for (featureColumnName <- featureColumns) {
-        tmpDf1 = tmpDf2.withColumnRenamed(featureColumnName, featureColumnName + "_1")
-        tmpDf2 = tmpDf2.withColumnRenamed(featureColumnName, featureColumnName + "_2")
-      }
-       */
 
     val crossJoinColumnNames: Seq[String] = tmpDf1.columns.zip(tmpDf2.columns).flatMap(t => Seq(t._1, t._2)).toSeq
 
     val crossJoinedDFs = tmpDf1.crossJoin(tmpDf2)
       .filter(!(col(crossJoinColumnNames(0)) === col(crossJoinColumnNames(1))))
       .select(crossJoinColumnNames.map(col(_)): _*)
+      .cache()
     crossJoinedDFs.show(false)
 
-    /*
-    Try out calculating single simialrity estimation of a pair of rows
+    var featureSimilarityScores: DataFrame = crossJoinedDFs.cache() // .select(crossJoinColumnNames(0), crossJoinColumnNames(1))
 
-    val row1: Row = collectedDataFrame.limit(2).first()
-    val row2: Row = collectedDataFrame.limit(2).first()
+    val similarityEstimation = udf( (a: mutable.WrappedArray[Any], b: mutable.WrappedArray[Any]) => {
 
-    println(row1)
-    println(row2)
+      val intersectionCard = a.toSet.intersect(b.toSet).size.toDouble
+      val unionCard = a.toSet.union(b.toSet).size.toDouble
+      val res = if (unionCard > 0) intersectionCard / unionCard else 0.0
+      res
+    })
 
-    def prototypeSimilairty(r1: Row, r2: Row, featureColumns: Seq[String], oldSchema: StructType): Double = {
+    for (featureName <- featureColumns) {
+      println(featureName)
+      val fc1 = f"collect_set($featureName)_1"
+      val fc2 = f"collect_set($featureName)_2"
 
-      val schemaR1: StructType = r1.schema
-      val schemaR2: StructType = r2.schema
+      val scn = f"${featureName}_Similarity"
 
-      println(schemaR1)
-      println(schemaR2)
-
-      for (currentFeatureColumnIndex <- 1 to featureColumns.size-1) {
-        val currentFeatureColumnNameString = featureColumns(currentFeatureColumnIndex)
-        println(currentFeatureColumnNameString)
-
-
-
-        val sfR1: StructField = schemaR1(currentFeatureColumnIndex)
-        val sfR2: StructField = schemaR2(currentFeatureColumnIndex)
-
-        val typeR1: DataType = sfR1.dataType
-        val typeR2: DataType = sfR2.dataType
-
-        println(typeR1, typeR2)
-        println(typeR1, typeR2)
-
-        val innerTypeR1 = oldSchema(currentFeatureColumnIndex).dataType match {
-          case StringType => String
-          case IntegerType => Int
-          case _ =>
-        }
-        println(f"inner data type $innerTypeR1")
-
-        println(r1, r2, r1.schema, r2.schema)
-
-        val fs1 = r1.getAs[collection.mutable.WrappedArray[innerTypeR1]](currentFeatureColumnIndex).toSeq
-        val fs2 = r2.getAs[collection.mutable.WrappedArray[innerTypeR1]](currentFeatureColumnIndex).toSeq
-        // println(currentFeatureColumnNameString)
-        println(fs1, fs2, fs1.getClass, fs2.getClass)
-        for (fe <- fs1) {
-          println(fe, fe.getClass)
-        }
-        for (fe <- fs2) {
-          println(fe, fe.getClass)
-        }
-      }
-      0.0
+      featureSimilarityScores = featureSimilarityScores
+        .withColumn(
+          scn,
+        similarityEstimation(col(fc1), col(fc2))
+      )
+        // .filter(col(scn) > 0)
     }
 
-    prototypeSimilairty(row1, row2, featureColumns, res.schema) */
+    val similairtyColumns = featureColumns.map(_ + "_Similarity")
+
+    featureSimilarityScores.limit(20).show(false)
+
+    featureSimilarityScores = featureSimilarityScores.filter(greatest(similairtyColumns.filter(_ != "movie__down_genre__down_film_genre_name_Similarity").map(col): _*) > 0)
+
+    featureSimilarityScores.limit(20).show(false)
   }
-
-
 }
 
 
