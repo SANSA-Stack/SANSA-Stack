7c2d1d1d48d1b153d12de6bb24ad3c55ef7fe691
carstendraschner <carsten.draschner@gmail.com>
2021-03-01 22:34:48 +0100

    some changes: - write rdf to file - changes fors to foreach - added prints to better follow process

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/MmDistSim/TryOutMmDistSim.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/MmDistSim/TryOutMmDistSim.scala
@@ -1,5 +1,7 @@
 package net.sansa_stack.ml.spark.similarity.MmDistSim
 
+import java.util.Calendar
+
 import net.sansa_stack.ml.spark.featureExtraction.{FeatureExtractingSparqlGenerator, SparqlFrame}
 import net.sansa_stack.ml.spark.utils.SimilarityExperimentMetaGraphFactory
 import net.sansa_stack.query.spark.SPARQLEngine
@@ -39,7 +41,7 @@ object TryOutMmDistSim {
       inputFileString,
       stopOnBadTerm = ErrorParseMode.SKIP,
       stopOnWarnings = WarningParseMode.IGNORE
-    ).toDS().cache()
+    ).toDS() // .cache()
     val numberTriples = dataset.count()
     println(f"READ IN DATA:\ndata consists of ${numberTriples} triples")
     dataset.take(n = 10).foreach(println(_))
@@ -117,8 +119,8 @@ object TryOutMmDistSim {
     val sparqlFrame = new SparqlFrame()
       .setSparqlQuery(queryString)
       .setQueryExcecutionEngine(SPARQLEngine.Sparqlify)
-    val res = sparqlFrame.transform(dataset).cache()
-    res.show(false)
+    val queryResultDf = sparqlFrame.transform(dataset).cache()
+    queryResultDf.show(false)
 
     /*
     Classify Column Type
@@ -144,63 +146,77 @@ object TryOutMmDistSim {
 
     // specify column names
     val keyColumnNameString: String = "movie"
-    val featureColumns: Seq[String] = List(res.columns: _*).filter(!Set(keyColumnNameString).contains(_)).toSeq
+    val featureColumns: Seq[String] = List(queryResultDf.columns: _*).filter(!Set(keyColumnNameString).contains(_)).toSeq
     println(s"feature columns $featureColumns")
 
 
     // collaps features into arrays instead of having multiple rows
-    var collectedDataFrame = res.select(keyColumnNameString).dropDuplicates().cache()
+    println("COLLAPS FEATURES TO SETS")
+    var collectedDataFrame = queryResultDf.select(keyColumnNameString).dropDuplicates().cache()
 
-    for (currentFeatureColumnNameString <- featureColumns) {
-      println(currentFeatureColumnNameString)
-      val tmpDf = res.select(keyColumnNameString, currentFeatureColumnNameString)
-      // tmpDf.show(false)
-      val collectedTmpDf = tmpDf.groupBy(keyColumnNameString).agg(collect_set(currentFeatureColumnNameString)).as(currentFeatureColumnNameString)
-      // collectedTmpDf.show(false)
-      collectedDataFrame = collectedDataFrame.join(collectedTmpDf, keyColumnNameString)
-    }
+    featureColumns.foreach(
+      currentFeatureColumnNameString =>
+      collectedDataFrame = collectedDataFrame.join(
+        queryResultDf
+          .select(keyColumnNameString, currentFeatureColumnNameString)
+          .groupBy(keyColumnNameString)
+          .agg(collect_set(currentFeatureColumnNameString))
+          .as(currentFeatureColumnNameString),
+        keyColumnNameString)
+    )
 
-    println(s"in total we have we have: ${collectedDataFrame.count()} elements")
+    println(s"In total we have we have: ${collectedDataFrame.count()} elements")
+    println(s"Sample to see behavior on smaller data snippets")
     collectedDataFrame = collectedDataFrame
-      // .sample(withReplacement = false, fraction = 0.01, seed = 504)
-    // println(s"after sampling we have: ${collectedDataFrame.count()} elements")
+      // .sample(withReplacement = false, fraction = 0.1, seed = 504) // TODO remove in later version
+    println(s"after sampling we have: ${collectedDataFrame.count()} elements")
 
     println(collectedDataFrame.schema)
     collectedDataFrame.show(false)
 
+    println("CREATE CROSS JOINED DF")
     /*
     cross join for all pair similairity
      */
-    val tmpDf1 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_1"): _*)
-    val tmpDf2 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_2"): _*)
+    val tmpDf1 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_1"): _*).cache()
+    val tmpDf2 = collectedDataFrame.toDF(collectedDataFrame.columns.map(_ + "_2"): _*).cache()
 
+    // create column name list to have them in paired order
     val crossJoinColumnNames: Seq[String] = tmpDf1.columns.zip(tmpDf2.columns).flatMap(t => Seq(t._1, t._2)).toSeq
 
     // drop diagonal entries
     // re order columns to have theim in pairs
     val crossJoinedDFs = tmpDf1.crossJoin(tmpDf2)
-      .filter(!(col(crossJoinColumnNames(0)) === col(crossJoinColumnNames(1))))
-      .select(crossJoinColumnNames.map(col(_)): _*)
+      // drop lines where same entities are present
+      .filter(!(col(keyColumnNameString + "_1") === col(keyColumnNameString + "_2")))
+      // reorder to have them in pairs
+      .select(crossJoinColumnNames.map(col(_)): _*) // TODO remove in later version
       .cache()
-    crossJoinedDFs.show(false)
+
+    crossJoinedDFs.show(false) // TODO remove in later version
+    // println(s"Full crossJoinedDFs size:  ${crossJoinedDFs.count()}") // TODO remove in later version
 
     // dataframe we operate on for similairty calculation
-    var featureSimilarityScores: DataFrame = crossJoinedDFs.cache()
+    var featureSimilarityScores: DataFrame = crossJoinedDFs // .cache()
 
     // udf for naive Jaccard index as first placeholder
     val similarityEstimation = udf( (a: mutable.WrappedArray[Any], b: mutable.WrappedArray[Any]) => {
-      val intersectionCard = a.toSet.intersect(b.toSet).size.toDouble
-      val unionCard = a.toSet.union(b.toSet).size.toDouble
+      val intersectionCard: Double = a.toSet.intersect(b.toSet).size.toDouble
+      val unionCard: Double = a.toSet.union(b.toSet).size.toDouble
       val res = if (unionCard > 0) intersectionCard / unionCard else 0.0
       res
     })
 
+
+    println("WEIGHTS AND THRESHOLDS")
     // drop thresholds
-    val minSimThresholds = mutable.Map(featureColumns.map((cn: String) => Tuple2(cn, 0.0)).toMap.toSeq: _*)
+    val minSimThresholds: mutable.Map[String, Double] = mutable.Map(featureColumns.map((cn: String) => Tuple2(cn, 0.0)).toMap.toSeq: _*)
+    // TODO Temporal change
+    minSimThresholds("movie__down_actor__down_actor_name") = 0.01 // TODO remove in later version
 
     // similairity column names
     val similairtyColumns = featureColumns.map(_ + "_Similarity")
-    println(f"similairtyColumns:\n$similairtyColumns")
+    println(f"similairtyColumns:\n$similairtyColumns") // TODO remove in later version
 
     // these weights are user given or calculated
     val importance: mutable.Map[String, Double] = mutable.Map(featureColumns.map((cn: String) => Tuple2(cn, 1.0)).toMap.toSeq: _*)
@@ -218,90 +234,101 @@ object TryOutMmDistSim {
     val importanceNormed: Map[String, Double] = normWeights(importance)
     val reliabilityNormed: Map[String, Double] = normWeights(reliability)
     val availabilityNormed: Map[String, Double] = normWeights(availability)
-    println(f"importanceNormed:\n$importanceNormed")
-    println(f"reliabilityNormed:\n$reliabilityNormed")
-    println(f"availabilityNormed:\n$availabilityNormed")
+    println(f"IMPORTANTS WEIGHTS: importanceNormed:\n$importanceNormed")
+    println(f"RELIABILITY WEIGHTS: reliabilityNormed:\n$reliabilityNormed")
+    println(f"AVAILABILITY WEIGHTS: availabilityNormed:\n$availabilityNormed")
+    println(f"MIN SIMILARITY THRESHOLDS:\n$minSimThresholds")
+
 
 
     val orderedFeatureColumnNamesByImportance: Seq[String] = ListMap(importanceNormed.toSeq.sortWith(_._2 > _._2): _*).map(_._1).toSeq
-    println(f"orderedFeatureColumnNamesByImportance:\n$orderedFeatureColumnNamesByImportance")
-
-
-    // TODO Temporal change
-    minSimThresholds("movie__down_actor__down_actor_name") = 0.01
-
-    println(f"min Similarity Thresholds:\n$minSimThresholds")
+    println(f"orderedFeatureColumnNamesByImportance:\n$orderedFeatureColumnNamesByImportance \n")
 
     /*
     Apply similairty score for pairs
      */
-    println("Apply similairty score for pairs")
-    for (featureName <- orderedFeatureColumnNamesByImportance) {
-      println(featureName)
-      val fc1 = f"collect_set($featureName)_1"
-      val fc2 = f"collect_set($featureName)_2"
+    println("CALCULATE COLUMN WISE SIMILARITY SCORES")
+    orderedFeatureColumnNamesByImportance.foreach(
+      featureName => {
+        println(featureName) // TODO remove in later version
+        // feature column name 1
+        val fc1 = f"collect_set($featureName)_1"
+        // feature column name 2
+        val fc2 = f"collect_set($featureName)_2"
+        // similarity column name
+        val scn = f"${featureName}_Similarity"
 
-      val scn = f"${featureName}_Similarity"
-
-      featureSimilarityScores = featureSimilarityScores
-        .withColumn(
-          scn,
-        similarityEstimation(col(fc1), col(fc2))
-      ).filter(col(scn) >= minSimThresholds(featureName))
-    }
+        featureSimilarityScores = featureSimilarityScores
+          .withColumn(
+            scn,
+            similarityEstimation(col(fc1), col(fc2))
+          )
+          // filter all rows where minimal similarity is not reached
+          .filter(col(scn) >= minSimThresholds(featureName))
+      }
+    )
 
     // drop columns where no similarity at all is given
+    println("DROP ROWS WHERE NO SIMILARITY IS GIVEN")
     featureSimilarityScores = featureSimilarityScores.filter(greatest(similairtyColumns.map(col): _*) > 0)
-    featureSimilarityScores.show(false)
+    println(f"number of pair with similairty above 0 and above thresholds: ${featureSimilarityScores.count()}")
+    featureSimilarityScores.show(false) // TODO remove in later version
 
     // calculate overall similarity
 
-
-
     // calculated weighted sum of all similairties
-    println("calculated weighted sum of all similairties")
-    for (featureColumn <- featureColumns) {
-      val similairtyColumn = featureColumn + "_Similarity"
-      featureSimilarityScores = featureSimilarityScores
-        .withColumn(
-          f"${similairtyColumn}_weighted",
-          col(similairtyColumn) * importanceNormed(featureColumn) * reliabilityNormed(featureColumn) * availabilityNormed(featureColumn)
-        )
-    }
+    println("CALCULATED WEIGHTED SIMILAIRTY SUM")
+    println("weight similairties")
+    featureColumns.foreach(
+      featureColumn => {
+        println(featureColumn)
+        val similairtyColumn = featureColumn + "_Similarity"
+        featureSimilarityScores = featureSimilarityScores
+          .withColumn(
+            f"${similairtyColumn}_weighted",
+            col(similairtyColumn) * importanceNormed(featureColumn) * reliabilityNormed(featureColumn) * availabilityNormed(featureColumn)
+          )
+      }
+    )
+
+    println("calc weighted sum")
     featureSimilarityScores = featureSimilarityScores
       .withColumn("overallSimilarity", similairtyColumns.map(s => s + "_weighted")
         .map(col).reduce(_ + _))
-    featureSimilarityScores.show(false)
-
-
-    // select only needed columns
-    /* featureSimilarityScores = featureSimilarityScores.select(
-      featureSimilarityScores.columns(0),
-      featureSimilarityScores.columns(1),
-      featureSimilarityScores.columns.last
-    )
-     */
+    featureSimilarityScores.show(false) // TODO remove in later version
 
     // order desc
-    // println("order desc")
-    // featureSimilarityScores = featureSimilarityScores.orderBy(desc(featureSimilarityScores.columns.last))
-
-    // featureSimilarityScores.show(false)
+    println("ORDER SIMIALRITIES DESC")
+    println("order desc")
+    featureSimilarityScores = featureSimilarityScores.orderBy(desc(featureSimilarityScores.columns.last))
+    featureSimilarityScores.show(false) // TODO remove in later version
 
 
     // make results rdf
+    println("SELECT ONLY NEEDED COLUMN")
+    featureSimilarityScores = featureSimilarityScores.select(featureSimilarityScores.columns(0), featureSimilarityScores.columns(1), featureSimilarityScores.columns.last)
+    featureSimilarityScores.show(false)
 
+    println("CREATE METAGRAPH")
     val mgc = new SimilarityExperimentMetaGraphFactory
     val semanticResult: Dataset[org.apache.jena.graph.Triple] = mgc.createRdfOutput(
-      outputDataset = featureSimilarityScores.select(featureSimilarityScores.columns(0), featureSimilarityScores.columns(1), featureSimilarityScores.columns.last)
+      outputDataset = featureSimilarityScores
+        // .limit(100) // TODO be careful with this limit, only to check memory issues
     )(
       modelInformationEstimatorName = "DaDistSim",
       modelInformationEstimatorType = "Similarity",
       modelInformationMeasurementType = "Mix"
     )(inputDatasetNumbertOfTriples = numberTriples, dataSetInformationFilePath = inputFileString
-    ).toDS()
+    ).toDS() // .cache()
 
-    semanticResult.take(50).foreach(println(_))
+    println("Metagraph looks like")
+    semanticResult.take(20).foreach(println(_))
+    println(f"Resulting Semantic Annotated Similarity consists of ${semanticResult.count()} triples")
+
+    val outputFolderPath = args(1)
+    val outputFilePath = f"${outputFolderPath}DaDistSimResult${Calendar.getInstance().getTime().toString.replace(" ", "")}"
+    println(f"STORE METAGRAPH\nwrite resulting MG to ${outputFilePath}")
+    semanticResult.rdd.coalesce(1).saveAsNTriplesFile(outputFilePath)
   }
 }
 
