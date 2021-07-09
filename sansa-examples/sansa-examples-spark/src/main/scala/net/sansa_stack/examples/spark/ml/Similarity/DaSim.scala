a3be9d944c2e6aff25a643af5c839c6f933f4fbd
carstendraschner <carsten.draschner@gmail.com>
2021-07-09 15:57:01 +0200

    play around with different feature extractors and further use DistRDF2ML modules for feature extraction

--- sansa-examples/sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/Similarity/DaSim.scala
+++ sansa-examples/sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/ml/Similarity/DaSim.scala
@@ -1,6 +1,6 @@
 package net.sansa_stack.examples.spark.ml.Similarity
 
-import net.sansa_stack.ml.spark.featureExtraction.{SmartVectorAssembler, SparqlFrame}
+import net.sansa_stack.ml.spark.featureExtraction.{FeatureExtractingSparqlGenerator, SmartVectorAssembler, SparqlFrame}
 import net.sansa_stack.ml.spark.similarity.similarityEstimationModels.MinHashModel
 import net.sansa_stack.ml.spark.utils.{FeatureExtractorModel, ML2Graph}
 import net.sansa_stack.rdf.common.io.riot.error.{ErrorParseMode, WarningParseMode}
@@ -13,7 +13,7 @@ import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel}
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.regression.RandomForestRegressor
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, Dataset, Encoder, SparkSession}
 
 object DaSim {
@@ -66,14 +66,32 @@ object DaSim {
     val sf = new SparqlFrame()
       .setSparqlQuery(p_seed_fetching_sparql)
 
-    val seeds = sf.transform(dataset).limit(10) // TODO this is temporary
+    val seeds = sf.transform(dataset).limit(100) // TODO this is temporary
 
+    println("seeds2")
+    val seeds2 = dataset.filter(t => ((t.getObject.toString().equals("http://data.linkedmdb.org/movie/film")) & (t.getPredicate.toString().equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"))))
+    seeds2
+      .rdd
+      .toDF()
+      .limit(100)
+      .show(false)
+    println("seeds1")
     seeds.show(false)
     println(seeds.count())
 
     println("GATHER CANDIDATE PAIRS")
 
     implicit val rdfTripleEncoder: Encoder[Triple] = org.apache.spark.sql.Encoders.kryo[Triple]
+
+    import spark.implicits._
+
+    val seedsList = seeds.select("seed").map(r => r.toString()).collect()
+
+    val filtered2 = dataset
+      .filter(t => seedsList.contains(t.getSubject))
+    filtered2
+      .take(100)
+      .foreach(println(_))
 
     val filtered: Dataset[Triple] = seeds.rdd
       .map(r => Tuple2(r(0).toString, r(0)))
@@ -83,7 +101,6 @@ object DaSim {
       .as[Triple]
     filtered.take(20).foreach(println(_))
     println(filtered.count())
-
 
     val triplesDf = filtered.rdd.toDF()
 
@@ -104,7 +121,9 @@ object DaSim {
     val tmpCvDf: DataFrame = cvModel.transform(extractedFeaturesDataFrame)
     // val isNoneZeroVector = udf({ v: Vector => v.numNonzeros > 0 }, DataTypes.BooleanType)
     val isNoneZeroVector = udf({ v: Vector => v.numNonzeros > 0 })
-    val countVectorizedFeaturesDataFrame: DataFrame = tmpCvDf.filter(isNoneZeroVector(col("vectorizedFeatures"))).select("uri", "vectorizedFeatures").cache()
+    val countVectorizedFeaturesDataFrame: DataFrame = tmpCvDf
+      // .filter(isNoneZeroVector(col("vectorizedFeatures")))
+      .select("uri", "vectorizedFeatures").cache()
     countVectorizedFeaturesDataFrame.show(false)
 
     // similarity Estimations Overview
@@ -115,9 +134,63 @@ object DaSim {
       .setInputCol("vectorizedFeatures")
       .setOutputCol("hashedFeatures")
       .fit(countVectorizedFeaturesDataFrame) */
-    minHashModel
-      .similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, 0.8, "distance")
+    val simDF = minHashModel
+      .similarityJoin(countVectorizedFeaturesDataFrame, countVectorizedFeaturesDataFrame, 0.5, "distance")
+      .filter(col("uriA").notEqual(col("uriB")))
+    simDF.show(false)
+
+    println("PROMISING CANDDATES")
+    val candidatesPerElement = 3
+    simDF.sort("uriA", "distance").show(false)
+    //  .groupBy("uriA").
+
+    println("LITERAL2FEATURE SPARQL QUERY")
+    val seedVarName = "?seed"
+    val whereClauseForSeed = "?seed <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://data.linkedmdb.org/movie/film>"
+    val maxUp = 0
+    val maxDown = 1
+    val numberSeeds = 5
+
+    val (totalSparqlQuery: String, var_names: List[String]) = FeatureExtractingSparqlGenerator.createSparql(
+      ds = dataset,
+      seedVarName = seedVarName,
+      seedWhereClause = whereClauseForSeed,
+      maxUp = maxUp,
+      maxDown = maxDown,
+      numberSeeds = numberSeeds
+    )
+    println(totalSparqlQuery)
+
+    println("SPARQL FRAME FEATURE EXTTRACTION")
+
+    val sf2 = new SparqlFrame()
+      .setSparqlQuery(totalSparqlQuery)
+      .setCollapsByKey(true)
+      .setCollapsColumnName("seed")
+
+    val featureDf = sf2
+      .transform(dataset)
+
+    featureDf.show(false)
+
+    println("PIVOT FEATURE EXTRACTOR")
+
+    val pivotFeatureDF = filtered
+      .rdd
+      .toDF()
+      .groupBy("s")
+      .pivot("p")
+      .agg(collect_list("o"))
+      .limit(1000)
+    pivotFeatureDF
       .show(false)
+
+
+
+
+
+
+
 
 
   }
