7a947930aa38ae1ca857cf0a1675a6dc249b3324
hebaallahibrahim <heba.a.ibrahim@gmail.com>
2022-02-28 13:23:54 +0100

    add statistical utils for TDT

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/classification/decisionTrees/Utils/StatsUtils.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/classification/decisionTrees/Utils/StatsUtils.scala
@@ -1,5 +1,27 @@
 package net.sansa_stack.ml.spark.classification.decisionTrees.Utils
 
+import org.apache.commons.math3.stat.StatUtils
+
 object StatsUtils {
   
+  
+  private var mean = .0
+  private var stddev = .0
+  
+  def average(rate: Array[Double]): Double = {
+    mean = StatUtils.mean(rate)
+    mean
+  }
+  
+  def variance(arr: Array[Double]): Double = {
+    stddev = StatUtils.variance(arr)
+    stddev
+  }
+  
+  
+  def stdDeviation(arr: Array[Double]): Double = {
+    stddev = StatUtils.variance(arr)
+    Math.sqrt(stddev)
+  
+  }
 }
