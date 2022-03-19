11244ae267b07191053fa386006b47c21ee440bc
Claus Stadler <RavenArkadon@googlemail.com>
2022-03-19 15:43:48 +0100

    Added skeleton for improving CSV multiline content detection (really tiresome battles with regexes and offsets)

--- sansa-hadoop-jena/src/main/java/net/sansa_stack/hadoop/core/pattern/CustomPatternCsv.java
+++ sansa-hadoop-jena/src/main/java/net/sansa_stack/hadoop/core/pattern/CustomPatternCsv.java
@@ -1,8 +1,57 @@
 package net.sansa_stack.hadoop.core.pattern;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * Matcher that searches for newlines that are NOT within a multiline field.
+ *
+ *
+ */
 public class CustomPatternCsv
     implements CustomPattern
 {
+    public static class Config {
+        protected char quoteChar;
+        protected char escapeChar;
+        protected String delimiter;
+        protected String lineTerminatorPattern;
+
+        public Config(char quoteChar, char escapeChar, String delimiter, String lineTerminatorPattern) {
+            this.quoteChar = quoteChar;
+            this.escapeChar = escapeChar;
+            this.delimiter = delimiter;
+            this.lineTerminatorPattern = lineTerminatorPattern;
+        }
+
+        public static Config createDefault() {
+            return new Config('"', '\\', ",", "\r?\n\r?");
+        }
+
+        public char getQuoteChar() {
+            return quoteChar;
+        }
+
+        public char getEscapeChar() {
+            return escapeChar;
+        }
+
+        public String getDelimiter() {
+            return delimiter;
+        }
+
+        public String getLineTerminatorPattern() {
+            return lineTerminatorPattern;
+        }
+    }
+
+
+    public static CustomPatternCsv create(Config dialect) {
+        return null;
+    }
+
+
     @Override
     public CustomMatcher matcher(CharSequence charSequence) {
         return new CustomMatcherCsv(charSequence);
@@ -14,33 +63,171 @@ public class CustomPatternCsv
         protected CharSequence charSequence;
         protected int regionStart;
         protected int regionEnd;
+        protected int newlineMatchStart = -1;
+        // protected int matchEnd = -1;
 
-        protected int matchStart;
-        protected int matchEnd;
+
+        protected boolean nextQuoteExamined = false;
+        protected int nextQuoteEnd = -1;
+        protected int nextQuoteStart = -1;
 
         protected char quoteChar;
         protected char quoteEscapeChar;
 
+        protected int pos;
+
+
         public CustomMatcherCsv(CharSequence charSequence) {
             this.charSequence = charSequence;
+            this.regionStart = 0;
+            this.regionEnd = charSequence.length();
         }
 
         @Override
         public void region(int start, int end) {
             this.regionStart = start;
             this.regionEnd = end;
+
+            this.pos = start;
+        }
+
+
+        // TODO finish the pattern
+        // In reverse matching the escape character is seen after the quote character
+        public static Pattern createBwdQuotePattern(char quoteChar, char escapeChar) {
+            String equoteFieldEnd = "\"(\r?\n|,|$)";
+            return null;
+        }
+
+        public static Pattern createFwdQuotePattern(char quoteChar, char escapeChar) {
+            String equoteFieldEnd = "(?<!((?<!\\\\)(\\\\\\\\){0,10}))\"(\r?\n|,|$)";
+
+            if (quoteChar != '"') {
+                String qcp = Pattern.quote(Character.toString(quoteChar));
+                equoteFieldEnd = equoteFieldEnd.replace("\"", qcp);
+            }
+
+            if (escapeChar != '\\') {
+                String ecp = Pattern.quote(Character.toString(escapeChar));
+                equoteFieldEnd = equoteFieldEnd.replace("\\\\", ecp);
+            }
+
+            Pattern result = Pattern.compile(equoteFieldEnd);
+            return result;
+        }
+
+        // Create a pattern for matching csv-like double quotes - where
+        // double-double quote escapes another double quote
+        // This pattern works for forward and reverse matching
+        public static Pattern createQuotePattern(char quoteChar) {
+            String equoteFieldEnd = "(?<!((?<!\")(\"\"){0,10})\")\"(\r?\n|,|$)";
+
+            if (quoteChar != '"') {
+                String qcp = Pattern.quote(Character.toString(quoteChar));
+                equoteFieldEnd = equoteFieldEnd.replace("\"", qcp);
+            }
+
+            Pattern result = Pattern.compile(equoteFieldEnd);
+            return result;
         }
 
         @Override
         public boolean find() {
-            // well, logic would go here
+            if (pos < 0) {
+                return false;
+            }
 
-            return false;
+            // createFwdQuotePattern('\"', '\\');
+            // Match the first equote of a sequence of possibly escaped quote chars
+            // String equoteStart = "((?<!\")\"(?=(\"\"){0,10}(\r?\n|,|$)))";
+            // String matchCharAfterNewline = "${equoteFirst}(\r?\n|,|$))).";
+            // Pattern equoteStartPattern = Pattern.compile(equoteStart, Pattern.DOTALL | Pattern.MULTILINE);
+
+            // Match an equote at the end of an field
+            String equoteFieldEnd = "(?<!((?<!\")(\"\"){0,10})\")\"(\r?\n|,|$)";
+
+            Pattern equoteFieldEndPattern = Pattern.compile(equoteFieldEnd, Pattern.DOTALL | Pattern.MULTILINE);
+
+            Pattern newline = Pattern.compile("\n.", Pattern.DOTALL);
+            Matcher newlineMatcher =  newline.matcher(charSequence);
+            newlineMatcher.region(pos, regionEnd);
+
+            int newlinePos = -1;
+            while (true) {
+                if (newlineMatcher.find()) {
+                    newlinePos = newlineMatcher.start();
+
+                    if (newlinePos < nextQuoteStart) {
+                        // the newline should be a safe match
+                        break;
+                    } else if (newlinePos < nextQuoteEnd) {
+                        // search the next line after the quot end
+                        nextQuoteExamined = false;
+                        pos = nextQuoteEnd + 1;
+                        newlineMatcher.region(pos, regionEnd);
+                        continue;
+                    }
+
+                    if (!nextQuoteExamined) {
+                        // Check if there is an equote before a field delimiter
+                        Matcher endMatcher = equoteFieldEndPattern.matcher(charSequence);
+                        endMatcher.region(newlinePos + 1, regionEnd);
+                        if (endMatcher.find()) {
+                            nextQuoteEnd = endMatcher.start();
+
+                            int reverseRegionStart = nextQuoteEnd - 1;
+                            int reverseSearchLength = reverseRegionStart - newlinePos;
+
+                            // Is there a matching equote?
+                            CharSequence reverse = new CharSequenceReverse(charSequence, reverseRegionStart);
+                            Matcher startMatcher = equoteFieldEndPattern.matcher(reverse);
+                            startMatcher.region(0, reverseSearchLength);
+                            // int startPos = -1;
+                            if (startMatcher.find()) {
+                                int reverseMatchStart = startMatcher.start();
+                                nextQuoteStart = nextQuoteEnd - reverseMatchStart;
+                            }
+
+
+                            // If the startPos is after the newline then we are not in a cell
+                            // Otherwise continue searching for newlines after the endPos
+                            if (nextQuoteStart > newlinePos) {
+                                pos = newlinePos + 1;
+                                break;
+                            } else {
+                                pos = nextQuoteEnd + 1;
+                                nextQuoteExamined = false;
+                                newlineMatcher.region(pos, regionEnd);
+                            }
+
+                        } else {
+                            // If there is no known end then accept the newline pos
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                } else {
+                    newlinePos = -1;
+                    break;
+                }
+            }
+
+            boolean result = newlinePos != -1;
+            newlineMatchStart = newlinePos;
+
+            if (!result) {
+                pos = -1;
+            } else {
+                pos = newlinePos + 1;
+            }
+
+            return result;
         }
 
         @Override
         public int start() {
-            return matchStart;
+            return newlineMatchStart;
         }
 
         @Override
@@ -50,7 +237,7 @@ public class CustomPatternCsv
 
         @Override
         public int end() {
-            return matchEnd;
+            return newlineMatchStart + 1;
         }
 
         @Override
@@ -60,7 +247,15 @@ public class CustomPatternCsv
 
         @Override
         public String group() {
-            throw new UnsupportedOperationException();
+            String result;
+
+            if (newlineMatchStart < 0) {
+                throw new IllegalStateException("No match found");
+            }
+
+            result = Character.toString(charSequence.charAt(newlineMatchStart));
+            return result;
+            // throw new UnsupportedOperationException();
         }
 
         @Override
--- sansa-hadoop-jena/src/test/java/net/sansa_stack/hadoop/TestCsvNewlineRegex.java
+++ sansa-hadoop-jena/src/test/java/net/sansa_stack/hadoop/TestCsvNewlineRegex.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TestCsvNewlineRegex {
@@ -19,7 +20,7 @@ public class TestCsvNewlineRegex {
         // foo,"""foo->
         // bar"
         // baz,bay
-        int cand = 0;
+        int cand = 3;
         switch (cand) {
             case 0:
                 return CustomPatternJava.compile("(?<=\n(?!((?<![^\"]\"[^\"]).){0,50000}\"(\r?\n|,|$))).",
@@ -27,21 +28,68 @@ public class TestCsvNewlineRegex {
 
                 // when going back from the quote char before the cell delimiters [,\n$]
                 //
-            case 1:
+            case 1: {
+                // Match an effective quote: A quote that is preceeded by an even number of quotes
+                String eQuote = "[^\"](\"\"){0,10}\"";
+
+                // Match up to n characters not preceeded by an effective quote
+                String noLeadingEQuotedChar = "((?<!${eQuote}(?!\")).){0,50000}";
+
+                // Match a character following a newline but only if the following there is
+                // no effective quote that is NOT preceeded by an effective quote
+                String matchCharAfterNewline = "(?<=\n(?!${noLeadingEQuotedChar}${eQuote}(\r?\n|,|$))).";
+//              String matchCharAfterNewline = "(?<=\n(?!${noLeadingEQuotedChar}${eQuote}(\r?\n|,|$))).";
+
+                String complete = matchCharAfterNewline
+                        .replace("${noLeadingEQuotedChar}", noLeadingEQuotedChar)
+                        .replace("${eQuote}", eQuote);
+
+                // System.out.println("complete:" + complete);
+
                 return CustomPatternJava.compile(
-                        //"(?<=\n(?!((?<![^\"]\"(\"\"){0,10}[^\"]).){0," + maxCharsPerColumn + "}(\"\"){0,10}\"(\r?\n|,|$))).",
-                        // "(?<=(\n|^)(?!((?<![^\"](\"\"){0,10}\"[^\"]).){0," + maxCharsPerColumn + "}(\"\"){0,10}\"(\r?\n|,|$))).",
-
-                        // There must not be an unescaped quote char sequence followed by a quote char
-                        "(?<=(\n|^)(?!((?<![^\"](\"\"){0,10}\"(?!\")).){0,50000}(\"\"){0,10}\"(\r?\n|,|$))).",
-
+                        // There must not be an unescaped quote char before line limiter
+                        // without a prior unescaped quote char
+                        //"(?<=(\n|^)(?!((?<![^\"](\"\"){0,10}\"(?!\")).){0,50000}[^\"](\"\"){0,10}\"(\r?\n|,|$))).",
+                        complete,
                         Pattern.DOTALL);
 /*
                 return Pattern.compile(
                         "(?<=\n(?!((?<!(?<![^\"](\"\"){0,10}\")).){0," + maxCharsPerColumn + "}\"(\r?\n|,|$))).",
                         Pattern.DOTALL);
 */
+            }
+            case 2: {
+                // Match the first quote in a sequence of quotes:
+                // A quote that is
+                // - not preceded by a quote
+                // - is followed by an even number of quotes followed by a non-quote char or end-of-line
+                String equoteFirst = "((?<!\")\"(?=(\"\"){0,10}([^\"]|$)))";
+                String equoteLast = "((?<=(^|[^\"])(\"\"){0,10})\"(?!\"))";
 
+                // A character not preceded by an effective quote
+                String unequotedChar = "((?<!${equoteLast}).)";
+
+                // Match a character following a newline but only if the following there is
+                // no effective quote that is NOT preceeded by an effective quote
+                String matchCharAfterNewline = "(?<=\n(?!(?<!${equoteLast}).{0,50000}${equoteFirst}(\r?\n|,|$))).";
+//              String matchCharAfterNewline = "(?<=\n(?!${noLeadingEQuotedChar}${eQuote}(\r?\n|,|$))).";
+
+                String complete = matchCharAfterNewline
+                        .replace("${unequotedChar}", unequotedChar)
+                        .replace("${equoteFirst}", equoteFirst)
+                        .replace("${equoteLast}", equoteLast)
+                        ;
+
+                // System.out.println("complete:" + complete);
+
+                return CustomPatternJava.compile(
+                        // There must not be an unescaped quote char before line limiter
+                        // without a prior unescaped quote char
+                        //"(?<=(\n|^)(?!((?<![^\"](\"\"){0,10}\"(?!\")).){0,50000}[^\"](\"\"){0,10}\"(\r?\n|,|$))).",
+                        complete,
+                        Pattern.DOTALL);
+            }
+            case 3: return new CustomPatternCsv();
             default:
                 return null;
         }
@@ -49,30 +97,52 @@ public class TestCsvNewlineRegex {
 
     @Test
     public void test1() {
-        CustomPattern pattern = createPattern();
 
-        String input1 = String.join("\n",
+        /*
+        Pattern testPattern = Pattern.compile("\n");
+        Matcher testMatcher = testPattern.matcher("abcd");
+        System.out.println(testMatcher.group());
+         */
+
+        CustomPattern pattern = new CustomPatternCsv();
+
+                //createPattern();
+
+        // expected: 4, 8, 35
+        String input0 = String.join("\n",
                 "a,b",
                 "x,y",
                 "\"\"\"this\nis\nmultiline\"\"\", d",
                 "e,f"
         );
 
-        String input2 = String.join("\n",
+        // expected 4, 8, 18
+        String input1 = String.join("\n",
                 "a,b",
                 "x,y",
                 "\"\"\"\"\"\", d",
                 "e,f"
         );
 
+        // 17
+        String input2 = String.join("\n",
+                "a,b",
+                "x,y",
+                "\"\"\"\"\", d",
+                "e,f"
+        );
 
-        List<String> inputs = Arrays.asList(input1, input2);
+
+        List<String> inputs = Arrays.asList(
+                input0,
+                input1,
+                input2);
         int i = 0;
         for (String input : inputs) {
             System.out.println("input #" + i++);
             CustomMatcher m = pattern.matcher(input);
             while (m.find()) {
-                System.out.println(m.start() + ": " + m.group());
+                System.out.println("newline at pos: " + m.start() + " --- group: " + m.group());
             }
         }
     }
