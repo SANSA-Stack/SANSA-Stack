bdf379f6123905fa04328895832337faba10f323
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2022-03-23 13:44:34 +0100

    Added support for ?ROWNUM in tarql operation (automatically applies zipWithIndex if that feature is used)

--- sansa-spark-jena-java/src/main/java/net/sansa_stack/spark/rdd/op/rdf/JavaRddOfBindingsOps.java
+++ sansa-spark-jena-java/src/main/java/net/sansa_stack/spark/rdd/op/rdf/JavaRddOfBindingsOps.java
@@ -12,19 +12,29 @@ import org.apache.jena.query.Query;
 import org.apache.jena.sparql.ARQConstants;
 import org.apache.jena.sparql.algebra.Algebra;
 import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.algebra.OpVars;
 import org.apache.jena.sparql.core.DatasetGraphFactory;
 import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.engine.binding.BindingFactory;
 import org.apache.jena.sparql.engine.main.OpExecutor;
+import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.syntax.Template;
 import org.apache.jena.sparql.util.Context;
 import org.apache.jena.sparql.util.NodeFactoryExtra;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 
+import java.util.Collection;
+
 public class JavaRddOfBindingsOps {
+
+    /** The special ?ROWNUM variable supported by tarql */
+    public static final Var ROWNUM = Var.alloc("ROWNUM");
 
     /**
      * Use an RDD of bindings as initial bindings for a construct query in order to yield triples.
@@ -45,6 +55,7 @@ public class JavaRddOfBindingsOps {
                     .flatMap(QueryFlowOps.createMapperTriples(template)::apply);
         };
 
+        rdd = enrichRddWithRowNumIfNeeded(rdd, query);
         return JavaRddRxOps.mapPartitions(rdd, mapper);
     }
 
@@ -67,6 +78,7 @@ public class JavaRddOfBindingsOps {
                     .flatMap(QueryFlowOps.createMapperQuads(template)::apply);
         };
 
+        rdd = enrichRddWithRowNumIfNeeded(rdd, query);
         return JavaRddRxOps.mapPartitions(rdd, mapper);
     }
 
@@ -89,6 +101,21 @@ public class JavaRddOfBindingsOps {
                     .toFlowable();
         };
 
+        rdd = enrichRddWithRowNumIfNeeded(rdd, query);
         return JavaRddRxOps.mapPartitions(rdd, mapper);
+    }
+
+    /** If the given query mentions a variable ?ROWNUM (upper case) then the input rdd of bindings is
+     *  zipped with index */
+    public static JavaRDD<Binding> enrichRddWithRowNumIfNeeded(JavaRDD<Binding> rdd, Query query) {
+        Op op = Algebra.compile(query);
+        Collection<Var> mentionedVars = OpVars.mentionedVars(op);
+        boolean usesRowNum = mentionedVars.contains(ROWNUM);
+
+        JavaRDD<Binding> result = usesRowNum
+                ? rdd.zipWithIndex().map(bi -> BindingFactory.binding(bi._1, ROWNUM, NodeValue.makeInteger(bi._2).asNode()))
+                : rdd;
+
+        return result;
     }
 }
