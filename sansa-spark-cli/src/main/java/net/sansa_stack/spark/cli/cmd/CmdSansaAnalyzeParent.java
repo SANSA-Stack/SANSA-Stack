b6f9b981669c69dce1c5216128e256c89b772941
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-09 16:35:14 +0100

    Docker build now pushes additional image with project version (not just 'latest')

--- sansa-spark-cli/src/main/java/net/sansa_stack/spark/cli/cmd/CmdSansaAnalyzeParent.java
+++ sansa-spark-cli/src/main/java/net/sansa_stack/spark/cli/cmd/CmdSansaAnalyzeParent.java
@@ -3,9 +3,8 @@ package net.sansa_stack.spark.cli.cmd;
 import picocli.CommandLine.Command;
 
 @Command(name = "analyze",
-    description = "Analyze parallel parallel parsing of files",
+    description = "Analyze parallel parsing of files",
     subcommands = { CmdSansaAnalyzeRdf.class, CmdSansaAnalyzeCsv.class }
 )
 public class CmdSansaAnalyzeParent {
-
 }
