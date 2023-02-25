f349cb9f667368a66e02d03831fc99b90c0418fa
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-25 21:07:41 +0100

    Added simple cost model to remove low impact cache operations from sparql dag scheduling

--- sansa-query/sansa-query-spark/src/main/java/net/sansa_stack/query/spark/rdd/op/JavaRddOfBindingsOps.java
+++ sansa-query/sansa-query-spark/src/main/java/net/sansa_stack/query/spark/rdd/op/JavaRddOfBindingsOps.java
@@ -6,20 +6,24 @@ import net.sansa_stack.query.spark.engine.ExecutionDispatch;
 import net.sansa_stack.query.spark.engine.OpExecutor;
 import net.sansa_stack.query.spark.engine.OpExecutorImpl;
 import net.sansa_stack.spark.util.JavaSparkContextUtils;
+import org.aksw.commons.util.algebra.ExprFilter;
+import org.aksw.commons.util.algebra.ExprOps;
 import org.aksw.commons.util.algebra.GenericDag;
 import org.aksw.jena_sparql_api.algebra.utils.OpUtils;
 import org.aksw.jena_sparql_api.algebra.utils.OpVar;
-import org.aksw.jenax.arq.util.quad.QuadUtils;
 import org.aksw.jenax.arq.util.syntax.QueryGenerationUtils;
+import org.aksw.rml.jena.impl.SparqlX_Rml_Terms;
 import org.apache.jena.atlas.iterator.Iter;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.query.ARQ;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryType;
 import org.apache.jena.sparql.ARQConstants;
 import org.apache.jena.sparql.algebra.Algebra;
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.op.OpDisjunction;
+import org.apache.jena.sparql.algebra.op.OpJoin;
 import org.apache.jena.sparql.algebra.op.OpLateral;
 import org.apache.jena.sparql.algebra.op.OpService;
 import org.apache.jena.sparql.core.Quad;
@@ -39,6 +43,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class JavaRddOfBindingsOps {
@@ -54,7 +59,7 @@ public class JavaRddOfBindingsOps {
         Quad quadVars = Quad.create(Var.alloc("__g__"), Var.alloc("__s__"), Var.alloc("__p__"), Var.alloc("__o__"));
 
         List<Query> lateralConstructQueries = queries.stream()
-                .map(query -> QueryGenerationUtils.constructToLateral(query, quadVars, false))
+                .map(query -> QueryGenerationUtils.constructToLateral(query, quadVars, QueryType.CONSTRUCT, false, true))
                 .collect(Collectors.toList());
 
         Op rootOp;
@@ -90,13 +95,17 @@ public class JavaRddOfBindingsOps {
         return result;
     }
 
+    public static boolean defaultBlocker(Op parent, int childIdx, Op child) {
+        boolean result = parent instanceof OpService || (parent instanceof OpLateral && childIdx != 0);
+        return result;
+    }
 
     public static GenericDag<Op, Var> buildDag(Collection<Query> queries) {
         List<Op> ops = queries.stream().map(Algebra::compile).collect(Collectors.toList());
         OpDisjunction union = OpDisjunction.create();
         ops.forEach(union::add);
         // Do not descend into the rhs of laterals
-        GenericDag<Op, Var> dag = new GenericDag<>(OpUtils.getOpOps(),  new VarAlloc("op")::allocVar, (p, i, c) -> p instanceof OpService || (p instanceof OpLateral && i != 0));
+        GenericDag<Op, Var> dag = new GenericDag<>(OpUtils.getOpOps(),  new VarAlloc("op")::allocVar, JavaRddOfBindingsOps::defaultBlocker);
         dag.addRoot(union);
 
         // Insert cache nodes:
@@ -104,17 +113,35 @@ public class JavaRddOfBindingsOps {
         // becomes
         // ?vx      := cache(vxCache)
         // ?vxCache := someOp(vy)
+        Set<Var> cacheCandidates = new HashSet<>();
         for (Map.Entry<Var, Collection<Var>> entry : dag.getChildToParent().asMap().entrySet()) {
             Var childVar = entry.getKey();
             Collection<Var> parentVars = entry.getValue();
             if (parentVars.size() > 1) {
+                cacheCandidates.add(childVar);
                 // System.out.println("Multiple parents on: " + entry.getKey());
-                Op childDef = dag.getExpr(childVar);
-                Var uncachedVar = Var.alloc(childVar.getName() + "_cached");
-                dag.getVarToExpr().remove(childVar);
-                dag.getVarToExpr().put(uncachedVar, childDef);
-                dag.getVarToExpr().put(childVar, new OpService(NodeFactory.createURI("rdd:cache"), new OpVar(uncachedVar), false));
             }
+        }
+        Set<Op> cacheCandidateOps = cacheCandidates.stream().map(dag::getExpr).collect(Collectors.toSet());
+        Map<Op, Float> costs = assessCacheImpact(dag, cacheCandidateOps);
+        for (Op candOp : cacheCandidateOps) {
+            float cost = costs.get(candOp);
+            if (cost < 10000) {
+                Var var = dag.getVar(candOp);
+                logger.info("Removing low impact cache candidate (cost=" + cost + "): " + var);
+                cacheCandidates.remove(var);
+            }
+        }
+        logger.info("Remaining cache nodes: " + cacheCandidates);
+
+        // Remove all ops with a low effectiveness
+
+        for (Var childVar : cacheCandidates) {
+            Op childDef = dag.getExpr(childVar);
+            Var uncachedVar = Var.alloc(childVar.getName() + "_cached");
+            dag.getVarToExpr().remove(childVar);
+            dag.getVarToExpr().put(uncachedVar, childDef);
+            dag.getVarToExpr().put(childVar, new OpService(NodeFactory.createURI("rdd:cache"), new OpVar(uncachedVar), false));
         }
 
         dag.collapse();
@@ -123,8 +150,77 @@ public class JavaRddOfBindingsOps {
             // logger.info(e.toString());
             System.err.println(e.toString());
         }
+
+        // if (true) { throw new RuntimeException(); }
+
         return dag;
     }
+
+
+    /**
+     * Remove cache operations closer to the root that do not bring sufficient benefit over cache operations lower in the tree.
+     * Uses a simple cost heuristic:
+     * - rml sources have high cost
+     * - joins have high cost
+     * - rest is cheap (e.g. extends)
+     */
+    public static Map<Op, Float> assessCacheImpact(GenericDag<Op, Var> dag, Set<Op> cacheCandidateOp) {
+        Map<Op, Float> costs = new HashMap<>();
+        Consumer<Op> computeCost = op -> {
+            float cost = assessCostContribution(op, costs, cacheCandidateOp);
+            costs.put(op, cost);
+        };
+        for (Op root : dag.getRoots()) {
+            GenericDag.depthFirstTraverse(dag, null, 0, root, JavaRddOfBindingsOps::defaultBlocker, computeCost);
+        }
+        return costs;
+    }
+
+    public static boolean isCacheOp(Op op) { return OpUtils.isServiceWithIri(op, "rdd:cache"); }
+    public static boolean isRmlSourceOp(Op op) { return OpUtils.isServiceWithIri(op, SparqlX_Rml_Terms.RML_SOURCE_SERVICE_IRI); }
+
+
+    /**
+     * A cacheable node itself has a (typically high) cost - however, a cacheable node in the role of a sub-node has cost 0.
+     *
+     * @param op
+     * @param costs
+     * @param cacheCandidates
+     * @return
+     */
+    public static float assessCostContribution(Op op, Map<Op, Float> costs, Set<Op> cacheCandidates) {
+        float result;
+        // if (isCacheOp(op)) {
+        if (isRmlSourceOp(op)) {
+            result = 1_000_000;
+        } else if (op instanceof OpJoin || op instanceof OpDisjunction) {
+            result = 1_000_000;
+        } else {
+            List<Op> subOps = OpUtils.getSubOps(op);
+            float maxCost = 0;
+            for (Op subOp : subOps) {
+                float cost;
+                if (cacheCandidates.contains(subOp)) {
+                    cost = 0;
+                } else {
+                    cost = costs.get(subOp);
+                }
+                if (cost > maxCost) {
+                    maxCost = cost;
+                }
+            }
+            // Certainly cheap ops: extend, project, union, disjunction
+            // Possibly expensive ops: filter (if selective)
+            result = maxCost + 1;
+        }
+        return result;
+    }
+
+    //    public static <E, V> void pruneSuperfluousCacheOps(GenericDag<E, V> dag, ExprFilter<E> isBlocked) {
+//        for (E root : dag.getRoots()) {
+//            pruneSuperfluousCacheOps(dag, null, 0, root, costs, isBlocked);
+//        }
+//    }
 
     public static JavaRDD<Quad> execSparqlConstruct(JavaRDD<Binding> initialRdd, Query query, Context cxt) {
         Op op = Algebra.compile(query);
