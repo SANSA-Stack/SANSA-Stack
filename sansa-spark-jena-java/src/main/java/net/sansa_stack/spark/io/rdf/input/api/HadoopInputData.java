a96aefd5fbe32ec80076cb1729f71259d10122fd
Claus Stadler <RavenArkadon@googlemail.com>
2023-03-08 01:44:02 +0100

    Added more javadoc

--- sansa-spark-jena-java/src/main/java/net/sansa_stack/spark/io/rdf/input/api/HadoopInputData.java
+++ sansa-spark-jena-java/src/main/java/net/sansa_stack/spark/io/rdf/input/api/HadoopInputData.java
@@ -5,8 +5,18 @@ import java.util.function.Function;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
-/** A class to capture the arguments of sparkContext.newApiHadoopRDD */
+/**
+ * A class to capture the arguments of {@link JavaSparkContext#newAPIHadoopFile(String, Class, Class, Class, Configuration)}.
+ * Furthermore, captures a mapping of the initial JavaPairRDD to a custom target type (typically another JavaRDD).
+ *
+ * To create RDDs from objects of this class use {@link InputFormatUtils#createRdd(JavaSparkContext, HadoopInputData)}.
+ * HadoopInputData based on the {@link net.sansa_stack.hadoop.core.RecordReaderGenericBase} can be wrapped using
+ * {@link InputFormatUtils#wrapWithAnalyzer(HadoopInputData)} which returns another HadoopInputData object.
+ * RDDs created from the wrapper compute information
+ * about the detected records, such as the byte offsets and the time it took to parse them.
+ */
 public class HadoopInputData<K, V, X> {
     protected String path;
     protected Class<? extends InputFormat<K, V>> inputFormatClass;
@@ -51,6 +61,7 @@ public class HadoopInputData<K, V, X> {
         return mapper;
     }
 
+    /** Return a fresh {@link HadoopInputData} instance where "nextMapper" is applied to the result of the current mapper */
     public <Y> HadoopInputData<K, V, Y> map(Function<? super X, Y> nextMapper) {
         return new HadoopInputData<>(path, inputFormatClass, keyClass, valueClass, configuration, mapper.andThen(nextMapper));
     }
